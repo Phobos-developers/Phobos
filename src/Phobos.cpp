9130ee62209b71317e11bc43cdddd2db9f9889da
Belonit <Belonit@ya.ru>
2020-09-05 19:22:15 +0300

    added notification of unstable versions

--- src/Phobos.cpp
+++ src/Phobos.cpp
@@ -38,3 +38,23 @@ DEFINE_HOOK(52F639, _YR_CmdLineParse, 5)
 //
 //	return true;
 //}
+
+#ifndef IS_RELEASE_VER
+DEFINE_HOOK(4F4583, GScreenClass_DrawText, 6)
+{
+	auto string = L"This version of Phobos is unstable";
+	auto wanted = Drawing::GetTextDimensions(string);
+
+	RectangleStruct rect = {
+		DSurface::Composite->GetWidth() - wanted.Width - 10,
+		0,
+		wanted.Width + 10,
+		wanted.Height + 10
+	};
+
+	DSurface::Composite->FillRect(&rect, COLOR_BLACK);
+	DSurface::Composite->DrawTextA(string, rect.X + 5, 5, COLOR_RED);
+
+	return 0;
+}
+#endif
