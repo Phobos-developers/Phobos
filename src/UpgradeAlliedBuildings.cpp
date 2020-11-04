aaf6ec3fcc7edb126d8dbbdfb3b6b411039ce91c
Meta (X230T) <crabiter@vivaldi.net>
2020-11-04 18:36:32 +0200

    More hooks and fixes

--- src/UpgradeAlliedBuildings.cpp
+++ src/UpgradeAlliedBuildings.cpp
@@ -3,14 +3,28 @@
 #include <BuildingTypeClass.h>
 #include <HouseClass.h>
 
-DEFINE_HOOK(452678, UpgradeAlliedBuildings, 8)
+DEFINE_HOOK(452678, CanUpgrade_UpgradeAlliedBuildings, 8)
 {
 	GET(BuildingClass*, pThis, ECX);
-	GET_BASE(BuildingTypeClass*, upgrade, 0x4);
-	GET_BASE(HouseClass*, upgradeOwner, 0x8);
+	GET(BuildingTypeClass*, upgrade, EDI);
+	GET(HouseClass*, upgradeOwner, EAX);
 
-	if (upgradeOwner == pThis->Owner)
-		return 0x452680; // continue checking
+	if (upgradeOwner != pThis->Owner)
+		return 0x4526B5; // can't upgrade
 	
-	return 0x4526B5; // can't upgrade
+	return 0x452680; // continue checking
+}
+
+DEFINE_HOOK(4408EB, Unlimbo_UpgradeAlliedBuildings, 10)
+{
+	GET(BuildingClass*, pThis, ESI);
+	GET(BuildingClass*, buildingUnderMouse, EDI);
+
+	R->EBX(pThis->Type);
+	//pThis->Owner = buildingUnderMouse->Owner;
+
+	if (pThis->Owner != buildingUnderMouse->Owner)
+		return 0x440926;
+	
+	return 0x4408F5;
 }
