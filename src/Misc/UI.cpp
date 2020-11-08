2130e22271e65f18df759352aa56e0939d22e89d
Belonit <Belonit@ya.ru>
2020-11-09 00:02:10 +0300

    add new tag for uimd.ini - [LoadingScreen]->DisableEmptySpawnPosition

--- src/Misc/UI.cpp
+++ src/Misc/UI.cpp
@@ -11,13 +11,28 @@ DEFINE_HOOK(777C41, UI_ApplyAppIcon, 9)
 
 	return 0;
 }
-
-DEFINE_HOOK(640B8D, UI_DisableBlackPoints, 6)
+bool LS_DisableEmptySpawnPosition = false;
+DEFINE_HOOK(640B8D, LoadingScreen_DisableEmptySpawnPosition, 6)
 {
-	return 0x640CE2;
+	GET(bool, esi, ESI);
+	if (LS_DisableEmptySpawnPosition || !esi) {
+		return 0x640CE2;
+	}
+	return 0x640B93;
 }
 
-//DEFINE_HOOK(640E78, UI_DisableColorPoints, 6)
+//DEFINE_HOOK(640E78, LoadingScreen_DisableColorPoints, 6)
 //{
 //	return 0x641071;
 //}
+
+DEFINE_HOOK(5FACDF, UIMD_LoadFromINI, 5)
+{
+	CCINIClass* pINI = Phobos::OpenConfig("uimd.ini");
+
+	LS_DisableEmptySpawnPosition =
+		pINI->ReadBool("LoadingScreen", "DisableEmptySpawnPosition", false);
+
+	Phobos::CloseConfig(pINI);
+	return 0;
+}
