4dfaab9a0bfc5ff7a1394bf068125070d12a12e6
Meta (X230T) <crabiter@vivaldi.net>
2021-02-20 13:28:57 +0200

    Fix MCRangeLimit not being serialized

--- src/Ext/TechnoType/Serialize.hpp
+++ src/Ext/TechnoType/Serialize.hpp
@@ -3,4 +3,5 @@ STM_Process(this->HealthBar_Hide)
 STM_Process(this->UIDescriptionLabel)
 STM_Process(this->LowSelectionPriority)
 STM_Process(this->GroupAs)
+STM_Process(this->MindControlRangeLimit)
 
