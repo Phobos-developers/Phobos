ef3aad4f1e4fb35bbb1a4a27a87a9dd3c90372c1
secsome <302702960@qq.com>
2021-03-16 22:36:40 +0800

    use STACK_OFFS while get_stack

--- src/Ext/TechnoType/Hooks.cpp
+++ src/Ext/TechnoType/Hooks.cpp
@@ -54,7 +54,7 @@ DEFINE_HOOK(6F9E50, TechnoClass_Update, 5)
 
 DEFINE_HOOK(6F3C56, TechnoClass_Transform_6F3AD0_TurretMultiOffset, 0)
 {
-	LEA_STACK(Matrix3D*, mtx, 0xD8 - 0x90);
+	LEA_STACK(Matrix3D*, mtx, STACK_OFFS(0xD8, 0x90));
 	GET(TechnoTypeClass*, technoType, EDX);
 
 	TechnoTypeExt::ApplyTurretOffset(technoType, mtx);
@@ -64,7 +64,7 @@ DEFINE_HOOK(6F3C56, TechnoClass_Transform_6F3AD0_TurretMultiOffset, 0)
 
 DEFINE_HOOK(6F3E6E, FootClass_firecoord_6F3D60_TurretMultiOffset, 0)
 {
-	LEA_STACK(Matrix3D*, mtx, 0xCC - 0x90);
+	LEA_STACK(Matrix3D*, mtx, STACK_OFFS(0xCC, 0x90));
 	GET(TechnoTypeClass*, technoType, EBP);
 
 	TechnoTypeExt::ApplyTurretOffset(technoType, mtx);
@@ -85,7 +85,7 @@ DEFINE_HOOK(73B780, UnitClass_DrawVXL_TurretMultiOffset, 0)
 
 DEFINE_HOOK(73BA4C, UnitClass_DrawVXL_TurretMultiOffset1, 0)
 {
-	LEA_STACK(Matrix3D*, mtx, 0x1D0 - 0x13C);
+	LEA_STACK(Matrix3D*, mtx, STACK_OFFS(0x1D0, 0x13C));
 	GET(TechnoTypeClass*, technoType, EBX);
 
 	double& factor = *reinterpret_cast<double*>(0xB1D008);
