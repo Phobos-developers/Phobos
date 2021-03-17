8c1dc3c5e8719c1fc99770f746fea6c54366fa8c
secsome <302702960@qq.com>
2021-03-17 14:08:36 +0800

    WarheadType

--- src/Ext/WarheadType/Body.h
+++ src/Ext/WarheadType/Body.h
@@ -49,19 +49,33 @@ public:
 		void Detonate(TechnoClass* pOwner, HouseClass* pHouse, BulletClass* pBullet, CoordStruct coords);
 		bool CanTargetHouse(HouseClass* pHouse, TechnoClass* pTechno);
 
-		virtual void LoadFromINIFile(CCINIClass* pINI) override;
 		virtual ~ExtData() = default;
 
-		virtual void InvalidatePointer(void* ptr, bool bRemoved) override {}
-		virtual void LoadFromStream(IStream* Stm);
-		virtual void SaveToStream(IStream* Stm) const;
+		virtual void Initialize() override;
+
+		virtual void LoadFromINIFile(CCINIClass* pINI) override;
+
+		virtual void InvalidatePointer(void* ptr, bool bRemoved) override {
+		}
+
+		virtual void LoadFromStream(PhobosStreamReader& Stm) override;
+
+		virtual void SaveToStream(PhobosStreamWriter& Stm) override;
+	
+	private:
+		template <typename T>
+		void Serialize(T& Stm);
 	};
 
 	class ExtContainer final : public Container<WarheadTypeExt> {
 	public:
 		ExtContainer();
 		~ExtContainer();
+
+		virtual void InvalidatePointer(void* ptr, bool bRemoved) override;
 	};
 
 	static ExtContainer ExtMap;
+	static bool LoadGlobals(PhobosStreamReader& Stm);
+	static bool SaveGlobals(PhobosStreamWriter& Stm);
 };
