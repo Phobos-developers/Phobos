c05cc44a523dee000995035c529b18df9adda31e
Uranusian <mikesariitten@gmail.com>
2021-03-29 23:16:44 +0800

    Fix missing hooks of RulesExt

--- src/Ext/Rules/Body.cpp
+++ src/Ext/Rules/Body.cpp
@@ -182,3 +182,32 @@ DEFINE_HOOK(675205, RulesClass_Save_Suffix, 8)
 
 	return 0;
 }
+
+DEFINE_HOOK(668BF0, RulesClass_Addition, 5)
+{
+	GET(RulesClass*, pItem, ECX);
+	GET_STACK(CCINIClass*, pINI, 0x4);
+
+	//	RulesClass::Initialized = false;
+	RulesExt::LoadFromINIFile(pItem, pINI);
+	return 0;
+}
+
+DEFINE_HOOK(679A15, RulesData_LoadBeforeTypeData, 6)
+{
+	GET(RulesClass*, pItem, ECX);
+	GET_STACK(CCINIClass*, pINI, 0x4);
+
+	//	RulesClass::Initialized = true;
+	RulesExt::LoadBeforeTypeData(pItem, pINI);
+	return 0;
+}
+
+DEFINE_HOOK(679CAF, RulesData_LoadAfterTypeData, 5)
+{
+	RulesClass* pItem = RulesClass::Global();
+	GET(CCINIClass*, pINI, ESI);
+
+	RulesExt::LoadAfterTypeData(pItem, pINI);
+	return 0;
+}
