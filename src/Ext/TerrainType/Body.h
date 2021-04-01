49a9dd54333093e0e3886b5b39d05b7ca96566dc
Meta (X230T) <crabiter@vivaldi.net>
2021-04-02 02:09:26 +0300

    TerrainTypeExt

---
+++ src/Ext/TerrainType/Body.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <TerrainTypeClass.h>
+
+#include <Helpers/Macro.h>
+#include "../_Container.hpp"
+#include "../../Utilities/TemplateDef.h"
+
+class TerrainTypeExt
+{
+public:
+    using base_type = TerrainTypeClass;
+
+    class ExtData final : public Extension<TerrainTypeClass>
+    {
+    public:
+        Valueable<int> SpawnsTiberium_Type;
+
+        ExtData(TerrainTypeClass* OwnerObject) : Extension<TerrainTypeClass>(OwnerObject),
+            SpawnsTiberium_Type(0)
+        { }
+
+        virtual ~ExtData() = default;
+
+        virtual void LoadFromINIFile(CCINIClass* pINI) override;
+
+        virtual void InvalidatePointer(void* ptr, bool bRemoved) override {}
+
+        virtual void LoadFromStream(PhobosStreamReader& Stm) override;
+        virtual void SaveToStream(PhobosStreamWriter& Stm) override;
+    
+    private:
+        template <typename T>
+        void Serialize(T& Stm);
+    };
+
+    class ExtContainer final : public Container<TerrainTypeExt> {
+    public:
+        ExtContainer();
+        ~ExtContainer();
+    };
+
+    static ExtContainer ExtMap;
+
+    static bool LoadGlobals(PhobosStreamReader& Stm);
+    static bool SaveGlobals(PhobosStreamWriter& Stm);
+};
