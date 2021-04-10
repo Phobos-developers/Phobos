07c8f8c23ffd23e0c354b9102cc90783e454a555
Meta (X230T) <crabiter@vivaldi.net>
2021-04-10 23:45:35 +0300

    Fixup YRpp and MassActions, increment version

--- src/Phobos.Ext.cpp
+++ src/Phobos.Ext.cpp
@@ -1,8 +1,10 @@
 #include "Phobos.h"
 
+#include "Ext/Building/Body.h"
 #include "Ext/BuildingType/Body.h"
 #include "Ext/Bullet/Body.h"
 #include "Ext/BulletType/Body.h"
+#include "Ext/House/Body.h"
 #include "Ext/RadSite/Body.h"
 #include "Ext/Rules/Body.h"
 #include "Ext/Script/Body.h"
@@ -10,6 +12,7 @@
 #include "Ext/SWType/Body.h"
 #include "Ext/Techno/Body.h"
 #include "Ext/TechnoType/Body.h"
+#include "Ext/TerrainType/Body.h"
 #include "Ext/WarheadType/Body.h"
 #include "Ext/WeaponType/Body.h"
 
@@ -23,37 +26,44 @@
 // which can make selecting an overload unattractive,
 // because it's a user-defined conversion. the more
 // conversions, the less attractive
-struct Dummy {
-	Dummy(int a) {};
+struct Dummy
+{
+	Dummy(int a) { };
 };
 
 // this is a typed nothing: a type list type
 template <typename...>
-struct DummyTypes {};
+struct DummyTypes { };
 
 // calls:
 // T::Clear()
 // T::ExtMap.Clear()
-struct ClearHelper {
+struct ClearHelper
+{
 	template <typename T>
-	static bool Process() {
+	static bool Process()
+	{
 		clear<T>(0, 0);
+
 		return true;
 	}
 
 private:
 	template <typename T>
-	static auto clear(int, int) -> decltype(T::Clear()) {
+	static auto clear(int, int) -> decltype(T::Clear())
+	{
 		T::Clear();
 	}
 
 	template <typename T>
-	static auto clear(Dummy, int) -> decltype(T::ExtMap.Clear()) {
+	static auto clear(Dummy, int) -> decltype(T::ExtMap.Clear())
+	{
 		T::ExtMap.Clear();
 	}
 
 	template <typename T>
-	static auto clear(Dummy, Dummy) -> void {
+	static auto clear(Dummy, Dummy) -> void
+	{
 		// do nothing
 	}
 };
@@ -61,50 +71,60 @@ private:
 // calls:
 // T::PointerGotInvalid(void*, bool)
 // T::ExtMap.PointerGotInvalid(void*, bool)
-struct InvalidatePointerHelper {
+struct InvalidatePointerHelper
+{
 	template <typename T>
-	static bool Process(void* ptr, bool removed) {
+	static bool Process(void* ptr, bool removed)
+	{
 		invalidpointer<T>(0, 0, ptr, removed);
+
 		return true;
 	}
 
 private:
 	template <typename T>
-	static auto invalidpointer(int, int, void* ptr, bool removed) -> decltype(T::PointerGotInvalid(ptr, removed)) {
+	static auto invalidpointer(int, int, void* ptr, bool removed) -> decltype(T::PointerGotInvalid(ptr, removed))
+	{
 		T::PointerGotInvalid(ptr, removed);
 	}
 
 	template <typename T>
-	static auto invalidpointer(Dummy, int, void* ptr, bool removed) -> decltype(T::ExtMap.PointerGotInvalid(ptr, removed)) {
+	static auto invalidpointer(Dummy, int, void* ptr, bool removed) -> decltype(T::ExtMap.PointerGotInvalid(ptr, removed))
+	{
 		T::ExtMap.PointerGotInvalid(ptr, removed);
 	}
 
 	template <typename T>
-	static auto invalidpointer(Dummy, Dummy, void* ptr, bool removed) -> void {
+	static auto invalidpointer(Dummy, Dummy, void* ptr, bool removed) -> void
+	{
 		// do nothing
 	}
 };
 
 // calls:
 // T::LoadGlobals(PhobosStreamReader&)
-struct LoadHelper {
+struct LoadHelper
+{
 	template <typename T>
-	static bool Process(IStream* pStm) {
+	static bool Process(IStream* pStm)
+	{
 		return load<T>(0, pStm);
 	}
 
 private:
 	template <typename T>
-	static auto load(int, IStream* pStm) -> decltype(T::LoadGlobals(std::declval<PhobosStreamReader&>())) {
+	static auto load(int, IStream* pStm) -> decltype(T::LoadGlobals(std::declval<PhobosStreamReader&>()))
+	{
 		PhobosByteStream Stm(0);
 		Stm.ReadBlockFromStream(pStm);
-
 		PhobosStreamReader Reader(Stm);
+
 		return T::LoadGlobals(Reader) && Reader.ExpectEndOfBlock();
 	}
 
 	template <typename T>
-	static auto load(Dummy, IStream* pStm) -> bool {
+	static auto load(Dummy, IStream* pStm) -> bool
+	{
 		// do nothing
 		return true;
 	}
@@ -112,22 +132,27 @@ private:
 
 // calls:
 // T::SaveGlobals(PhobosStreamWriter&)
-struct SaveHelper {
+struct SaveHelper
+{
 	template <typename T>
-	static bool Process(IStream* pStm) {
+	static bool Process(IStream* pStm)
+	{
 		return save<T>(0, pStm);
 	}
 
 private:
 	template <typename T>
-	static auto save(int, IStream* pStm) -> decltype(T::SaveGlobals(std::declval<PhobosStreamWriter&>())) {
+	static auto save(int, IStream* pStm) -> decltype(T::SaveGlobals(std::declval<PhobosStreamWriter&>()))
+	{
 		PhobosByteStream Stm;
 		PhobosStreamWriter Writer(Stm);
+
 		return T::SaveGlobals(Writer) && Stm.WriteBlockToStream(pStm);
 	}
 
 	template <typename T>
-	static auto save(Dummy, IStream* pStm) -> bool {
+	static auto save(Dummy, IStream* pStm) -> bool
+	{
 		// do nothing
 		return true;
 	}
@@ -137,20 +162,25 @@ private:
 // instantiation of this type, and the most appropriate method for each type
 // will be called with no overhead of virtual functions.
 template <typename... Ts>
-struct MassAction {
-	__forceinline void Clear() {
+struct MassAction
+{
+	__forceinline void Clear()
+	{
 		process<ClearHelper>(DummyTypes<Ts...>());
 	}
 
-	__forceinline void InvalidPointer(void* ptr, bool removed) {
+	__forceinline void InvalidPointer(void* ptr, bool removed)
+	{
 		process<InvalidatePointerHelper>(DummyTypes<Ts...>(), ptr, removed);
 	}
 
-	__forceinline bool Load(IStream* pStm) {
+	__forceinline bool Load(IStream* pStm)
+	{
 		return process<LoadHelper>(DummyTypes<Ts...>(), pStm);
 	}
 
-	__forceinline bool Save(IStream* pStm) {
+	__forceinline bool Save(IStream* pStm)
+	{
 		return process<SaveHelper>(DummyTypes<Ts...>(), pStm);
 	}
 
@@ -162,17 +192,19 @@ private:
 
 	// this is the base case, no more types, nothing to call
 	template <typename T, typename... TArgs>
-	bool process(DummyTypes<>, TArgs... args) {
+	bool process(DummyTypes<>, TArgs... args)
+	{
 		return true;
 	}
 
 	// this is the recursion part: invoke T:Process() for first type, then
 	// recurse with the remaining types
 	template <typename T, typename... TArgs, typename TType, typename... TTypes>
-	__forceinline bool process(DummyTypes<TType, TTypes...>, TArgs... args) {
-		if (!T::Process<TType>(args...)) {
+	__forceinline bool process(DummyTypes<TType, TTypes...>, TArgs... args)
+	{
+		if (!T::Process<TType>(args...))
 			return false;
-		}
+
 		return process<T>(DummyTypes<TTypes...>(), args...);
 	}
 };
@@ -182,9 +214,11 @@ private:
 // Add more class names as you like
 auto MassActions = MassAction <
 	// Ext classes
+	BuildingExt,
 	BuildingTypeExt,
 	BulletExt,
 	BulletTypeExt,
+	HouseExt,
 	RadSiteExt,
 	RulesExt,
 	ScriptExt,
@@ -192,6 +226,7 @@ auto MassActions = MassAction <
 	SWTypeExt,
 	TechnoExt,
 	TechnoTypeExt,
+	TerrainTypeExt,
 	WarheadTypeExt,
 	WeaponTypeExt
 	// enum classes
@@ -215,34 +250,34 @@ DEFINE_HOOK(685659, Scenario_ClearClasses, a)
 	return 0;
 }
 
-void Phobos::Clear() {
+void Phobos::Clear()
+{
 	MassActions.Clear();
 }
 
-void Phobos::PointerGotInvalid(
-	AbstractClass* const pInvalid, bool const removed)
+void Phobos::PointerGotInvalid(AbstractClass* const pInvalid, bool const removed)
 {
 	MassActions.InvalidPointer(pInvalid, removed);
 }
 
-HRESULT Phobos::SaveGameData(IStream* pStm) {
+HRESULT Phobos::SaveGameData(IStream* pStm)
+{
 	Debug::Log("Saving global Phobos data\n");
 
-	if (!MassActions.Save(pStm)) {
+	if (!MassActions.Save(pStm))
 		return E_FAIL;
-	}
 
 	Debug::Log("Finished saving the game\n");
+
 	return S_OK;
 }
 
-void Phobos::LoadGameData(IStream* pStm) {
+void Phobos::LoadGameData(IStream* pStm)
+{
 	Debug::Log("Loading global Phobos data\n");
 
-	if (!MassActions.Load(pStm)) {
+	if (!MassActions.Load(pStm))
 		Debug::Log("Error loading the game\n");
-	}
-	else {
+	else
 		Debug::Log("Finished loading the game\n");
-	}
 }
--- src/Phobos.version.h
+++ src/Phobos.version.h
@@ -12,7 +12,7 @@
 #define VERSION_REVISION 0
 
 // Latest devbuild
-#define BUILD_NUMBER 10
+#define BUILD_NUMBER 11
 
 // Nightly defines GIT_COMMIT and GIT_BRANCH in GH Actions
 
