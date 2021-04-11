f6095c7dcb99ce678cecc697ed512b8aa2332b34
secsome <302702960@qq.com>
2021-04-11 17:58:35 +0800

    add definitions for all Hooks.Process

--- src/Misc/FogOfWar/Hooks.Process.cpp
+++ src/Misc/FogOfWar/Hooks.Process.cpp
@@ -45,8 +45,7 @@ DEFINE_HOOK(4A9CA0, MapClass_RevealFogShroud, 7)
 	return 0x4A9DC6;
 }
 
-// NOT COMPLETELY  IMPLEMENTED YET!
-DEFINE_HOOK(486BF0, CleanFog, 9)
+DEFINE_HOOK(486BF0, CellClass_CleanFog, 9)
 {
 	GET(CellClass*, pCell_, ECX);
 
@@ -57,11 +56,88 @@ DEFINE_HOOK(486BF0, CleanFog, 9)
 		if (pCell->Level >= i - 2 && pCell->Level <= i)
 		{
 			pCell->Flags &= 0xFFBFFFFF;
-			pCell->ClearFoggedObjects(); // call 486C50, which should be rewritten!
+			pCell->ClearFoggedObjects();
 			++pLocation.X;
 			++pLocation.Y;
 		}
 	}
 
 	return 0x486C4C;
+}
+
+// NOT IMPLEMENTED YET!
+DEFINE_HOOK(486A70, CellClass_FogCell, 5)
+{
+	GET(CellClass*, pCell, ECX);
+
+	// rewritten me pls!
+
+	UNREFERENCED_PARAMETER(pCell);
+
+	return 0;
+	// return 0x486BE6;
+}
+
+DEFINE_HOOK(440B8D, BuildingClass_Put_CheckFog, 6)
+{
+	GET(BuildingClass*, pBuilding, ESI);
+
+	if (pBuilding->Is_Fogged())
+	{
+		auto pCell = pBuilding->GetCell();
+
+		// Several extra works to be done here
+		UNREFERENCED_PARAMETER(pCell);
+	}
+
+	return 0x440C08;
+}
+
+// TO BE IMPLEMENTED!
+DEFINE_HOOK(486C50, CellClass_ClearFoggedObjects, 6)
+{
+	GET(CellClass*, pCell, ECX);
+
+	UNREFERENCED_PARAMETER(pCell);
+
+	return 0;
+	// return 0x486D8A;
+}
+
+// TO BE IMPLEMENTED!
+DEFINE_HOOK(70076E, TechnoClass_GetCursorOverCell_OverFog, 5)
+{
+
+	return 0;
+	// return 0x700800;
+}
+
+// TO BE IMPLEMENTED!
+DEFINE_HOOK(6D3470, TacticalClass_DrawFoggedObject, 8)
+{
+	// GET(TacticalClass*, pTactical, ECX);
+	auto const pTactical = TacticalClass::Global();
+	GET_STACK(RectangleStruct*, pRect1, 0x4);
+	GET_STACK(RectangleStruct*, pRect2, 0x8);
+	GET_STACK(bool, bUkn, 0xC);
+
+	UNREFERENCED_PARAMETER(pTactical);
+	UNREFERENCED_PARAMETER(pRect1);
+	UNREFERENCED_PARAMETER(pRect2);
+	UNREFERENCED_PARAMETER(bUkn);
+
+	// Draw them
+
+	return 0;
+	// return 0x6D3650;
+}
+
+// TO BE IMPLEMENTED!
+DEFINE_HOOK(51F97C, InfantryClass_MouseOverCell_OverFog, 5)
+{
+	GET(CellClass*, pCell, EAX);
+
+	UNREFERENCED_PARAMETER(pCell);
+
+	return 0;
 }
