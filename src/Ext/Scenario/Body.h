6496d2b7ca6f34ee6325180f2123dea1083221b8
secsome <302702960@qq.com>
2021-06-10 22:43:47 +0800

    initialize scenarioclass

---
+++ src/Ext/Scenario/Body.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <ScenarioClass.h>
+
+#include <Helpers/Macro.h>
+#include <Ext/_Container.hpp>
+#include <Utilities/TemplateDef.h>
+
+class ScenarioExt
+{
+public:
+	using base_type = ScenarioClass;
+
+	class ExtData final : public Extension<ScenarioClass>
+	{
+	public:
+		ExtData(ScenarioClass* OwnerObject) : Extension<ScenarioClass>(OwnerObject)
+		{
+		}
+
+		virtual ~ExtData() = default;
+
+		virtual void LoadFromINIFile(CCINIClass* pINI) override;
+
+		virtual void InvalidatePointer(void* ptr, bool bRemoved) override { }
+
+		virtual void LoadFromStream(PhobosStreamReader& Stm) override;
+
+		virtual void SaveToStream(PhobosStreamWriter& Stm) override;
+	private:
+		template <typename T>
+		void Serialize(T& Stm);
+	};
+
+private:
+	static std::unique_ptr<ExtData> Data;
+
+public:
+	static IStream* g_pStm;
+
+	static void Allocate(ScenarioClass* pThis);
+	static void Remove(ScenarioClass* pThis);
+
+	static void LoadFromINIFile(ScenarioClass* pThis, CCINIClass* pINI);
+
+	static ExtData* Global()
+	{
+		return Data.get();
+	}
+
+	static void Clear()
+	{
+		Allocate(ScenarioClass::Instance);
+	}
+
+	static void PointerGotInvalid(void* ptr, bool removed)
+	{
+		Global()->InvalidatePointer(ptr, removed);
+	}
+
+	static bool LoadGlobals(PhobosStreamReader& Stm);
+	static bool SaveGlobals(PhobosStreamWriter& Stm);
+};
