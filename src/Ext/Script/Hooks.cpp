a2e2b7fc33a5d59963ce7c6cd29233fd998e0a90
secsome <302702960@qq.com>
2021-06-12 23:39:17 +0800

    Possible fix buggy MoveToCell which uses 128 as leftovers

--- src/Ext/Script/Hooks.cpp
+++ src/Ext/Script/Hooks.cpp
@@ -9,3 +9,14 @@ DEFINE_HOOK(6E9443, TeamClass_AI, 8) {
 	
 	return 0;
 }
+
+DEFINE_HOOK(6E95B7, TeamClass_AI_MoveToCell, 8)
+{
+	GET(int, nCoord, ECX);
+	REF_STACK(CellStruct, cell, STACK_OFFS(0x38, 0x28));
+
+	cell.X = static_cast<short>(nCoord % 1000);
+	cell.Y = static_cast<short>(nCoord / 1000);
+
+	return 0x6E95CD;
+}
