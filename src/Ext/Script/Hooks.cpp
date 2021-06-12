a73d7d769fa94349ec8c90c190df7a7dfb10ce20
secsome <302702960@qq.com>
2021-06-13 01:22:04 +0800

    fix

--- src/Ext/Script/Hooks.cpp
+++ src/Ext/Script/Hooks.cpp
@@ -2,6 +2,8 @@
 
 #include "Body.h"
 
+#include <MapClass.h>
+
 DEFINE_HOOK(6E9443, TeamClass_AI, 8) {
 	GET(TeamClass *, pTeam, ESI);
 
@@ -10,13 +12,18 @@ DEFINE_HOOK(6E9443, TeamClass_AI, 8) {
 	return 0;
 }
 
-DEFINE_HOOK(6E95B7, TeamClass_AI_MoveToCell, 8)
+// Idea from E1Elite - secsome
+DEFINE_HOOK(6E95B3, TeamClass_AI_MoveToCell, 6)
 {
+	if (!R->BL())
+		return 0x6E95A4;
+
 	GET(int, nCoord, ECX);
 	REF_STACK(CellStruct, cell, STACK_OFFS(0x38, 0x28));
 
 	cell.X = static_cast<short>(nCoord % 1000);
 	cell.Y = static_cast<short>(nCoord / 1000);
 
-	return 0x6E95CD;
+	R->EAX(MapClass::Instance->GetCellAt(cell));
+	return 0x6E959C;
 }
