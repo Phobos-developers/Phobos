1e2d108533dfe46690a94ac67a320a2513b90b46
secsome <302702960@qq.com>
2021-06-23 21:43:08 +0800

    made it optional & buildup plays twice fix

--- src/Ext/TAction/Hooks.cpp
+++ src/Ext/TAction/Hooks.cpp
@@ -8,14 +8,19 @@
 // Bugfix : TAction 125 Build At do not display the buildups
 DEFINE_HOOK(6E427D, TActionClass_CreateBuildingAt, 9)
 {
+	GET(TActionClass*, pThis, ESI);
 	GET(BuildingTypeClass*, pBldType, ECX);
 	GET(HouseClass*, pHouse, EDI);
 	REF_STACK(CoordStruct, coord, STACK_OFFS(0x24, 0x18));
 
 	auto pBld = GameCreate<BuildingClass>(pBldType, pHouse);
 
-	pBld->QueueMission(Mission::Construction, true);
-	if (!pBld->Put(coord, Direction::North))
+	if (pThis->Bounds.X) // use this one for our flag : bPlayBuildUp
+		pBld->QueueMission(Mission::Construction, true);
+		
+	if (pBld->Put(coord, Direction::North))
+		pBld->IsReadyToCommence = true;
+	else 
 		pBld->UnInit();
 
 	return 0x6E42C1;
