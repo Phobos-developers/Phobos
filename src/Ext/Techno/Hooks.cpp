f646a0565f0eadb8ed81bf078d4d00285fda1351
Starkku <starkku.ppmod@gmail.com>
2021-07-07 19:07:03 +0300

    Implement weapon-specific burst delays.

--- src/Ext/Techno/Hooks.cpp
+++ src/Ext/Techno/Hooks.cpp
@@ -3,6 +3,7 @@
 #include "Body.h"
 
 #include <Ext/TechnoType/Body.h>
+#include <Ext/WeaponType/Body.h>
 
 DEFINE_HOOK(0x6F9E50, TechnoClass_AI, 0x5)
 {
@@ -55,6 +56,35 @@ DEFINE_HOOK(0x414057, TechnoClass_Init_InitialStrength, 0x6)       // AircraftCl
 		auto strength = pTypeExt->InitialStrength.Get(R->EDX<int>());
 		pThis->Health = strength;
 		pThis->EstimatedHealth = strength;
+	}
+
+	return 0;
+}
+
+// Issue #271: Separate burst delay for weapon type
+// Author: Starkku
+DEFINE_HOOK(0x6FD05E, TechnoClass_Rearm_Delay_BurstDelays, 0x7)
+{
+	GET(TechnoClass*, pThis, ESI);
+	GET(WeaponTypeClass*, pWeapon, EDI);
+
+	auto pWeaponExt = WeaponTypeExt::ExtMap.Find(pWeapon);
+
+	int burstDelay = -1;
+
+	if (pWeaponExt->Burst_Delays.size() >= (unsigned)pThis->CurrentBurstIndex)
+	{
+		burstDelay = pWeaponExt->Burst_Delays[pThis->CurrentBurstIndex - 1];
+	}
+	else if (pWeaponExt->Burst_Delays.size() > 0)
+	{
+		burstDelay = pWeaponExt->Burst_Delays[pWeaponExt->Burst_Delays.size() - 1];
+	}
+
+	if (burstDelay >= 0)
+	{
+		R->EAX(burstDelay);
+		return 0x6FD099;
 	}
 
 	return 0;
