f7bf4cabd364484db32fb5f27163e95889fe9194
Meta (X230T) <crabiter@vivaldi.net>
2021-07-31 00:28:48 +0300

    Add VSCode debugger configuration

--- scripts/configure_vscode_cpp.bat
+++ scripts/configure_vscode_cpp.bat
@@ -1,15 +1,21 @@
 @if not defined _echo echo off
 
-rem Copies and opens example config file for VSCode C++ extension.
+rem Copies and opens example config files for VSCode.
 
 rem Ensure we're in correct directory.
 cd /D "%~dp0"
 cd ..\.vscode\
 
 if exist c_cpp_properties.json (
-    echo Configuration file already present, exiting.
-    exit /b 0
+    echo C++ extension configuration file already present, skipping.
 ) else (
     copy c_cpp_properties.example.json c_cpp_properties.json
     code c_cpp_properties.json
+)
+
+if exist launch.json (
+    echo Debug configuration file already present, skipping.
+) else (
+    copy launch.example.json launch.json
+    code launch.json
 )
