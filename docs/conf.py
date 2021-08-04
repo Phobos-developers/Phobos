3b5d37b20c1fab243dd818e4def1442ff6ebdf16
FS-21 <firestorm21@gmail.com>
2021-08-04 16:13:45 +0200

    doc test 4

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = 'The Phobos Contributors'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_rtd_theme', 'myst_parser']
+extensions = ['sphinx_rtd_theme', 'myst_parser', 'sphinx_markdown_tables']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
