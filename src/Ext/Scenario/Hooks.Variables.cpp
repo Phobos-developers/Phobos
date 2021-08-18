af3226f824f4bbb638abc67956874283cbe86e7b
secsome <302702960@qq.com>
2021-08-18 22:38:34 +0800

    Reduce duplicate codes

--- src/Ext/Scenario/Hooks.Variables.cpp
+++ src/Ext/Scenario/Hooks.Variables.cpp
@@ -9,14 +9,7 @@ DEFINE_HOOK(0x689910, ScenarioClass_SetLocalToByID, 0x5)
 	GET_STACK(const int, nIndex, 0x4);
 	GET_STACK(const char, bState, 0x8);
 
-	auto itr = ScenarioExt::Global()->LocalVariables.find(nIndex);
-
-	if (itr != ScenarioExt::Global()->LocalVariables.end() && itr->second.Value != bState)
-	{
-		itr->second.Value = bState;
-		ScenarioClass::Instance->VariablesChanged = true;
-		TagClass::NotifyLocalChanged(nIndex);
-	}
+	ScenarioExt::Global()->SetVariableToByID(false, nIndex, bState);
 
 	return 0x689955;
 }
@@ -26,9 +19,7 @@ DEFINE_HOOK(0x689A00, ScenarioClass_GetLocalStateByID, 0x6)
 	GET_STACK(const int, nIndex, 0x4);
 	GET_STACK(char*, pOut, 0x8);
 
-	auto itr = ScenarioExt::Global()->LocalVariables.find(nIndex);
-	if (itr != ScenarioExt::Global()->LocalVariables.end())
-		*pOut = itr->second.Value;
+	ScenarioExt::Global()->GetVariableStateByID(false, nIndex, pOut);
 
 	return 0x689A26;
 }
@@ -37,22 +28,7 @@ DEFINE_HOOK(0x689B20, ScenarioClass_ReadLocalVariables, 0x6)
 {
 	GET_STACK(CCINIClass*, pINI, 0x4);
 
-	int nCount = pINI->GetKeyCount("VariableNames");
-	for (int i = 0; i < nCount; ++i)
-	{
-		auto pKey = pINI->GetKeyName("VariableNames", i);
-		int nIndex;
-		if (sscanf_s(pKey, "%d", &nIndex) == 1)
-		{
-			auto& var = ScenarioExt::Global()->LocalVariables[nIndex];
-			pINI->ReadString("VariableNames", pKey, pKey, Phobos::readBuffer);
-			strcpy(var.Name, strtok(Phobos::readBuffer, ","));
-			if (auto pState = strtok(nullptr, ","))
-				var.Value = atoi(pState) != 0;
-			else
-				var.Value = 0;
-		}
-	}
+	ScenarioExt::Global()->ReadVariables(false, pINI);
 
 	return 0x689C4B;
 }
@@ -62,14 +38,7 @@ DEFINE_HOOK(0x689670, ScenarioClass_SetGlobalToByID, 0x5)
 	GET_STACK(const int, nIndex, 0x4);
 	GET_STACK(const char, bState, 0x8);
 
-	auto itr = ScenarioExt::Global()->GlobalVariables.find(nIndex);
-
-	if (itr != ScenarioExt::Global()->GlobalVariables.end() && itr->second.Value != bState)
-	{
-		itr->second.Value = bState;
-		ScenarioClass::Instance->VariablesChanged = true;
-		TagClass::NotifyGlobalChanged(nIndex);
-	}
+	ScenarioExt::Global()->SetVariableToByID(true, nIndex, bState);
 
 	return 0x6896AF;
 }
@@ -79,9 +48,7 @@ DEFINE_HOOK(0x689760, ScenarioClass_GetGlobalStateByID, 0x6)
 	GET_STACK(const int, nIndex, 0x4);
 	GET_STACK(char*, pOut, 0x8);
 
-	auto itr = ScenarioExt::Global()->GlobalVariables.find(nIndex);
-	if (itr != ScenarioExt::Global()->GlobalVariables.end())
-		*pOut = itr->second.Value;
+	ScenarioExt::Global()->GetVariableStateByID(true, nIndex, pOut);
 
 	return 0x689786;
 }
@@ -90,22 +57,7 @@ DEFINE_HOOK(0x689880, ScenarioClass_ReadGlobalVariables, 0x6)
 {
 	GET_STACK(CCINIClass*, pINI, 0x4);
 
-	int nCount = pINI->GetKeyCount("VariableNames");
-	for (int i = 0; i < nCount; ++i)
-	{
-		auto pKey = pINI->GetKeyName("VariableNames", i);
-		int nIndex;
-		if (sscanf_s(pKey, "%d", &nIndex) == 1)
-		{
-			auto& var = ScenarioExt::Global()->GlobalVariables[nIndex];
-			pINI->ReadString("VariableNames", pKey, pKey, Phobos::readBuffer);
-			strcpy(var.Name, strtok(Phobos::readBuffer, ","));
-			if (auto pState = strtok(nullptr, ","))
-				var.Value = atoi(pState) != 0;
-			else
-				var.Value = 0;
-		}
-	}
+	ScenarioExt::Global()->ReadVariables(true, pINI);
 
 	return 0x6898FF;
 }
