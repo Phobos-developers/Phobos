6274c4ff79cbff175d12326ff508fe5c03fd5b2a
Otamaa <otamaindodapuq2@gmail.com>
2021-08-18 18:23:44 +0700

    Re-Write 0x65B580 to reduce hooks amount (#262)
    * Re-Write 0x65B580 to reduce hooks amount
    
    * Apply code suggestions and cleanup
    
    - Replace previous fix with new one 
    - Move function(s) outside ExtData
    - Invalid pointer handle for `RadHouse`
    
    * camelStyle , remove debug line
    
    * Fix array subscript out of bounds of CameoPriority
    
    * Fix order of 125 Action fix
    
    * update whats-new
    
    * Order fix x2
    
    * Fix Area Detonation with nullptr House
    
    * RadSite Hook(s) adjusment and fix
    
    * WWp set target not working if Cell is occupied fix
    
    Using little hack to stop Cloaking to prevent this , because `pInf->SetTarget(pCell)` is not working properly when Cell is occupied !
    
    Co-authored-by: Uranusian <mikesariitten@gmail.com>
    Co-authored-by: Kerbiter <crabiter@vivaldi.net>

--- src/Ext/RadSite/Body.h
+++ src/Ext/RadSite/Body.h
@@ -35,14 +35,14 @@ public:
 			return sizeof(*this);
 		}
 
-		virtual void InvalidatePointer(void* ptr, bool bRemoved) { }
+		virtual void InvalidatePointer(void* ptr, bool bRemoved) 
+		{
+			AnnounceInvalidPointer(RadHouse, ptr);
+		}
 
 		virtual void LoadFromStream(PhobosStreamReader& Stm) override;
 		virtual void SaveToStream(PhobosStreamWriter& Stm) override;
-
-		virtual void Add(int amount);
-		virtual void SetRadLevel(int amount);
-		virtual double GetRadLevelAt(CellStruct const& cell);
+		virtual void Initialize() override;
 
 	private:
 		template <typename T>
@@ -52,7 +52,10 @@ public:
 	static DynamicVectorClass<RadSiteExt::ExtData*> Array;
 
 	static void CreateInstance(CellStruct location, int spread, int amount, WeaponTypeExt::ExtData* pWeaponExt, HouseClass* const pOwner);
-	static void CreateInstance(CellStruct location, int spread, int amount, RadTypeClass* pType, HouseClass* const pOwner);
+	static void CreateLight(RadSiteClass* pThis);
+	static void Add(RadSiteClass* pThis,int amount);
+	static void SetRadLevel(RadSiteClass* pThis,int amount);
+	static const double GetRadLevelAt(RadSiteClass* pThis,CellStruct const& cell);
 	
 	class ExtContainer final : public Container<RadSiteExt>
 	{
