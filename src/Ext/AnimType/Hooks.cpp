5854ef01c3fb0ca7d5cc15da47406e55a60e0da9
MortonPL <bartm12@wp.pl>
2021-09-02 13:34:08 +0200

    Review changes
    Simplified the code

--- src/Ext/AnimType/Hooks.cpp
+++ src/Ext/AnimType/Hooks.cpp
@@ -7,10 +7,7 @@ DEFINE_HOOK(0x422CAB, AnimClass_DrawIt_XDrawOffset, 0x5)
 	GET(AnimClass* const, pThis, ECX);
 	GET_STACK(Point2D*, pCoord, STACK_OFFS(0x100, -0x4));
 	auto pThisTypeExt = AnimTypeExt::ExtMap.Find(pThis->Type);
-	
-	int offset = pThisTypeExt->XDrawOffset;
-	if (offset != 0)
-		pCoord->X += offset;
+	pCoord->X += pThisTypeExt->XDrawOffset;
 
 	return 0;
 }
