0648adab687bc93b58b6290a6cebcedd33f4e6c1
ADefaultUsername <GoodGame13241324@gmail.com>
2021-09-17 22:13:11 -0700

    works for sold buildings in addition to destroyed buildings

--- src/Ext/HouseType/Hooks.cpp
+++ src/Ext/HouseType/Hooks.cpp
@@ -6,11 +6,24 @@
 DEFINE_HOOK(0x4430BC, BuildingClass_RegisterDestruction_EjectCountryCrew, 0x6)
 {
 	GET(InfantryTypeClass*, pInfType, EAX);
-	GET(HouseClass*, pHouse, EDX);
-	auto pExt = HouseTypeExt::ExtMap.Find(pHouse->Type);
+	GET(BuildingClass*, pThis, EDI);
+	auto pExt = HouseTypeExt::ExtMap.Find(pThis->Owner->Type);
 
 	if (pExt->CountryCrew)
 		R->EAX(pExt->CountryCrew_Type.Get(pInfType));
 
 	return 0;
 }
+
+DEFINE_HOOK(0x44A645, BuildingClass_MissionDeconstruction_EjectCountryCrew, 0x6)
+{
+	GET(InfantryTypeClass*, pInfType, ESI);
+	GET(BuildingClass*, pThis, EBP);
+	auto pExt = HouseTypeExt::ExtMap.Find(pThis->Owner->Type);
+
+	if (pExt->CountryCrew)
+		R->ESI(pExt->CountryCrew_Type.Get(pInfType));
+
+	return 0;
+}
+
