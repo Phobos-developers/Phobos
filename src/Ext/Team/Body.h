24350bfce8421fbfe46f52886d05481f2a7d2310
FS-21 <firestorm21@gmail.com>
2021-09-24 06:29:22 +0200

    Import from PR 296 part 3

---
+++ src/Ext/Team/Body.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <TeamClass.h>
+
+#include <Helpers/Enumerators.h>
+#include <Helpers/Macro.h>
+#include <Utilities/Container.h>
+#include <Utilities/TemplateDef.h>
+#include <Phobos.h>
+
+class TeamExt
+{
+public:
+	using base_type = TeamClass;
+
+	class ExtData final : public Extension<TeamClass>
+	{
+	public:
+		int WaitNoTargetAttempts;
+		double NextSuccessWeightAward;
+		int IdxSelectedObjectFromAIList;
+		double CloseEnough;
+
+		ExtData(TeamClass* OwnerObject) : Extension<TeamClass>(OwnerObject),
+			WaitNoTargetAttempts(0),
+			NextSuccessWeightAward(0),
+			IdxSelectedObjectFromAIList(-1),
+			CloseEnough(-1)
+		{ }
+
+		virtual ~ExtData() = default;
+		virtual void InvalidatePointer(void* ptr, bool bRemoved) override {}
+		virtual void LoadFromStream(PhobosStreamReader& Stm) override;
+		virtual void SaveToStream(PhobosStreamWriter& Stm) override;
+
+	private:
+		template <typename T>
+		void Serialize(T& Stm);
+	};
+
+	class ExtContainer final : public Container<TeamExt>
+	{
+	public:
+		ExtContainer();
+		~ExtContainer();
+	};
+
+	static ExtContainer ExtMap;
+
+};
