bbb0ab04f08c7b3f5b14136afee6ecd68b0eff1f
Meta <crabiter@vivaldi.net>
2021-10-09 20:46:16 +0300

    Build number++

--- src/Phobos.version.h
+++ src/Phobos.version.h
@@ -23,7 +23,7 @@
 #pragma endregion
 
 // Build number. Incremented on each released build.
-#define BUILD_NUMBER 23
+#define BUILD_NUMBER 24
 
 // Nightly defines GIT_COMMIT and GIT_BRANCH in GH Actions
 
