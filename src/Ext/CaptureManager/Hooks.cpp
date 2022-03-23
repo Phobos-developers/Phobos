334a3e1266795263a6c4559b56be5e9d38eca071
Otamaa <otamaindodapuq2@gmail.com>
2022-03-24 02:54:33 +0700

    Update
    - Remove Unused Codes
    - Add safeguard to prevent crash for certain situation

--- src/Ext/CaptureManager/Hooks.cpp
+++ src/Ext/CaptureManager/Hooks.cpp
@@ -7,89 +7,7 @@
 #include <ScenarioClass.h>
 #include <ParticleSystemClass.h>
 
-/*
-DEFINE_HOOK(0x471A8D, CaptureManagerClass_Overload_Count_A, 0x6)
-{
-	GET(CaptureManagerClass*, pThis, ESI);
-	GET(int, nControlNodeCount, EDX);
-	R->ECX(CaptureExt::ExtMap.Find(pThis)->OverloadCount.Items);
-	return 0x471A93;
-}
-
-// replacing whole loop
-// using outside vector causing item-1 , which i dunno why
-DEFINE_HOOK(0x471A82, CaptureManagerClass_Overload_Count,0x6)
-{
-	GET(CaptureManagerClass*, pThis, ESI);
-
-	auto const &OverloadCount = CaptureExt::ExtMap.Find(pThis)->OverloadCount;
-	int nCurIdx = 0;
-	int nOvcIdx = 1;
-
-	if (pThis->ControlNodes.Count > OverloadCount[0])
-	{
-		do
-		{
-			if (nCurIdx >= (OverloadCount.Count - 1) || nOvcIdx >= (OverloadCount.Count - 1))
-				break;
-
-			++nCurIdx;
-		} while (pThis->ControlNodes.Count > OverloadCount[nOvcIdx++]);
-	}
-
-	R->EAX(nCurIdx);
-	return 0x471AB1;
-}
-
-
-DEFINE_HOOK(0x471A9B, CeptureManagerClass_Overload_Count_B, 0x6)
-{
-	GET(CaptureManagerClass*, pThis, ESI);
-	R->EDI((CaptureExt::ExtMap.Find(pThis)->OverloadCount.Count - 1));
-	return 0x471AA2;
-}
-
-DEFINE_HOOK(0x471AB5, CeptureManagerClass_Overload_Frames, 0x6)
-{
-	GET(RulesClass*, pRules, EBX);
-	GET(CaptureManagerClass*, pThis, ESI);
-	GET(int, nIdx, EAX);
-	auto const pManagerOwnerExt = TechnoTypeExt::ExtMap.Find(pThis->Owner->GetTechnoType());
-	R->EDX(pManagerOwnerExt->Overload_Frames.GetElements(pRules->OverloadFrames)[nIdx]);
-	return 0x471ABE;
-}
-
-DEFINE_HOOK(0x471AC7, CeptureManagerClass_Overload_Damage, 0x6)
-{
-	GET(RulesClass*, pRules, ECX);
-	GET(CaptureManagerClass*, pThis, ESI);
-	GET(int, nIdx, EAX);
-	auto const pManagerOwnerExt = TechnoTypeExt::ExtMap.Find(pThis->Owner->GetTechnoType());
-	R->EAX(pManagerOwnerExt->Overload_Damage.GetElements(pRules->OverloadDamage)[nIdx]);
-	return 0x471AD0;
-}
-
-DEFINE_HOOK(0x471B39, CaptureManagerClass_Overload_DeathSound, 0x6)
-{
-	GET(RulesClass*, pRules, ECX);
-	GET(CaptureManagerClass*, pThis, ESI);
-	auto const pManagerOwnerExt = TechnoTypeExt::ExtMap.Find(pThis->Owner->GetTechnoType());
-	auto const nSound = pManagerOwnerExt->Overload_DeathSound.Get(pRules->MasterMindOverloadDeathSound);
-	R->ECX(nSound);
-	return 0x471B3F;
-}
-
-DEFINE_HOOK(0x471C0C, CaptureManagerClass_Overload_ParticleSys, 0x6)
-{
-	GET(RulesClass*, pRules, EDX);
-	GET(CaptureManagerClass*, pThis, ESI);
-	auto const pExt = TechnoTypeExt::ExtMap.Find(pThis->Owner->GetTechnoType());
-	R->ECX(pExt->Overload_ParticleSys.Get(pRules->DefaultSparkSystem));
-	return 0x471C12;
-}*/
-
-static void __stdcall DrawALinkTo(CoordStruct nFrom, CoordStruct nTo, ColorStruct color)
-{
+static void __stdcall DrawALinkTo(CoordStruct nFrom, CoordStruct nTo, ColorStruct color) {
 	JMP_STD(0x704E40);
 }
 
@@ -114,14 +32,20 @@ DEFINE_HOOK(0x4721E6, CaptureManagerClass_DrawLinkToVictim, 0x6)
 	return 0x472287;
 }
 
-
-void __fastcall CaptureManagerClass_Overload_AI(CaptureManagerClass *pThis , void*_)
+void __fastcall CaptureManagerClass_Overload_AI(CaptureManagerClass* pThis, void* _)
 {
+
+	auto const pOwner = pThis->Owner;
+	auto const pOwnerTypeExt = TechnoTypeExt::ExtMap.Find(pOwner->GetTechnoType());
+
+	if (!pOwnerTypeExt) // we cant find type Ext for this , just return to original function !
+	{
+		pThis->HandleOverload();
+		return;
+	}
 
 	if (pThis->InfiniteMindControl)
 	{
-		auto const pOwner = pThis->Owner;
-		auto const pOwnerTypeExt = TechnoTypeExt::ExtMap.Find(pOwner->GetTechnoType());
 		auto const pRules = RulesClass::Instance();
 
 		if (pThis->OverloadPipState > 0)
@@ -129,23 +53,39 @@ void __fastcall CaptureManagerClass_Overload_AI(CaptureManagerClass *pThis , voi
 
 		if (pThis->OverloadDamageDelay <= 0)
 		{
-			auto const &OverloadCount = pOwnerTypeExt->Overload_Count.GetElements(pRules->OverloadCount);
+			auto const& OverloadCount = pOwnerTypeExt->Overload_Count.GetElements(pRules->OverloadCount);
+
+			if (OverloadCount.empty())
+				return;
+
 			int nCurIdx = 0;
 			int const nNodeCount = pThis->ControlNodes.Count;
 
 			if (nNodeCount > OverloadCount[0])
 			{
-				for (int i = 1; nNodeCount > OverloadCount[i]; ++i)
+				if (OverloadCount.size() > 1) //check if the Overload size actually more than one
 				{
-					if (nCurIdx >= (int)(OverloadCount.size() - 1) || i >= (int)(OverloadCount.size() - 1))
-						break;
+					for (int i = 1; nNodeCount > OverloadCount[i]; ++i)
+					{
+						if (nCurIdx >= (int)(OverloadCount.size() - 1) || i >= (int)(OverloadCount.size() - 1))
+							break;
 
-					++nCurIdx;
+						++nCurIdx;
+					}
 				}
 			}
 
-			pThis->OverloadDamageDelay = pOwnerTypeExt->Overload_Frames.GetElements(pRules->OverloadFrames)[nCurIdx];
-			auto nDamage = pOwnerTypeExt->Overload_Damage.GetElements(pRules->OverloadDamage)[nCurIdx];
+			// nCuridx is following Overload .size , so it may causing problem if other vector .size not match Overload .size
+			// fixed it !
+			constexpr auto FixIdx = [](const Iterator<int>& iter, int nInput) {
+				return iter.empty() ? 0 : iter[nInput > (int)iter.size() ? (int)iter.size() : nInput];
+			};
+
+			auto const& nOverloadfr = pOwnerTypeExt->Overload_Frames.GetElements(pRules->OverloadFrames);
+			pThis->OverloadDamageDelay = FixIdx(nOverloadfr, nCurIdx);
+
+			auto const& nOverloadDmg = pOwnerTypeExt->Overload_Damage.GetElements(pRules->OverloadDamage);
+			auto nDamage = FixIdx(nOverloadDmg, nCurIdx);
 
 			if (nDamage <= 0)
 			{
@@ -162,17 +102,20 @@ void __fastcall CaptureManagerClass_Overload_AI(CaptureManagerClass *pThis , voi
 					pThis->OverloadDeathSoundPlayed = true;
 				}
 
-				for (int i = pOwnerTypeExt->Overload_ParticleSysCount.Get(5); i > 0; --i)
+				if (auto const pParticle = pOwnerTypeExt->Overload_ParticleSys.Get(pRules->DefaultSparkSystem))
 				{
-					auto const nRandomY = ScenarioClass::Instance->Random.RandomRanged(-200, 200);
-					auto const nRamdomX = ScenarioClass::Instance->Random.RandomRanged(-200, 200);
-					CoordStruct nParticleCoord{ pOwner->Location.X + nRamdomX, nRandomY + pOwner->Location.Y, pOwner->Location.Z + 100 };
-					GameCreate<ParticleSystemClass>(pOwnerTypeExt->Overload_ParticleSys.Get(pRules->DefaultSparkSystem), nParticleCoord, nullptr, nullptr, CoordStruct::Empty, nullptr);
+					for (int i = pOwnerTypeExt->Overload_ParticleSysCount.Get(5); i > 0; --i)
+					{
+						auto const nRandomY = ScenarioClass::Instance->Random.RandomRanged(-200, 200);
+						auto const nRamdomX = ScenarioClass::Instance->Random.RandomRanged(-200, 200);
+						CoordStruct nParticleCoord{ pOwner->Location.X + nRamdomX, nRandomY + pOwner->Location.Y, pOwner->Location.Z + 100 };
+						GameCreate<ParticleSystemClass>(pParticle, nParticleCoord, nullptr, nullptr, CoordStruct::Empty, nullptr);
+					}
 				}
 
 				if (nCurIdx > 0 && pOwner->IsAlive)
 				{
-					double const nBase = (nCurIdx != 1) ?  0.015 : 0.029999999;
+					double const nBase = (nCurIdx != 1) ? 0.015 : 0.029999999;
 					double const nCopied_base = (ScenarioClass::Instance->Random.RandomRanged(0, 100) < 50) ? -nBase : nBase;
 					pOwner->RockingSidewaysPerFrame = (float)nCopied_base;
 				}
