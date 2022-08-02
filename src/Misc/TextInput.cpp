14a998ac0f9c4f4477bee5f477db2c045a471825
Belonit <Belonit@ya.ru>
2022-08-02 16:49:43 +0300

    Set Сorrect hook size

--- src/Misc/TextInput.cpp
+++ src/Misc/TextInput.cpp
@@ -23,7 +23,7 @@ wchar_t LocalizeCharacter(char character)
 	return result;
 }
 
-DEFINE_HOOK(0x5D46C7, MessageListClass_Input, 5)
+DEFINE_HOOK(0x5D46C7, MessageListClass_Input, 0x5)
 {
 	if (!IMEBuffer[0])
 		R->EBX<wchar_t>(LocalizeCharacter(R->EBX<char>()));
@@ -31,7 +31,7 @@ DEFINE_HOOK(0x5D46C7, MessageListClass_Input, 5)
 	return 0;
 }
 
-DEFINE_HOOK(0x61510E, WWUI_NewEditCtrl, 0)
+DEFINE_HOOK(0x61510E, WWUI_NewEditCtrl, 0x7)
 {
 	R->EDI<wchar_t>(LocalizeCharacter(R->EBX<char>()));
 	return 0x615226;
@@ -42,7 +42,7 @@ DEFINE_HOOK(0x61510E, WWUI_NewEditCtrl, 0)
 HIMC& IMEContext = *reinterpret_cast<HIMC*>(0xB7355C);
 wchar_t* IMECompositionString = reinterpret_cast<wchar_t*>(0xB73318);
 
-DEFINE_HOOK(0x777F15, IMEUpdateCompositionString, 7)
+DEFINE_HOOK(0x777F15, IMEUpdateCompositionString, 0x7)
 {
 	IMECompositionString[0] = 0;
 	ImmGetCompositionStringW(IMEContext, GCS_COMPSTR, IMECompositionString, 256);
