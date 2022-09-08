c7d89016c9d89f03a442e181304a82fd6f850835
ststl <alskdjfh2018@qq.com>
2022-09-08 19:43:03 +0800

    CE

--- src/Misc/TriggerMPOwner.h
+++ src/Misc/TriggerMPOwner.h
@@ -9,19 +9,7 @@ class TriggerMPOwner
 public:
 	static std::map<int, int> TriggerType_Owner;
 
-	static bool LoadGlobals(PhobosStreamReader& stm)
-	{
-		return stm
-			.Process(TriggerType_Owner)
-			.Success();
-	}
+	static bool LoadGlobals(PhobosStreamReader& stm);
 
-	static bool SaveGlobals(PhobosStreamWriter& stm)
-	{
-		return stm
-			.Process(TriggerType_Owner)
-			.Success();
-	}
+	static bool SaveGlobals(PhobosStreamWriter& stm);
 };
-
-std::map<int, int> TriggerMPOwner::TriggerType_Owner;
