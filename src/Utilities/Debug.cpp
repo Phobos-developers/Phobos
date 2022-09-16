1b834bef44ca27a569627bad274b4414b73f9bcb
secsome <302702960@qq.com>
2022-09-16 15:01:09 +0800

    Add missing NULL check

--- src/Utilities/Debug.cpp
+++ src/Utilities/Debug.cpp
@@ -130,6 +130,9 @@ void Console::SetBackColor(ConsoleColor color)
 
 void Console::EnableUnderscore(bool enable)
 {
+	if (NULL == ConsoleHandle)
+		return;
+
 	if (TextAttribute.Underscore == enable)
 		return;
 
