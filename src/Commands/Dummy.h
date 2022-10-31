68638d430bcd1b0a194e266036e61bfd2b2cbb3a
Trsdy <914137150@qq.com>
2022-10-31 21:33:05 +0800

    restore Dummy.h

---
+++ src/Commands/Dummy.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Commands.h"
+
+// The example command class
+class DummyCommandClass : public PhobosCommandClass
+{
+public:
+	// CommandClass
+	virtual const char* GetName() const override;
+	virtual const wchar_t* GetUIName() const override;
+	virtual const wchar_t* GetUICategory() const override;
+	virtual const wchar_t* GetUIDescription() const override;
+	virtual void Execute(WWKey eInput) const override;
+};
