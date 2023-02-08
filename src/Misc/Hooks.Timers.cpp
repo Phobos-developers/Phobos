1939ca83007442fd4a7ae7f8bf273859973c96ed
MortonPL <bartm12@wp.pl>
2023-02-08 23:22:22 +0100

    Make tooltips and SW timers tell real time

--- src/Misc/Hooks.Timers.cpp
+++ src/Misc/Hooks.Timers.cpp
@@ -1,12 +1,25 @@
 #include <Utilities/Macro.h>
 #include <GameOptionsClass.h>
+#include <FPSCounter.h>
+#include <SessionClass.h>
 
-DEFINE_HOOK(0x6D4B50, Print_Timer_On_Tactical, 0x6)
+int oldValue;
+DEFINE_HOOK(0x6D4B50, Print_Timer_On_Tactical_Start, 0x6)
 {
-	REF_STACK(int, value, STACK_OFFS(0, 0x8));
+	REF_STACK(int, value, STACK_OFFSET(0, 0x4));
+	oldValue = value;
+
+	if (false && !SessionClass::IsMultiplayer()) // TODO change when custom game speed gets merged
+	{
+		value = std::max(value / (int)(std::max((double)FPSCounter::CurrentFrameRate, 1.0) / 15.0), 1);
+		return 0;
+	}
+
 	switch (GameOptionsClass::Instance->GameSpeed)
 	{
 	case 0: // no cap
+		value = (int)((double)value / (std::max((double)FPSCounter::CurrentFrameRate, 1.0) / 15.0));
+		break;
 	case 1:	// 60 FPS
 		value = value / 4;
 		break;
@@ -27,4 +40,13 @@ DEFINE_HOOK(0x6D4B50, Print_Timer_On_Tactical, 0x6)
 	default:
 		break;
 	}
+
+	return 0;
+}
+
+DEFINE_HOOK(0x6D4C68, Print_Timer_On_Tactical_End, 0x8)
+{
+	REF_STACK(int, value, STACK_OFFSET(0x654, 0x4));
+	value = oldValue;
+	return 0;
 }
