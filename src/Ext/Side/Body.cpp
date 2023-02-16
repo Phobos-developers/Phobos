eb7cfd8d6d5baadd00324a8058ced1994a5650b1
Starkku <starkku.ppmod@gmail.com>
2023-02-16 20:37:32 +0200

    Fix side INI parsing

--- src/Ext/Side/Body.cpp
+++ src/Ext/Side/Body.cpp
@@ -162,7 +162,7 @@ DEFINE_HOOK(0x679A10, SideClass_LoadAllFromINI, 0x5)
 	GET_STACK(CCINIClass*, pINI, 0x4);
 
 	for (auto pSide : *SideClass::Array)
-		SideExt::ExtMap.Find(pSide)->LoadFromINIFile(pINI);
+		SideExt::ExtMap.Find(pSide)->LoadFromINI(pINI);
 
 	return 0;
 }
