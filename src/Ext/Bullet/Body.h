de451204b793f200a863f1a1d55915e26d53132c
Starkku <starkku.ppmod@gmail.com>
2023-03-15 17:30:02 +0200

    Fix error

--- src/Ext/Bullet/Body.h
+++ src/Ext/Bullet/Body.h
@@ -14,7 +14,7 @@ public:
 	using base_type = BulletClass;
 
 	static constexpr DWORD Canary = 0x2A2A2A2A;
-	static constexpr size_t ExtPointerOffset = 0xE4;
+	static constexpr size_t ExtPointerOffset = 0x18;
 
 	class ExtData final : public Extension<BulletClass>
 	{
