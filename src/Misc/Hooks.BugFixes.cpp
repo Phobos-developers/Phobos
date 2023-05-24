8d329580f788c0e6029e50057732b6f665665490
Starkku <Starkku@users.noreply.github.com>
2023-05-24 23:01:29 +0300

    Disable AnimType/BuildingTypeClass first INI parsing pass (#856)

--- src/Misc/Hooks.BugFixes.cpp
+++ src/Misc/Hooks.BugFixes.cpp
@@ -691,3 +691,6 @@ DEFINE_HOOK(0x451033, BuildingClass_AnimationAI_SuperAnim, 0x6)
 
 	return 0;
 }
+
+// Stops INI parsing for Anim/BuildingTypeClass on game startup, will only be read on scenario load later like everything else.
+DEFINE_JUMP(LJMP, 0x52C9C4, 0x52CA37);
