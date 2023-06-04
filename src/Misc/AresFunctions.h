2f3204184c0ea4d79359e033f89563cd2979c0e8
MortonPL <bartm12@wp.pl>
2023-06-04 14:41:34 +0200

    Use static class instead of namespace

--- src/Misc/AresFunctions.h
+++ src/Misc/AresFunctions.h
@@ -11,35 +11,36 @@ class BuildingTypeClass;
 
 #define ARES_FUN(name) std::string(NAMEOF(AresFunctions::name))
 
-namespace AresFunctions
+class AresFunctions
 {
+public:
 #define JMP_ARES(name) const auto x = AresData::AresFunctionOffsetsFinal[ARES_FUN(name)]; JMP(x)
 #define JMP_THIS_ARES(name) const auto x = AresData::AresFunctionOffsetsFinal[ARES_FUN(name)]; JMP_THIS(x)
 
 	// ???
-	bool ConvertTypeTo(TechnoClass* pFoot, TechnoTypeClass* pConvertTo)
+	static bool ConvertTypeTo(TechnoClass* pFoot, TechnoTypeClass* pConvertTo)
 	{
 		JMP_ARES(ConvertTypeTo);
 	}
 
 	// TechnoExt
-	void SpawnSurvivors(FootClass* const pThis, TechnoClass* const pKiller, const bool Select, const bool IgnoreDefenses)
+	static void SpawnSurvivors(FootClass* const pThis, TechnoClass* const pKiller, const bool Select, const bool IgnoreDefenses)
 	{
 		JMP_ARES(SpawnSurvivors);
 	}
 
 	// HouseExt
-	int HasFactory(int buffer, HouseClass* pOwner, TechnoTypeClass* pType, bool skipAircraft, bool requirePower, bool checkCanBuild, bool unknown)
+	static int HasFactory(int buffer, HouseClass* pOwner, TechnoTypeClass* pType, bool skipAircraft, bool requirePower, bool checkCanBuild, bool unknown)
 	{
 		JMP_ARES(HasFactory);
 	}
 
 	// TechnoTypeExt
-	bool CanBeBuiltAt(DWORD pTechnoTypeExt, BuildingTypeClass* pBuildingType)
+	static bool CanBeBuiltAt(DWORD pTechnoTypeExt, BuildingTypeClass* pBuildingType)
 	{
 		JMP_THIS_ARES(CanBeBuiltAt);
 	}
 
 #undef JMP_ARES
 #undef JMP_THIS_ARES
-}
+};
