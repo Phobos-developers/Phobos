5091d3a2a16d145c5f237abb876bd28773fdfacd
MortonPL <bartm12@wp.pl>
2023-06-07 00:19:02 +0200

    Fix/improve Ares calling

--- src/Utilities/AresFunctions.h
+++ src/Utilities/AresFunctions.h
@@ -3,6 +3,7 @@
 
 #include <nameof/nameof.h>
 #include <ASMMacros.h>
+#include <functional>
 
 class TechnoClass;
 class TechnoTypeClass;
@@ -10,36 +11,43 @@ class FootClass;
 class HouseClass;
 class BuildingTypeClass;
 
+class AresTechnoTypeExt
+{
+public:
+	__declspec(noinline) bool __thiscall CanBeBuiltAt(BuildingTypeClass* pBuildingType) { return 0; }
+};
+
+
 class AresFunctions
 {
 public:
-#define JMP_ARES(name) const auto x = AresHelper::AresFunctionOffsetsFinal[ARES_FUN(name)]; JMP(x)
-#define JMP_THIS_ARES(name) const auto x = AresHelper::AresFunctionOffsetsFinal[ARES_FUN(name)]; JMP_THIS(x)
+#define CALL_ARES(name, ...) std::invoke(reinterpret_cast<decltype(name)*>(AresHelper::AresFunctionOffsetsFinal[ARES_FUN(name)]), __VA_ARGS__)
+#define THIS_CALL_ARES(name, ...) std::invoke(*reinterpret_cast<decltype(name)*>(AresHelper::AresFunctionOffsetsFinal[ARES_FUN_M(name)]), __VA_ARGS__)
 
 	// ???
 	static bool ConvertTypeTo(TechnoClass* pFoot, TechnoTypeClass* pConvertTo)
 	{
-		JMP_ARES(ConvertTypeTo);
+		return CALL_ARES(ConvertTypeTo, pFoot, pConvertTo);
 	}
 
 	// TechnoExt
 	static void SpawnSurvivors(FootClass* const pThis, TechnoClass* const pKiller, const bool Select, const bool IgnoreDefenses)
 	{
-		JMP_ARES(SpawnSurvivors);
+		CALL_ARES(SpawnSurvivors, pThis, pKiller, Select, IgnoreDefenses);
 	}
 
 	// HouseExt
 	static int HasFactory(int buffer, HouseClass* pOwner, TechnoTypeClass* pType, bool skipAircraft, bool requirePower, bool checkCanBuild, bool unknown)
 	{
-		JMP_ARES(HasFactory);
+		return CALL_ARES(HasFactory, buffer, pOwner, pType, skipAircraft, requirePower, checkCanBuild, unknown);
 	}
 
 	// TechnoTypeExt
-	static bool CanBeBuiltAt(DWORD pTechnoTypeExt, BuildingTypeClass* pBuildingType)
+	static bool CanBeBuiltAt(AresTechnoTypeExt* pExt, BuildingTypeClass* pBuildingType)
 	{
-		JMP_THIS_ARES(CanBeBuiltAt);
+		return THIS_CALL_ARES(&AresTechnoTypeExt::CanBeBuiltAt, pExt, pBuildingType);
 	}
 
-#undef JMP_ARES
-#undef JMP_THIS_ARES
+#undef CALL_ARES
+#undef THIS_CALL_ARES
 };
