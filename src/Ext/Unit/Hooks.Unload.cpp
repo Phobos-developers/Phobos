608763668560eed848b08cdb1ea97edd19bb9beb
Fryone <epsilon.fryone@gmail.com>
2023-07-09 11:54:36 +0300

    Added VoiceCantDeploy sound
    Added "VoiceCantDeploy" sound entry when unit is unable deploy.

--- src/Ext/Unit/Hooks.Unload.cpp
+++ src/Ext/Unit/Hooks.Unload.cpp
@@ -1,9 +1,27 @@
 #include <UnitClass.h>
+#include <GameStrings.h>
 
 #include <Ext/TechnoType/Body.h>
 #include <Utilities/EnumFunctions.h>
 #include <Utilities/GeneralUtils.h>
 #include <Utilities/Macro.h>
+
+DEFINE_HOOK(0x7394FF, UnitClass_TryToDeploy_CantDeployVoice, 0x10)
+{
+	GET(UnitClass* const, pThis, EBP);
+
+	auto const pThisTechno = TechnoTypeExt::ExtMap.Find(pThis->GetTechnoType());
+
+	if(pThis->Owner->IsControlledByCurrentPlayer())
+	{
+		if(pThisTechno->VoiceCantDeploy.isset())
+			VocClass::PlayGlobal(pThisTechno->VoiceCantDeploy, 0x2000, 1.0);
+		else
+			VoxClass::Play(GameStrings::EVA_CannotDeployHere);
+	}
+
+	return 0x73950F;
+}
 
 DEFINE_HOOK(0x739AC6, UnitClass_ToggleDeployState_IgnoreDeploying, 0x6)
 {
@@ -15,8 +33,8 @@ DEFINE_HOOK(0x739AC6, UnitClass_ToggleDeployState_IgnoreDeploying, 0x6)
 	auto const pThisTechno = TechnoTypeExt::ExtMap.Find(pThis->GetTechnoType());
 	if(pThisTechno->OnAmmoDepletion_DeployUnlockAmount != 0 && pThis->Ammo < pThisTechno->OnAmmoDepletion_DeployUnlockAmount)
 	{
-		if(pThisTechno->VoiceCantDeploy)
-			VocClass::PlayIndexAtPos(pThisTechno->VoiceCantDeploy, pThis->Location);
+		if(pThisTechno->VoiceCantDeploy.isset() && pThis->Owner->IsControlledByCurrentPlayer())
+			VocClass::PlayGlobal(pThisTechno->VoiceCantDeploy, 0x2000, 1.0);
 		return Ignore;
 	}
 
@@ -34,8 +52,8 @@ DEFINE_HOOK(0x739CD7, UnitClass_ToggleSimpleDeploy_IgnoreDeploying, 0x6)
 	auto const pThisTechno = TechnoTypeExt::ExtMap.Find(pThis->GetTechnoType());
 	if(pThisTechno->OnAmmoDepletion_DeployUnlockAmount != 0 && pThis->Ammo < pThisTechno->OnAmmoDepletion_DeployUnlockAmount)
 	{
-		if(pThisTechno->VoiceCantDeploy)
-			VocClass::PlayIndexAtPos(pThisTechno->VoiceCantDeploy, pThis->Location);
+		if(pThisTechno->VoiceCantDeploy.isset() && pThis->Owner->IsControlledByCurrentPlayer())
+			VocClass::PlayGlobal(pThisTechno->VoiceCantDeploy, 0x2000, 1.0);
 		return Ignore;
 	}
 
