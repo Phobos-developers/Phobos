3e099c8bf33ff9ba01cc804570e5cad333a2bc33
Fryone <epsilon.fryone@gmail.com>
2023-07-15 00:03:28 +0300

    Update Hooks.Unload.cpp
    Fixed Deploying on hotkey

--- src/Ext/Unit/Hooks.Unload.cpp
+++ src/Ext/Unit/Hooks.Unload.cpp
@@ -22,3 +22,20 @@ DEFINE_HOOK(0x73FFE6, UnitClass_WhatAction_RemoveDeploying, 0xA)
 
 	return Continue;
 }
+
+DEFINE_HOOK(0x730C70, DeployClass_Execute_RemoveDeploying, 0xA)
+{
+	enum { Continue = 0x730C7A };
+
+	GET(TechnoClass*, pThis, ESI);
+	auto const pThisType = TechnoTypeExt::ExtMap.Find(pThis->GetTechnoType());
+
+	R->AL(pThis->CanDeploySlashUnload());
+	if(pThis->CanDeploySlashUnload())
+	{
+		if(pThisType->OnAmmoDepletion_DeployUnlockAmount != 0 && pThis->Ammo < pThisType->OnAmmoDepletion_DeployUnlockAmount)
+			R->AL(false);
+	}
+
+	return Continue;
+}
