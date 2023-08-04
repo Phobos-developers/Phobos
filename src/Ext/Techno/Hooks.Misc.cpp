3637a0f1a4330385ba84948ce1a96e8a3cfd362e
Belonit <54427022+Belonit@users.noreply.github.com>
2023-08-05 00:21:57 +0300

    Fix code style

--- src/Ext/Techno/Hooks.Misc.cpp
+++ src/Ext/Techno/Hooks.Misc.cpp
@@ -76,24 +76,24 @@ DEFINE_HOOK(0x730D1F, DeployCommandClass_Execute_VoiceDeploy, 0x5)
 {
 	GET(TechnoClass* const, pThis, ESI);
 	GET_STACK(int, unitsToDeploy, STACK_OFFSET(0x18, -0x4));
-	REF_STACK(char, endDeploy, STACK_OFFSET(0x18, -0x5));
 
-	if ((pThis->WhatAmI() == AbstractType::Infantry) && unitsToDeploy == 1)
+	const int voiceDeploy = pThis->GetTechnoType()->VoiceDeploy;
+
+	if (unitsToDeploy == 1 && voiceDeploy && pThis->IsOwnedByCurrentPlayer)
 	{
-		auto pInfantry = abstract_cast<InfantryClass*>(pThis);
-		if(pInfantry->GetTechnoType()->VoiceDeploy && pInfantry->IsOwnedByCurrentPlayer && pInfantry->CanDeployNow())
+		if (AbstractType::Infantry == pThis->WhatAmI())
 		{
-			pInfantry->QueueVoice(pInfantry->GetTechnoType()->VoiceDeploy);
+			auto pInfantry = abstract_cast<InfantryClass*>(pThis);
+			if (pInfantry->CanDeployNow())
+				pThis->QueueVoice(voiceDeploy);
+		}
+		else if (AbstractType::Unit == pThis->WhatAmI())
+		{
+			auto pUnit = abstract_cast<UnitClass*>(pThis);
+			if (pUnit->TryToDeploy() || pUnit->Type->IsSimpleDeployer)
+				pThis->QueueVoice(voiceDeploy);
 		}
 	}
-	else if ((pThis->WhatAmI() == AbstractType::Unit) && unitsToDeploy == 1)
-	{
-		auto pUnit = abstract_cast<UnitClass*>(pThis);
-		if(pUnit->GetTechnoType()->VoiceDeploy && pUnit->IsOwnedByCurrentPlayer && (pUnit->TryToDeploy() || pUnit->Type->IsSimpleDeployer))
-		{
-			pUnit->QueueVoice(pUnit->GetTechnoType()->VoiceDeploy);
-		}
-	}
-	endDeploy = 1;
-	return 0x730D24;
+
+	return 0;
 }
