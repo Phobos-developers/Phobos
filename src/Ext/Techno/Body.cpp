a8a1637efba437d9162d6e3a1e28c8aa6a9745ec
Starkku <starkku.ppmod@gmail.com>
2023-08-31 20:20:50 +0300

    Fix ROFMultiplier & save/load

--- src/Ext/Techno/Body.cpp
+++ src/Ext/Techno/Body.cpp
@@ -421,6 +421,13 @@ void TechnoExt::ExtData::Serialize(T& Stm)
 		.Process(this->DeployFireTimer)
 		.Process(this->ForceFullRearmDelay)
 		.Process(this->WHAnimRemainingCreationInterval)
+		.Process(this->AttachedEffects)
+		.Process(this->AE_FirepowerMultiplier)
+		.Process(this->AE_ArmorMultiplier)
+		.Process(this->AE_SpeedMultiplier)
+		.Process(this->AE_ROFMultiplier)
+		.Process(this->AE_Cloakable)
+		.Process(this->AE_ForceDecloak)
 		;
 }
 
