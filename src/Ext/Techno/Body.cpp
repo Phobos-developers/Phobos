da32f5871236ddd66e2c4e7a6bdf8dc1056a7dfb
FS-21 <firestorm21@gmail.com>
2023-10-07 18:00:00 +0200

    tweak

--- src/Ext/Techno/Body.cpp
+++ src/Ext/Techno/Body.cpp
@@ -794,8 +794,6 @@ TechnoClass* TechnoExt::GetRandomTarget(TechnoClass* pThis)
 	// Looking for all valid targeting candidates
 	for (auto pCandidate : *TechnoClass::Array)
 	{
-		auto debugType = pCandidate->GetTechnoType();
-
 		if (pCandidate == pThis
 			|| !ScriptExt::IsUnitAvailable(pCandidate, true)
 			|| pThisType->Immune
