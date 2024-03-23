26f1b8a5e81c86c4ea5a8f92445283d13e09c52f
Otamaa <otamaindodapuq2@gmail.com>
2024-03-23 16:32:07 +0700

    - Fix crash that happen when `Unit` using `DroppodLocomotor` because of `DroppodType` is exclusive to `Infantry` - Fix crash when `TypeConvertGroup::Convert` is called with `nullptr` Owner and get checked with `EnumFunctions::CanTargetHouse`

--- src/Ext/TechnoType/Body.cpp
+++ src/Ext/TechnoType/Body.cpp
@@ -407,16 +407,10 @@ void TechnoTypeExt::ExtData::LoadFromINIFile(CCINIClass* const pINI)
 		this->InterceptorType.reset();
 	}
 
-	if (this->OwnerObject()->WhatAmI() == AbstractType::InfantryType)
-	{
-		if (this->DroppodType == nullptr)
-			this->DroppodType = std::make_unique<DroppodTypeClass>();
-		this->DroppodType->LoadFromINI(pINI, pSection);
-	}
-	else
-	{
-		this->DroppodType.reset();
-	}
+	if (this->DroppodType == nullptr)
+		this->DroppodType = std::make_unique<DroppodTypeClass>();
+
+	this->DroppodType->LoadFromINI(pINI, pSection);
 }
 
 template <typename T>
