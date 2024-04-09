9d0a9b25e7f274c6d55b07431c882251fc83d436
Coronia <2217891145@qq.com>
2024-04-09 08:58:35 +0800

    update count on build

--- src/Ext/House/Hooks.BuildLimit.cpp
+++ src/Ext/House/Hooks.BuildLimit.cpp
@@ -1,10 +1,31 @@
+#include <FactoryClass.h>
 #include <HouseClass.h>
 
 #include <Utilities/Macro.h>
 
 #include <Ext/TechnoType/Body.h>
 
-inline bool __fastcall ReachedBuildLimit(const HouseClass* pHouse, const TechnoTypeClass* pType)
+inline int __fastcall QueuedNum(const HouseClass* pHouse, const TechnoTypeClass* pType)
+{
+	const AbstractType absType = pType->WhatAmI();
+	const FactoryClass* pFactory = pHouse->GetPrimaryFactory(absType, pType->Naval, BuildCat::DontCare);
+	int queued = 0;
+
+	if (pFactory)
+	{
+		queued = pFactory->CountTotal(pType);
+
+		if (const auto pObject = pFactory->Object)
+		{
+			if (pObject->GetType() == pType)
+				--queued;
+		}
+	}
+
+	return queued;
+}
+
+inline bool __fastcall ReachedBuildLimit(const HouseClass* pHouse, const TechnoTypeClass* pType, bool ignoreQueued)
 {
 	const auto pTypeExt = TechnoTypeExt::ExtMap.Find(pType);
 
@@ -19,6 +40,9 @@ inline bool __fastcall ReachedBuildLimit(const HouseClass* pHouse, const TechnoT
 
 		for (const TechnoTypeClass* pTmpType : pTypeExt->BuildLimit_Group_Types)
 		{
+			if (!ignoreQueued)
+				num += QueuedNum(pHouse, pTmpType);
+
 			num += pHouse->CountOwnedNow(pTmpType);
 		}
 
@@ -33,8 +57,9 @@ inline bool __fastcall ReachedBuildLimit(const HouseClass* pHouse, const TechnoT
 		for (size_t i = 0; i < size; i++)
 		{
 			const TechnoTypeClass* pTmpType = pTypeExt->BuildLimit_Group_Types[i];
+			int queued = ignoreQueued ? 0 : QueuedNum(pHouse, pTmpType);
 
-			if (pHouse->CountOwnedNow(pTmpType) >= pTypeExt->BuildLimit_Group_Limits[i])
+			if (queued + pHouse->CountOwnedNow(pTmpType) >= pTypeExt->BuildLimit_Group_Limits[i])
 			{
 				if (pTypeExt->BuildLimit_Group_Any)
 					return true;
@@ -58,10 +83,11 @@ DEFINE_HOOK(0x4F8361, HouseClass_CanBuild, 0x5)
 	GET_STACK(const TechnoTypeClass*, pType, 0x4);
 	GET(CanBuildResult, aresResult, EAX);
 
+
 	if (aresResult != CanBuildResult::Buildable)
 		return 0;
 
-	if (ReachedBuildLimit(pThis, pType))
+	if (ReachedBuildLimit(pThis, pType, true))
 		R->EAX(CanBuildResult::TemporarilyUnbuildable);
 
 	return 0;
@@ -76,7 +102,10 @@ DEFINE_HOOK(0x50B669, HouseClass_ShouldDisableCameo, 0x5)
 	if (aresDisable)
 		return 0;
 
-	if (ReachedBuildLimit(pThis, pType))
+	if (pType == nullptr)
+		return 0;
+
+	if (ReachedBuildLimit(pThis, pType, false))
 		R->EAX(true);
 
 	return 0;
