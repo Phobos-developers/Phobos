42c91d8da2522eb7c1ebcec7972f573c9cdbfd74
Starkku <starkku.ppmod@gmail.com>
2024-06-01 15:05:52 +0300

    Fix inconsistent digital display offset behaviour and add way to adjust value scale

--- src/New/Type/DigitalDisplayTypeClass.h
+++ src/New/Type/DigitalDisplayTypeClass.h
@@ -23,6 +23,7 @@ public:
 	Valueable<bool> VisibleToHouses_Observer;
 	Valueable<AffectedHouse> VisibleToHouses;
 	Valueable<DisplayInfoType> InfoType;
+	Valueable<int> ValueScaleDivisor;
 
 	DigitalDisplayTypeClass(const char* pTitle = NONE_STR) : Enumerable<DigitalDisplayTypeClass>(pTitle)
 		, Text_Color({ 0, 255, 0 }, { 255,255,0 }, { 255,0,0 })
@@ -40,6 +41,7 @@ public:
 		, VisibleToHouses_Observer(true)
 		, VisibleToHouses(AffectedHouse::All)
 		, InfoType(DisplayInfoType::Health)
+		, ValueScaleDivisor { 1 }
 	{ }
 
 	virtual ~DigitalDisplayTypeClass() override = default;
