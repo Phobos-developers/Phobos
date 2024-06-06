Phobos-developers/Phobos

commit c1736fea72676c184ad5f88eabbb1fa081158755

Couple of Warhead detonation-related bugfixes
- Phobos Warhead effects on zero-CellSpread Warheads no longer apply to target if projectile detonates prematurely, far-away from target
- ExtraWarheads now snaps on target of the original projectile where applicable

src/Ext/Bullet/Hooks.DetonateLogics.cpp
@@ -288,7 +288,7 @@ DEFINE_HOOK(0x46A290, BulletClass_Logics_Extras, 0x5)
 				detonate = pWeaponExt->ExtraWarheads_DetonationChances[size - 1] >= ScenarioClass::Instance->Random.RandomDouble();
 
 			if (detonate)
-				WarheadTypeExt::DetonateAt(pWH, *coords, pThis->Owner, damage, pOwner);
+				WarheadTypeExt::DetonateAt(pWH, *coords, pThis->Owner, damage, pOwner, pThis->Target);
 		}
 	}
 

src/Ext/WarheadType/Body.cpp
@@ -58,23 +58,16 @@ bool WarheadTypeExt::ExtData::CanAffectTarget(TechnoClass* pTarget, TechnoExt::E
 	return GeneralUtils::GetWarheadVersusArmor(this->OwnerObject(), armorType) != 0.0;
 }
 
-namespace DetonateTemp
-{
-	AbstractClass* pTarget = nullptr;
-}
-
 void WarheadTypeExt::DetonateAt(WarheadTypeClass* pThis, AbstractClass* pTarget, TechnoClass* pOwner, int damage, HouseClass* pFiringHouse)
 {
-	DetonateTemp::pTarget = pTarget;
-	WarheadTypeExt::DetonateAt(pThis, pTarget->GetCoords(), pOwner, damage, pFiringHouse);
-	DetonateTemp::pTarget = nullptr;
+	WarheadTypeExt::DetonateAt(pThis, pTarget->GetCoords(), pOwner, damage, pFiringHouse, pTarget);
 }
 
-void WarheadTypeExt::DetonateAt(WarheadTypeClass* pThis, const CoordStruct& coords, TechnoClass* pOwner, int damage, HouseClass* pFiringHouse)
+void WarheadTypeExt::DetonateAt(WarheadTypeClass* pThis, const CoordStruct& coords, TechnoClass* pOwner, int damage, HouseClass* pFiringHouse, AbstractClass* pTarget)
 {
 	BulletTypeClass* pType = BulletTypeExt::GetDefaultBulletType();
 
-	if (BulletClass* pBullet = pType->CreateBullet(DetonateTemp::pTarget, pOwner,
+	if (BulletClass* pBullet = pType->CreateBullet(pTarget, pOwner,
 		damage, pThis, 0, pThis->Bright))
 	{
 		if (pFiringHouse)

src/Ext/WarheadType/Body.h
@@ -307,5 +307,5 @@ public:
 	static bool SaveGlobals(PhobosStreamWriter& Stm);
 
 	static void DetonateAt(WarheadTypeClass* pThis, AbstractClass* pTarget, TechnoClass* pOwner, int damage, HouseClass* pFiringHouse = nullptr);
-	static void DetonateAt(WarheadTypeClass* pThis, const CoordStruct& coords, TechnoClass* pOwner, int damage, HouseClass* pFiringHouse = nullptr);
+	static void DetonateAt(WarheadTypeClass* pThis, const CoordStruct& coords, TechnoClass* pOwner, int damage, HouseClass* pFiringHouse = nullptr, AbstractClass* pTarget = nullptr);
 };

src/Ext/WarheadType/Detonate.cpp
@@ -122,7 +122,12 @@ void WarheadTypeExt::ExtData::Detonate(TechnoClass* pOwner, HouseClass* pHouse, 
 		else if (pBullet)
 		{
 			if (auto pTarget = abstract_cast<TechnoClass*>(pBullet->Target))
-				this->DetonateOnOneUnit(pHouse, pTarget, pOwner, bulletWasIntercepted);
+			{
+				// Starkku: We should only detonate on the target if the bullet, at the moment of detonation is within acceptable distance of the target.
+				// Ares uses 64 leptons / quarter of a cell as a tolerance, so for sake of consistency we're gonna do the same here.
+				if (pBullet->DistanceFrom(pTarget) < Unsorted::LeptonsPerCell / 4)
+					this->DetonateOnOneUnit(pHouse, pTarget, pOwner, bulletWasIntercepted);
+			}
 		}
 	}
 }

commit 947ed421768d5998861f614739e9ee0c53967c78

Fix issue with DetonateOnAllMapObjects + CellSpread=0 introduced by c1736fea72676c184ad5f88eabbb1fa081158755

src/Ext/Bullet/Hooks.DetonateLogics.cpp
@@ -66,6 +66,7 @@ DEFINE_HOOK(0x4690C1, BulletClass_Logics_DetonateOnAllMapObjects, 0x8)
 			pWHExt->DetonateOnAllMapObjects_AffectHouses != AffectedHouse::None)
 		{
 			pWHExt->WasDetonatedOnAllMapObjects = true;
+			auto const originalLocation = pThis->Location;
 			auto const pOriginalTarget = pThis->Target;
 			auto const pExt = BulletExt::ExtMap.Find(pThis);
 			auto pOwner = pThis->Owner ? pThis->Owner->Owner : pExt->FirerHouse;
@@ -75,6 +76,7 @@ DEFINE_HOOK(0x4690C1, BulletClass_Logics_DetonateOnAllMapObjects, 0x8)
 				if (pWHExt->EligibleForFullMapDetonation(pTechno, pOwner))
 				{
 					pThis->Target = pTechno;
+					pThis->Location = pTechno->GetCoords();
 					pThis->Detonate(pTechno->GetCoords());
 				}
 			};
@@ -119,6 +121,7 @@ DEFINE_HOOK(0x4690C1, BulletClass_Logics_DetonateOnAllMapObjects, 0x8)
 			}
 
 			pThis->Target = pOriginalTarget;
+			pThis->Location = originalLocation;
 			pWHExt->WasDetonatedOnAllMapObjects = false;
 
 			return ReturnFromFunction;