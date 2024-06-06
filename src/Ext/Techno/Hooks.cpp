e05f9694a1fe5e58d4c2daa5d05e21eb52c289de
Starkku <starkku.ppmod@gmail.com>
2024-06-06 19:26:46 +0300

    A couple of cloak-related fixes / additions
    - Fix a regression in AE cloak code
    - Fix DecloakToFire=no not preventing decloaking when having a target in range
    - Move cloak-related code to its own file

--- src/Ext/Techno/Hooks.cpp
+++ src/Ext/Techno/Hooks.cpp
@@ -269,39 +269,6 @@ DEFINE_HOOK(0x4D7221, FootClass_Unlimbo_LaserTrails, 0x6)
 	return 0;
 }
 
-DEFINE_HOOK_AGAIN(0x703789, TechnoClass_CloakUpdateMCAnim, 0x6) // TechnoClass_Do_Cloak
-DEFINE_HOOK(0x6FB9D7, TechnoClass_CloakUpdateMCAnim, 0x6)       // TechnoClass_Cloaking_AI
-{
-	GET(TechnoClass*, pThis, ESI);
-
-	if (const auto pExt = TechnoExt::ExtMap.Find(pThis))
-		pExt->UpdateMindControlAnim();
-
-	return 0;
-}
-
-DEFINE_HOOK(0x703A09, TechnoClass_VisualCharacter_CloakVisibility, 0x7)
-{
-	enum { UseShadowyVisual = 0x703A5A, CheckMutualAlliance = 0x703A16 };
-
-	// Allow observers to always see cloaked objects.
-	// Skip IsCampaign check (confirmed being useless from Mental Omega mappers)
-	if (HouseClass::IsCurrentPlayerObserver())
-		return UseShadowyVisual;
-
-	return CheckMutualAlliance;
-}
-
-DEFINE_HOOK(0x45455B, BuildingClass_VisualCharacter_CloakVisibility, 0x5)
-{
-	enum { UseShadowyVisual = 0x45452D, CheckMutualAlliance = 0x454564 };
-
-	if (HouseClass::IsCurrentPlayerObserver())
-		return UseShadowyVisual;
-
-	return CheckMutualAlliance;
-}
-
 DEFINE_HOOK(0x4DEAEE, FootClass_IronCurtain_Organics, 0x6)
 {
 	GET(FootClass*, pThis, ESI);
