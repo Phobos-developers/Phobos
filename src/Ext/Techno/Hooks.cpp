8ec1be620f5d2550faa5d99f709d91cf82829b8b
Trsdy <914137150@qq.com>
2024-06-19 15:49:18 +0800

    Remove unused/useless files, fix compile warning

--- src/Ext/Techno/Hooks.cpp
+++ src/Ext/Techno/Hooks.cpp
@@ -15,9 +15,7 @@ DEFINE_HOOK(0x6F9E50, TechnoClass_AI, 0x5)
 	GET(TechnoClass*, pThis, ECX);
 
 	// Do not search this up again in any functions called here because it is costly for performance - Starkku
-	auto pExt = TechnoExt::ExtMap.Find(pThis);
-	pExt->OnEarlyUpdate();
-	pExt->UpdateAttachEffects();
+	TechnoExt::ExtMap.Find(pThis)->OnEarlyUpdate();
 	TechnoExt::ApplyMindControlRangeLimit(pThis);
 
 	return 0;
