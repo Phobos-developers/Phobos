796d446ef7985cf2c0b3dc41950ea8e302d6698b
Starkku <starkku.ppmod@gmail.com>
2024-06-23 17:31:12 +0300

    Fix AttachEffect issues
    - Defer ExpireWeapon firing to after looping through active AttachEffect instances on techno to prevent crashes/infinite loops from modifying the vector during iteration
    - AE transfer of a non-cumulative type from techno to another had a fault that could generate crashes

--- src/Ext/Techno/Body.Update.cpp
+++ src/Ext/Techno/Body.Update.cpp
@@ -7,6 +7,7 @@
 #include <Ext/Anim/Body.h>
 #include <Ext/Bullet/Body.h>
 #include <Ext/House/Body.h>
+#include <Ext/WeaponType/Body.h>
 #include <Utilities/EnumFunctions.h>
 #include <Utilities/AresFunctions.h>
 
@@ -803,9 +804,11 @@ void TechnoExt::ExtData::UpdateTemporal()
 // Updates state of all AttachEffects on techno.
 void TechnoExt::ExtData::UpdateAttachEffects()
 {
+	auto const pThis = this->OwnerObject();
 	bool inTunnel = this->IsInTunnel || this->IsBurrowed;
 	bool markForRedraw = false;
 	std::vector<std::unique_ptr<AttachEffectClass>>::iterator it;
+	std::vector<WeaponTypeClass*> expireWeapons;
 
 	for (it = this->AttachedEffects.begin(); it != this->AttachedEffects.end(); )
 	{
@@ -828,7 +831,7 @@ void TechnoExt::ExtData::UpdateAttachEffects()
 			if (pType->ExpireWeapon && (pType->ExpireWeapon_TriggerOn & ExpireWeaponCondition::Expire) != ExpireWeaponCondition::None)
 			{
 				if (!pType->Cumulative || !pType->ExpireWeapon_CumulativeOnlyOnce || this->GetAttachedEffectCumulativeCount(pType) < 1)
-					attachEffect->ExpireWeapon();
+					expireWeapons.push_back(pType->ExpireWeapon);
 			}
 
 			if (!attachEffect->AllowedToBeActive() && attachEffect->ResetIfRecreatable())
@@ -848,7 +851,15 @@ void TechnoExt::ExtData::UpdateAttachEffects()
 	this->RecalculateStatMultipliers();
 
 	if (markForRedraw)
-		this->OwnerObject()->MarkForRedraw();
+		pThis->MarkForRedraw();
+
+	auto const coords = pThis->GetCoords();
+	auto const pOwner = pThis->Owner;
+
+	for (auto const& pWeapon : expireWeapons)
+	{
+		WeaponTypeExt::DetonateAt(pWeapon, coords, pThis, pOwner, pThis);
+	}
 }
 
 // Updates state of AttachEffects of same cumulative type on techno, (which one is first active instance existing, if any), kills animations if needed.
