349794c066b9a62f256a9868962189ef41fcb21b
Kerbiter <crabiter@vivaldi.net>
2024-07-13 15:47:41 +0300

    Fix code a bit

--- src/Ext/Infantry/Hooks.cpp
+++ src/Ext/Infantry/Hooks.cpp
@@ -8,24 +8,22 @@
 
 DEFINE_HOOK(0x51A3A2, InfantryClass_PerCellProcess_CyborgLegsCheck, 0x5)
 {
-	GET(TechnoClass*, pTechno, ESI);
-	GET(TechnoClass*, pTransport, EDI);
+	GET(InfantryClass* const, pThis, ESI);
+	GET(TechnoClass* const, pTransport, EDI);
 
-		auto const pInf = static_cast<InfantryClass*>(pTechno);
+	// Note: "SequenceAnim" and "Crawling" values will suffer a reset before entering into transports.
+	// The Cyborg legs state will be saved for the unlimbo case
+	if (pThis->Type->Cyborg && pThis->Crawling)
+	{
+		auto pTechnoExt = TechnoExt::ExtMap.Find(pTechno);
+		pTechnoExt->IsLeglessCyborg = true;
 
-		// Note: "SequenceAnim" and "Crawling" values will suffer a reset before entering into transports.
-		// The Cyborg legs state will be saved for the unlimbo case
-		if (pInf->Type->Cyborg && pInf->Crawling)
+		if (auto const pTransportTypeExt = TechnoTypeExt::ExtMap.Find(pTransport->GetTechnoType()))
 		{
-			auto pTechnoExt = TechnoExt::ExtMap.Find(pTechno);
-			pTechnoExt->IsLeglessCyborg = true;
-
-			if (auto const pTransportTypeExt = TechnoTypeExt::ExtMap.Find(pTransport->GetTechnoType()))
-			{
-				if (pTransportTypeExt->Transporter_FixCyborgLegs)
-					pTechnoExt->IsLeglessCyborg = false;
-			}
+			if (pTransportTypeExt->Transporter_FixCyborgLegs)
+				pTechnoExt->IsLeglessCyborg = false;
 		}
+	}
 
 	return 0;
 }
