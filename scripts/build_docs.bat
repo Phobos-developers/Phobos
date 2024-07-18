8f335285674412b407c84bf0affb8124ec1b4e43
Sovietianqi <130513099+Sovietianqi@users.noreply.github.com>
2024-07-18 13:09:48 +0800

    Delete scripts directory

--- scripts/build_docs.bat
+++
@@ -1,9 +0,0 @@
-@if not defined _echo echo off
-
-rem Builds Phobos docs with Sphinx.
-
-rem Ensure we're in correct directory.
-cd /D "%~dp0"
-cd ..\docs
-
-call make.bat html
--- scripts/build_release.bat
+++
@@ -1,8 +0,0 @@
-@if not defined _echo echo off
-
-rem Builds Phobos Release.
-
-rem Ensure we're in correct directory.
-cd /D "%~dp0"
-
-call run_msbuild /maxCpuCount /consoleloggerparameters:NoSummary /property:Configuration=Release
