4ca8334a6eea6d5126f474ce67cde74254c03c08
Metadorius <crabiter@vivaldi.net>
2024-07-21 23:05:37 +0300

    Animatable template shenanigans + some template improvements p.1

--- src/Utilities/Savegame.h
+++ src/Utilities/Savegame.h
@@ -8,6 +8,23 @@
 namespace Savegame
 {
 	template <typename T>
+	concept ImplementsUpperCaseSaveLoad = requires (PhobosStreamWriter& stmWriter, PhobosStreamReader& stmReader, T& value, bool registerForChange)
+	{
+		value.Save(stmWriter);
+		value.Load(stmReader, registerForChange);
+	};
+
+	template <typename T>
+	concept ImplementsLowerCaseSaveLoad = requires (PhobosStreamWriter & stmWriter, PhobosStreamReader & stmReader, T& value, bool registerForChange)
+	{
+		value.save(stmWriter);
+		value.load(stmReader, registerForChange);
+	};
+
+	template <typename T>
+	concept ImplementsSaveLoad = ImplementsUpperCaseSaveLoad<T> || ImplementsLowerCaseSaveLoad<T>;
+
+	template <typename T>
 	bool ReadPhobosStream(PhobosStreamReader& Stm, T& Value, bool RegisterForChange = true);
 
 	template <typename T>
