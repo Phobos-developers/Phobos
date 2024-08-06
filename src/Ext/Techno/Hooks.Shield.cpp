29280119c3eeaafd689733f3f7e84943791cb4eb
TaranDahl <sd_zealot@163.com>
2024-08-07 01:11:25 +0800

    Update Hooks.Shield.cpp
    Co-Authored-By: CrimRecya <169989423+crimrecya@users.noreply.github.com>

--- src/Ext/Techno/Hooks.Shield.cpp
+++ src/Ext/Techno/Hooks.Shield.cpp
@@ -79,16 +79,16 @@ DEFINE_HOOK(0x701900, TechnoClass_ReceiveDamage_Shield, 0x6)
 			pHouseExt->CombatAlertTimer.Start(pRules->CombatAlert_Interval);
 			RadarEventClass::Create(RadarEventType::Combat, CellClass::Coord2Cell(coordInMap));
 
+			int index = -1;
+
 			if (!pRules->CombatAlert_MakeAVoice) // No one want to play two sound at a time, I guess?
 				break;
 			else if (pRules->CombatAlert_UseFeedbackVoice && pType->VoiceFeedback.Count > 0) // Use VoiceFeedback first
 				VocClass::PlayGlobal(pType->VoiceFeedback.GetItem(0), 0x2000, 1.0);
 			else if (pRules->CombatAlert_UseAttackVoice && pType->VoiceAttack.Count > 0) // Use VoiceAttack then
 				VocClass::PlayGlobal(pType->VoiceAttack.GetItem(0), 0x2000, 1.0);
-			else if (!pRules->CombatAlert_UseEVA) // Use Eva finally
-				break;
-
-			const int index = pTypeExt->CombatAlert_EVA.Get(VoxClass::FindIndex((const char*)"EVA_UnitsInCombat"));
+			else if (pRules->CombatAlert_UseEVA) // Use Eva finally
+				index = pTypeExt->CombatAlert_EVA.Get(VoxClass::FindIndex((const char*)"EVA_UnitsInCombat"));
 
 			if (index != -1)
 				VoxClass::PlayIndex(index);
