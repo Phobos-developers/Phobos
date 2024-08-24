a6292ebcfd71938589320f3a1735d1fe76f3f48e
CrimRecya <335958461@qq.com>
2024-08-24 15:49:29 +0800

    Update Hooks.cpp

--- src/Ext/House/Hooks.cpp
+++ src/Ext/House/Hooks.cpp
@@ -290,13 +290,15 @@ DEFINE_HOOK(0x50B669, HouseClass_ShouldDisableCameo_GreyCameo, 0x5)
 // All technos have AlwaysExistTheCameo=true need to change the EVA_NewConstructionOptions playing time
 DEFINE_HOOK(0x6A640B, SideBarClass_AddCameo_DoNotPlayEVA, 0x5)
 {
+	enum { SkipPlaying = 0x6A641A };
+
 	GET(AbstractType, absType, ESI);
 	GET(int, idxType, EBP);
 
 	if (auto const pType = ObjectTypeClass::GetTechnoType(absType, idxType))
 	{
 		if (TechnoTypeExt::ExtMap.Find(pType)->AlwaysExistTheCameo.Get(RulesExt::Global()->AlwaysExistTheCameo))
-			return 0x6A641A; // Skip playing
+			return SkipPlaying;
 	}
 
 	return 0;
