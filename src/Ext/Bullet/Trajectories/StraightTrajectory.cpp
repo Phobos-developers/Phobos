1660fa650c4b653b5873676df554eb29512316e5
CrimRecya <335958461@qq.com>
2024-08-30 20:18:01 +0800

    Fix

--- src/Ext/Bullet/Trajectories/StraightTrajectory.cpp
+++ src/Ext/Bullet/Trajectories/StraightTrajectory.cpp
@@ -288,7 +288,7 @@ void StraightTrajectory::OnUnlimbo(BulletClass* pBullet, CoordStruct* pCoord, Bu
 			this->OffsetCoord.Y = -(this->OffsetCoord.Y);
 	}
 
-	if (!this->LeadTimeCalculate || (pBullet->Target && (pBullet->Target->AbstractFlags & AbstractFlags::Foot) != AbstractFlags::None))
+	if (!this->LeadTimeCalculate || !abstract_cast<FootClass*>(pBullet->Target))
 		this->PrepareForOpenFire(pBullet);
 	else
 		this->WaitOneFrame.Start(1);
