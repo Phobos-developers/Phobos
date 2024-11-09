f260915c799474119d0b296bb54855d83898661c
CrimRecya <335958461@qq.com>
2024-11-09 13:42:03 +0800

    Const auto

--- src/Ext/Techno/Body.Internal.cpp
+++ src/Ext/Techno/Body.Internal.cpp
@@ -146,19 +146,19 @@ CoordStruct TechnoExt::GetSimpleFLH(InfantryClass* pThis, int weaponIndex, bool&
 
 void TechnoExt::ExtData::InitializeDisplayInfo()
 {
-	auto const pThis = this->OwnerObject();
-	auto const pType = pThis->GetTechnoType();
-	auto const pPrimary = pThis->GetWeapon(0);
-	auto const pSecondary = pThis->GetWeapon(1);
+	const auto pThis = this->OwnerObject();
+	const auto pType = pThis->GetTechnoType();
+	const auto pPrimary = pThis->GetWeapon(0);
+	const auto pSecondary = pThis->GetWeapon(1);
 
 	if (pPrimary && pPrimary->WeaponType && pType->LandTargeting != LandTargetingType::Land_Not_OK)
 		pThis->ChargeTurretDelay = pPrimary->WeaponType->ROF;
 	else if (pSecondary && pSecondary->WeaponType)
 		pThis->ChargeTurretDelay = pSecondary->WeaponType->ROF;
 
-	if (auto pTypeExt = this->TypeExtData)
+	if (const auto pTypeExt = this->TypeExtData)
 	{
-		auto pDelType = pTypeExt->PassengerDeletionType.get();
+		const auto pDelType = pTypeExt->PassengerDeletionType.get();
 
 		if (pDelType && !pThis->Passengers.GetFirstPassenger())
 			this->PassengerDeletionTimer.TimeLeft = pDelType->Rate;
