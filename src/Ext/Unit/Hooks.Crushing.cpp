5dca5df10da0d46971303d1515802160af7f0e44
Aephiex <34618932+Aephiex@users.noreply.github.com>
2024-11-25 01:32:38 +0800

    Update Hooks.Crushing.cpp

--- src/Ext/Unit/Hooks.Crushing.cpp
+++ src/Ext/Unit/Hooks.Crushing.cpp
@@ -56,6 +56,32 @@ DEFINE_HOOK(0x4B1150, DriveLocomotionClass_WhileMoving_CrushSlowdown, 0x9)
 
 }
 
+DEFINE_HOOK(0x7418AA, UnitClass_CrushCell_WhenCrushed, 6)
+{
+	GET(UnitClass* const, pThis, EDI);
+	GET(ObjectClass* const, pVictim, ESI);
+
+	if(auto const pTechno = abstract_cast<TechnoClass*>(pVictim)) {
+		auto pExt = TechnoTypeExt::ExtMap.Find(pVictim->GetTechnoType());
+
+		const auto pWeapon = pExt->WhenCrushed_Weapon;
+
+		auto const pWarhead = pExt->WhenCrushed_Warhead.Get(RulesClass::Instance->C4Warhead);
+
+		if (pWeapon)
+			WeaponTypeExt::DetonateAt(pWeapon, pThis->GetCoords(), nullptr, pExt->WhenCrushed_Damage.Get(pWeapon->Damage), pVictim->GetOwningHouse());
+		else
+		{
+			if (pExt->WhenCrushed_Warhead_Full)
+				WarheadTypeExt::DetonateAt(pWarhead, pThis->GetCoords(), nullptr, pExt->WhenCrushed_Damage.Get(0),  pVictim->GetOwningHouse());
+			else
+				MapClass::DamageArea(pThis->GetCoords(), pExt->WhenCrushed_Damage.Get(0), nullptr, pWarhead, true,  pVictim->GetOwningHouse());
+		}
+	}
+
+	return 0;
+}
+
 DEFINE_HOOK_AGAIN(0x4B1A4B, DriveLocomotionClass_WhileMoving_CrushTilt, 0xD)
 DEFINE_HOOK(0x4B19F7, DriveLocomotionClass_WhileMoving_CrushTilt, 0xD)
 {
