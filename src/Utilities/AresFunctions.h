715ebea5bb42493d29604060bd0e735f10a9953b
Trsdy <914137150@qq.com>
2024-12-01 15:03:34 +0800

    Rename AresInit file

--- src/Utilities/AresFunctions.h
+++ src/Utilities/AresFunctions.h
@@ -1,9 +1,19 @@
 #pragma once
+#include <functional>
 class TechnoClass;
 class TechnoTypeClass;
 class FootClass;
 class HouseClass;
 class BuildingTypeClass;
+class BuildingClass;
+class HouseTypeClass;
+class SuperClass;
+class SuperWeaponTypeClass;
+
+class AresTechnoExtData;
+class AresTechnoTypeExtData;
+class AresHouseExtData;
+class AresSWTypeExtData;
 
 class AresFunctions
 {
@@ -14,9 +24,13 @@ public:
 	static bool(__stdcall* ConvertTypeTo)(TechnoClass* pFoot, TechnoTypeClass* pConvertTo);
 
 	static void(__stdcall* SpawnSurvivors)(FootClass* pThis, TechnoClass* pKiller, bool Select, bool IgnoreDefenses);
+	static std::function<AresSWTypeExtData* (SuperWeaponTypeClass*)> SWTypeExtMap_Find;
 private:
 	static constexpr bool _maybe = false;
 
 	static constexpr bool AresWasWrongAboutSpawnSurvivors = _maybe;
 	static constexpr bool AresWasWrongAboutAduction = true;
+
+	static void* _SWTypeExtMap;
+	static AresSWTypeExtData* (__thiscall* _SWTypeExtMapFind)(void*, SuperWeaponTypeClass*);
 };
