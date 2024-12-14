5ea6f1151486e5cdd4cf6d412a142beed8752be7
CrimRecya <335958461@qq.com>
2024-12-14 22:10:55 +0800

    Aircrafts with team will not smooth their route, because of the scripts' location check

--- src/Ext/Aircraft/Hooks.cpp
+++ src/Ext/Aircraft/Hooks.cpp
@@ -346,7 +346,7 @@ DEFINE_HOOK(0x416A0A, AircraftClass_Mission_Move_SmoothMoving, 0x5)
 
 	const auto pType = pThis->Type;
 
-	if (!pType->AirportBound || pThis->Airstrike || pThis->Spawned)
+	if (!pType->AirportBound || pThis->Team || pThis->Airstrike || pThis->Spawned)
 		return 0;
 
 	const int distance = Game::F2I(Point2D { pCoords->X, pCoords->Y }.DistanceFrom(Point2D { pThis->Location.X, pThis->Location.Y }));
