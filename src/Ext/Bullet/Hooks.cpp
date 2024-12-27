1609f1f7cc18729cc0023ae9994d1d73b66dda68
CrimRecya <335958461@qq.com>
2024-12-27 23:55:33 +0800

    Rename function

--- src/Ext/Bullet/Hooks.cpp
+++ src/Ext/Bullet/Hooks.cpp
@@ -290,7 +290,7 @@ DEFINE_HOOK(0x46902C, BulletClass_Explode_Cluster, 0x6)
 	return SkipGameCode;
 }
 
-constexpr bool CheckTrajectoryCanSnap(const TrajectoryFlag flag)
+constexpr bool CheckTrajectoryCanNotAlwaysSnap(const TrajectoryFlag flag)
 {
 	return flag == TrajectoryFlag::Straight;
 }
@@ -309,7 +309,7 @@ DEFINE_HOOK(0x467CCA, BulletClass_AI_TargetSnapChecks, 0x6)
 	}
 	else if (auto const pExt = BulletAITemp::ExtData)
 	{
-		if (pExt->Trajectory && CheckTrajectoryCanSnap(pExt->Trajectory->Flag()))
+		if (pExt->Trajectory && CheckTrajectoryCanNotAlwaysSnap(pExt->Trajectory->Flag()))
 		{
 			R->EAX(pThis->Type);
 			return SkipChecks;
@@ -337,7 +337,7 @@ DEFINE_HOOK(0x468E61, BulletClass_Explode_TargetSnapChecks1, 0x6)
 	}
 	else if (auto const pExt = BulletExt::ExtMap.Find(pThis))
 	{
-		if (pExt->Trajectory && CheckTrajectoryCanSnap(pExt->Trajectory->Flag()) && !pExt->SnappedToTarget)
+		if (pExt->Trajectory && CheckTrajectoryCanNotAlwaysSnap(pExt->Trajectory->Flag()) && !pExt->SnappedToTarget)
 		{
 			R->EAX(pThis->Type);
 			return SkipChecks;
@@ -368,7 +368,7 @@ DEFINE_HOOK(0x468E9F, BulletClass_Explode_TargetSnapChecks2, 0x6)
 	// Fixes issues with walls etc.
 	if (auto const pExt = BulletExt::ExtMap.Find(pThis))
 	{
-		if (pExt->Trajectory && CheckTrajectoryCanSnap(pExt->Trajectory->Flag()) && !pExt->SnappedToTarget)
+		if (pExt->Trajectory && CheckTrajectoryCanNotAlwaysSnap(pExt->Trajectory->Flag()) && !pExt->SnappedToTarget)
 			return SkipSetCoordinate;
 	}
 
@@ -383,7 +383,7 @@ DEFINE_HOOK(0x468D3F, BulletClass_ShouldExplode_AirTarget, 0x6)
 
 	if (auto const pExt = BulletExt::ExtMap.Find(pThis))
 	{
-		if (pExt->Trajectory && CheckTrajectoryCanSnap(pExt->Trajectory->Flag()))
+		if (pExt->Trajectory && CheckTrajectoryCanNotAlwaysSnap(pExt->Trajectory->Flag()))
 			return SkipCheck;
 	}
 
