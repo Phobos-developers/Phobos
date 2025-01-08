77685c1b87e82f72ba45c00193847ac22efed838
CrimRecya <335958461@qq.com>
2025-01-08 18:37:55 +0800

    Remove useless static_cast

--- src/Ext/Unit/Hooks.Jumpjet.cpp
+++ src/Ext/Unit/Hooks.Jumpjet.cpp
@@ -292,14 +292,14 @@ int JumpjetRushHelpers::JumpjetLocomotionPredictHeight(JumpjetLocomotionClass* p
 		if ((curCoord.X >> 8) != (lastCoord.X >> 8) && (curCoord.Y >> 8) != (lastCoord.Y >> 8))
 		{
 			bool lastX = (abs(stepCoord.X) > abs(stepCoord.Y)) ?
-				(((curCoord.Y - static_cast<int>(((stepCoord.X > 0) ?
+				(((curCoord.Y - ((stepCoord.X > 0) ?
 					(curCoord.X & 0XFF) :
 					((curCoord.X & 0XFF) - Unsorted::LeptonsPerCell)) *
-				checkCoord.Y / checkCoord.X)) >> 8) == (curCoord.Y >> 8)) :
-				(((curCoord.X - static_cast<int>(((stepCoord.Y > 0) ?
+				checkCoord.Y / checkCoord.X) >> 8) == (curCoord.Y >> 8)) :
+				(((curCoord.X - ((stepCoord.Y > 0) ?
 					(curCoord.Y & 0XFF) :
 					((curCoord.Y & 0XFF) - Unsorted::LeptonsPerCell)) *
-				checkCoord.X / checkCoord.Y)) >> 8) != (curCoord.X >> 8));
+				checkCoord.X / checkCoord.Y) >> 8) != (curCoord.X >> 8));
 
 			if (const auto pCheckCell = MapClass::Instance->TryGetCellAt(lastX ?
 				CellStruct { static_cast<short>(lastCoord.X >> 8), static_cast<short>(curCoord.Y >> 8) } :
@@ -335,14 +335,14 @@ int JumpjetRushHelpers::JumpjetLocomotionPredictHeight(JumpjetLocomotionClass* p
 			if ((curCoord.X >> 8) != (lastCoord.X >> 8) && (curCoord.Y >> 8) != (lastCoord.Y >> 8))
 			{
 				bool lastX = (abs(stepCoord.X) > abs(stepCoord.Y)) ?
-					(((curCoord.Y - static_cast<int>(((stepCoord.X > 0) ?
+					(((curCoord.Y - ((stepCoord.X > 0) ?
 						(curCoord.X & 0XFF) :
 						((curCoord.X & 0XFF) - Unsorted::LeptonsPerCell)) *
-					checkCoord.Y / checkCoord.X)) >> 8) == (curCoord.Y >> 8)) :
-					(((curCoord.X - static_cast<int>(((stepCoord.Y > 0) ?
+					checkCoord.Y / checkCoord.X) >> 8) == (curCoord.Y >> 8)) :
+					(((curCoord.X - ((stepCoord.Y > 0) ?
 						(curCoord.Y & 0XFF) :
 						((curCoord.Y & 0XFF) - Unsorted::LeptonsPerCell)) *
-					checkCoord.X / checkCoord.Y)) >> 8) != (curCoord.X >> 8));
+					checkCoord.X / checkCoord.Y) >> 8) != (curCoord.X >> 8));
 
 				if (const auto pCheckCell = MapClass::Instance->TryGetCellAt(lastX ?
 					CellStruct { static_cast<short>(lastCoord.X >> 8), static_cast<short>(curCoord.Y >> 8) } :
