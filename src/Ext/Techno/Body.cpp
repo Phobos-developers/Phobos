2bc0ed63bb9572576c0c62be791e23d89dc810fa
Aephiex <34618932+Aephiex@users.noreply.github.com>
2025-01-12 22:25:49 +0800

    Update Body.cpp

--- src/Ext/Techno/Body.cpp
+++ src/Ext/Techno/Body.cpp
@@ -471,6 +471,7 @@ int TechnoExt::ExtData::GetAttachedEffectCumulativeCount(AttachEffectTypeClass* 
 // it doesn't check if this transport is actually a transport or not.
 bool TechnoExt::ExtData::CanLoadPassenger(TechnoClass* pPassenger) const
 {
+	auto const pTransport = this->OwnerObject();
 	auto const pTransportTypeExt = this->TypeExtData;
 	auto const pTransportType = pTransportTypeExt->OwnerObject();
 	auto const pPassengerType = pPassenger->GetTechnoType();
@@ -496,7 +497,6 @@ bool TechnoExt::ExtData::CanLoadAny(std::vector<TechnoClass*> pPassengerList) co
 // Checks if a transport can load any of the passengers inside a map.
 bool TechnoExt::ExtData::CanLoadAny(std::map<int, std::vector<TechnoClass*>> passengerMap) const
 {
-	auto const pTransport = this->OwnerObject();
 	auto const pTransportTypeExt = this->TypeExtData;
 	auto const pTransportType = pTransportTypeExt->OwnerObject();
 	auto const sizeLimit = static_cast<int>(pTransportType->SizeLimit);
