420a1978f2462711301665d6a6676a33e8687be3
Kerbiter <crabiter@vivaldi.net>
2025-02-12 13:17:11 +0200

    Apply suggestions from code review

--- src/Ext/Bullet/Hooks.DetonateLogics.cpp
+++ src/Ext/Bullet/Hooks.DetonateLogics.cpp
@@ -329,11 +329,7 @@ DEFINE_HOOK(0x469AA4, BulletClass_Logics_Extras, 0x5)
 				if (pWHExt->EffectsRequireVerses && versus == 0.0)
 					continue;
 
-				double hp = pTarget->GetHealthPercentage();
-				bool hpBelowPercent = hp <= pWHExt->AffectsBelowPercent;
-				bool hpAbovePercent = hp > pWHExt->AffectsAbovePercent;
-
-				if (!hpBelowPercent || !hpAbovePercent)
+				if (!pWHExt->IsHealthInThreshold(pTarget))
 					continue;
 			}
 
