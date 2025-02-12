91cc56301d88577bf2c947ac207382da43091556
Metadorius <crabiter@vivaldi.net>
2025-02-12 21:53:27 +0200

    Use Furo theme instead of RTD theme, add some useful doc libs

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,14 @@ author = 'The Phobos Contributors'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_rtd_theme', 'myst_parser', 'sphinx.ext.mathjax']
+extensions = [
+    'sphinx_inline_tabs',
+    'sphinxext.opengraph',
+    'sphinx_copybutton',
+    'sphinx_design',
+    'myst_parser',
+    'sphinx.ext.mathjax'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -44,12 +51,16 @@ gettext_compact = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_css_files = [
+    'css/custom.css',
+]
 
 myst_heading_anchors = 3
 
