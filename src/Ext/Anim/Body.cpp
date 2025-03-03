e6283ec32492cbc868a83d69e9c485e8ca6c5a48
Starkku <starkku.ppmod@gmail.com>
2025-03-03 19:37:21 +0200

    Fix oversight in attached particle system pointer invalidation

--- src/Ext/Anim/Body.cpp
+++ src/Ext/Anim/Body.cpp
@@ -432,7 +432,12 @@ void AnimExt::InvalidateParticleSystemPointers(ParticleSystemClass* pParticleSys
 		}
 
 		if (pExt->AttachedSystem == pParticleSystem)
+		{
 			pExt->AttachedSystem = nullptr;
+
+			auto& vec = AnimExt::AnimsWithAttachedParticles;
+			vec.erase(std::remove(vec.begin(), vec.end(), pAnim), vec.end());
+		}
 	}
 }
 
