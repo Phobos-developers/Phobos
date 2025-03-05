535dc2d0c736f198a66decc79f4b5eb8b6aea2ae
Coronia <2217891145@qq.com>
2025-03-05 17:02:13 +0800

    remove problematic line for Bolt

--- src/Ext/WeaponType/Hook.EBolt.cpp
+++ src/Ext/WeaponType/Hook.EBolt.cpp
@@ -15,7 +15,6 @@ DEFINE_HOOK(0x6FD494, TechnoClass_FireEBolt_SetExtMap_AfterAres, 0x7)
 	{
 		auto const pWeaponExt = WeaponTypeExt::ExtMap.Find(pWeapon);
 		WeaponTypeExt::BoltWeaponMap[pBolt] = pWeaponExt;
-		pBolt->Lifetime = 1 << (Math::clamp(pWeaponExt->Bolt_Duration, 1, 31) - 1);
 	}
 
 	return 0;
