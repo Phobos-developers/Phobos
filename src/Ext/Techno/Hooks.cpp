c25b0cd2ceaec20a216dd6f9e8c5265400a41598
Coronia <2217891145@qq.com>
2025-03-08 12:00:35 +0800

    [Minor] Use SetAnimOwnerHouseKind to set owner for anim (#1502)
    Currently, many anims are setting their ownership with `pAnim->Owner=`
    method, which actually misses several check and set logic of Phobos
    (mostly related to create unit). This is another consistency fix to make
    all of these cases use the proper function to set anim's ownership.

--- src/Ext/Techno/Hooks.cpp
+++ src/Ext/Techno/Hooks.cpp
@@ -5,6 +5,7 @@
 #include <ScenarioClass.h>
 #include <TunnelLocomotionClass.h>
 
+#include <Ext/Anim/Body.h>
 #include <Ext/BuildingType/Body.h>
 #include <Ext/House/Body.h>
 #include <Ext/WarheadType/Body.h>
@@ -83,8 +84,12 @@ DEFINE_HOOK(0x6F9FA9, TechnoClass_AI_PromoteAnim, 0x6)
 		else if (RulesExt::Global()->Promote_EliteAnimation)
 			promAnim = GameCreate<AnimClass>(RulesExt::Global()->Promote_EliteAnimation, pThis->GetCenterCoords());
 
-		if(promAnim)
+		if (promAnim)
+		{
 			promAnim->SetOwnerObject(pThis);
+			AnimExt::SetAnimOwnerHouseKind(promAnim, pThis->Owner, nullptr, false, true);
+			AnimExt::ExtMap.Find(promAnim)->SetInvoker(pThis);
+		}
 	}
 
 	return aresProcess();
