f1c16e046e39634cc4bd110694ac3c1c9095a7d0
Kirill Andriiashin <kirill.andriiashin@gmail.com>
2025-03-14 18:20:24 +0300

    BREAKING Replace reference<> and constant_ptr<> (#1574)
    This PR replaces reference<> and constant_ptr<> with normal references.
    Requires https://github.com/Phobos-developers/YRpp/pull/27

--- src/Ext/Bullet/Trajectories/StraightTrajectory.cpp
+++ src/Ext/Bullet/Trajectories/StraightTrajectory.cpp
@@ -223,7 +223,7 @@ void StraightTrajectory::OnAIPreDetonate(BulletClass* pBullet)
 	if (pType->PassDetonateLocal)
 	{
 		CoordStruct detonateCoords = pBullet->Location;
-		detonateCoords.Z = MapClass::Instance->GetCellFloorHeight(detonateCoords);
+		detonateCoords.Z = MapClass::Instance.GetCellFloorHeight(detonateCoords);
 		pBullet->SetLocation(detonateCoords);
 	}
 
@@ -531,11 +531,11 @@ bool StraightTrajectory::BulletDetonatePreCheck(BulletClass* pBullet)
 		return true;
 
 	// Below ground level? (16 ->error range)
-	if (pType->SubjectToGround && MapClass::Instance->GetCellFloorHeight(pBullet->Location) >= (pBullet->Location.Z + 16))
+	if (pType->SubjectToGround && MapClass::Instance.GetCellFloorHeight(pBullet->Location) >= (pBullet->Location.Z + 16))
 		return true;
 
 	// Out of map?
-	if (const auto pCell = MapClass::Instance->TryGetCellAt(pBullet->Location))
+	if (const auto pCell = MapClass::Instance.TryGetCellAt(pBullet->Location))
 		return false;
 	else
 		return true;
@@ -558,7 +558,7 @@ void StraightTrajectory::BulletDetonateVelocityCheck(BulletClass* pBullet, House
 	if (pType->Trajectory_Speed < 256.0) // Low speed with checkSubject was already done well.
 	{
 		// Blocked by obstacles?
-		if (checkThrough && this->CheckThroughAndSubjectInCell(pBullet, MapClass::Instance->GetCellAt(pBullet->Location), pOwner))
+		if (checkThrough && this->CheckThroughAndSubjectInCell(pBullet, MapClass::Instance.GetCellAt(pBullet->Location), pOwner))
 		{
 			locationDistance = 0.0;
 			velocityCheck = true;
@@ -582,13 +582,13 @@ void StraightTrajectory::BulletDetonateVelocityCheck(BulletClass* pBullet, House
 		auto largePace = static_cast<size_t>(std::max(cellPace.X, cellPace.Y));
 		const auto stepCoord = !largePace ? CoordStruct::Empty : (theTargetCoords - theSourceCoords) * (1.0 / largePace);
 		auto curCoord = theSourceCoords;
-		auto pCurCell = MapClass::Instance->GetCellAt(sourceCell);
+		auto pCurCell = MapClass::Instance.GetCellAt(sourceCell);
 		double cellDistance = locationDistance;
 
 		for (size_t i = 0; i < largePace; ++i)
 		{
-			if ((pType->SubjectToGround && (curCoord.Z + 16) < MapClass::Instance->GetCellFloorHeight(curCoord)) // Below ground level? (16 ->error range)
-				|| (pBullet->Type->SubjectToWalls && pCurCell->OverlayTypeIndex != -1 && OverlayTypeClass::Array->GetItem(pCurCell->OverlayTypeIndex)->Wall) // Impact on the wall?
+			if ((pType->SubjectToGround && (curCoord.Z + 16) < MapClass::Instance.GetCellFloorHeight(curCoord)) // Below ground level? (16 ->error range)
+				|| (pBullet->Type->SubjectToWalls && pCurCell->OverlayTypeIndex != -1 && OverlayTypeClass::Array.GetItem(pCurCell->OverlayTypeIndex)->Wall) // Impact on the wall?
 				|| (checkThrough && this->CheckThroughAndSubjectInCell(pBullet, pCurCell, pOwner))) // Blocked by obstacles?
 			{
 				velocityCheck = true;
@@ -597,7 +597,7 @@ void StraightTrajectory::BulletDetonateVelocityCheck(BulletClass* pBullet, House
 			}
 
 			curCoord += stepCoord;
-			pCurCell = MapClass::Instance->GetCellAt(curCoord);
+			pCurCell = MapClass::Instance.GetCellAt(curCoord);
 		}
 
 		locationDistance = cellDistance;
@@ -756,7 +756,7 @@ void StraightTrajectory::PassWithDetonateAt(BulletClass* pBullet, HouseClass* pO
 
 		// Whether to detonate at ground level?
 		if (pType->PassDetonateLocal)
-			detonateCoords.Z = MapClass::Instance->GetCellFloorHeight(detonateCoords);
+			detonateCoords.Z = MapClass::Instance.GetCellFloorHeight(detonateCoords);
 
 		const auto damage = this->GetTheTrueDamage(this->PassDetonateDamage, pBullet, nullptr, false);
 		WarheadTypeExt::DetonateAt(pWH, detonateCoords, pBullet->Owner, damage, pOwner);
@@ -847,7 +847,7 @@ void StraightTrajectory::PrepareForDetonateAt(BulletClass* pBullet, HouseClass* 
 	if (pType->ProximityFlight)
 	{
 		const auto airTracker = &AircraftTrackerClass::Instance;
-		airTracker->FillCurrentVector(MapClass::Instance->GetCellAt(pBullet->Location + velocityCrd * 0.5),
+		airTracker->FillCurrentVector(MapClass::Instance.GetCellAt(pBullet->Location + velocityCrd * 0.5),
 			Game::F2I(sqrt(radius * radius + (velocitySq / 4)) / Unsorted::LeptonsPerCell));
 
 		for (auto pTechno = airTracker->Get(); pTechno; pTechno = airTracker->Get())
@@ -1032,9 +1032,9 @@ bool StraightTrajectory::PassAndConfineAtHeight(BulletClass* pBullet)
 		pBullet->Location.Z + static_cast<int>(pBullet->Velocity.Z)
 	};
 
-	auto checkDifference = MapClass::Instance->GetCellFloorHeight(futureCoords) - futureCoords.Z;
+	auto checkDifference = MapClass::Instance.GetCellFloorHeight(futureCoords) - futureCoords.Z;
 
-	if (MapClass::Instance->GetCellAt(futureCoords)->ContainsBridge())
+	if (MapClass::Instance.GetCellAt(futureCoords)->ContainsBridge())
 	{
 		const auto differenceOnBridge = checkDifference + CellClass::BridgeHeight;
 
--- src/Ext/House/Hooks.cpp
+++ src/Ext/House/Hooks.cpp
@@ -76,11 +76,11 @@ DEFINE_HOOK(0x508D8D, HouseClass_UpdatePower_Techno, 0x6)
 				pThis->PowerDrain -= pExt->Power * count;
 	};
 
-	for (const auto pType : *InfantryTypeClass::Array)
+	for (const auto pType : InfantryTypeClass::Array)
 		updateDrainForThisType(pType);
-	for (const auto pType : *UnitTypeClass::Array)
+	for (const auto pType : UnitTypeClass::Array)
 		updateDrainForThisType(pType);
-	for (const auto pType : *AircraftTypeClass::Array)
+	for (const auto pType : AircraftTypeClass::Array)
 		updateDrainForThisType(pType);
 	// Don't do this for buildings, they've already been counted.
 
@@ -129,7 +129,7 @@ DEFINE_HOOK(0x4FD1CD, HouseClass_RecalcCenter_LimboDelivery, 0x6)
 
 	auto const pExt = RecalcCenterTemp::pExtData;
 
-	if (!MapClass::Instance->CoordinatesLegal(pBuilding->GetMapCoords())
+	if (!MapClass::Instance.CoordinatesLegal(pBuilding->GetMapCoords())
 		|| (pExt && pExt->OwnsLimboDeliveredBuilding(pBuilding)))
 	{
 		return R->Origin() == 0x4FD1CD ? SkipBuilding1 : SkipBuilding2;
@@ -144,7 +144,7 @@ DEFINE_HOOK(0x4AC534, DisplayClass_ComputeStartPosition_IllegalCoords, 0x6)
 
 	GET(TechnoClass* const, pTechno, ECX);
 
-	if (!MapClass::Instance->CoordinatesLegal(pTechno->GetMapCoords()))
+	if (!MapClass::Instance.CoordinatesLegal(pTechno->GetMapCoords()))
 		return SkipTechno;
 
 	return 0;
@@ -164,7 +164,7 @@ namespace LimboTrackingTemp
 
 DEFINE_HOOK(0x687B18, ScenarioClass_ReadINI_StartTracking, 0x7)
 {
-	for (auto const pTechno : *TechnoClass::Array())
+	for (auto const pTechno : TechnoClass::Array)
 	{
 		auto const pType = pTechno->GetTechnoType();
 
