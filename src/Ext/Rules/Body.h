b75c8f06ec7914f2701c113b014e518ab846d601
绯红热茶 <335958461@qq.com>
2025-03-22 15:10:07 +0800

    [Customized] Delay automatic attack on the controlled unit (#1474)
    - Now you can make the techno that has just been mind controlled not be
    automatically attacked by its original friendly forces for a period of
    time defined by `MindControl.ThreatDelay` on the mind control warhead,
    default to `[General]->MindControl.ThreatDelay`. This will not affect
    the manual selection of attacks and is useless with permanent mind
    control.
    
    In `rulesmd.ini`:
    ```ini
    [General]
    MindControl.ThreatDelay=0     ; integer, game frames
    
    [SOMEWARHEAD]                 ; Warhead
    MindControl.ThreatDelay=      ; integer, game frames
    ```

--- src/Ext/Rules/Body.h
+++ src/Ext/Rules/Body.h
@@ -175,6 +175,8 @@ public:
 		// Nullable<Vector3D<float>> VoxelShadowLightSource;
 		Valueable<bool> UseFixedVoxelLighting;
 
+		Valueable<int> MindControl_ThreatDelay;
+
 		Valueable<bool> RecountBurst;
 		Valueable<bool> NoRearm_UnderEMP;
 		Valueable<bool> NoRearm_Temporal;
@@ -325,6 +327,7 @@ public:
 			, CombatAlert_UseAttackVoice { true }
 			, CombatAlert_UseEVA { true }
 			, UseFixedVoxelLighting { false }
+			, MindControl_ThreatDelay { 0 }
 			, RecountBurst { false }
 			, NoRearm_UnderEMP { false }
 			, NoRearm_Temporal { false }
