f0509c8413b15edcb68acaca96e9897d26e571ea
Starkku <starkku.ppmod@gmail.com>
2025-04-02 19:26:31 +0300

    Fix another EBolt save/load issue

--- src/Ext/Techno/Body.cpp
+++ src/Ext/Techno/Body.cpp
@@ -41,11 +41,6 @@ TechnoExt::ExtData::~ExtData()
 		vec.erase(std::remove(vec.begin(), vec.end(), pThis), vec.end());
 	}
 
-	this->ClearElectricBolts();
-}
-
-void TechnoExt::ExtData::ClearElectricBolts()
-{
 	for (auto const pBolt : this->ElectricBolts)
 	{
 		pBolt->Owner = nullptr;
@@ -609,8 +604,6 @@ void TechnoExt::ExtData::SaveToStream(PhobosStreamWriter& Stm)
 {
 	Extension<TechnoClass>::SaveToStream(Stm);
 	this->Serialize(Stm);
-
-	this->ClearElectricBolts();
 }
 
 bool TechnoExt::LoadGlobals(PhobosStreamReader& Stm)
