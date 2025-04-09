085afa7483db6f379fe41dfbe085dd797768734a
NetsuNegi39 <negi542554365@outlook.com>
2025-04-09 17:25:02 +0800

    Fix the bug that `EnterGrinderSound` on technotype does not used

--- CREDITS.md
+++ CREDITS.md
@@ -366,7 +366,7 @@ This page lists all the individual contributions to the project by their author.
    - Allow voxel projectiles to use `AnimPalette` and `FirersPalette`
    - Customize damaged speed ratio of drive/ship loco
    - Customize overpower logic
-   - Fix the bug that `EnterBioReactorSound` and `LeaveBioReactorSound` on technotype does not used
+   - Fix the bug that `EnterBioReactorSound`, `LeaveBioReactorSound`, `EnterGrinderSound` on technotype does not used
    - Fix the bug that harvester dont stop unloading and cannot unload cargos anymore when lifting by `IsLocomotor=yes` warhead
 - **Apollo** - Translucent SHP drawing patches
 - **ststl**:
--- src/Misc/Hooks.BugFixes.cpp
+++ src/Misc/Hooks.BugFixes.cpp
@@ -1764,12 +1764,27 @@ DEFINE_HOOK(0x4DFB28, FootClass_FindGrinder_CheckValid, 0x8)
 
 #pragma endregion
 
+DEFINE_HOOK_AGAIN(0x73A1D3, FootClass_UpdatePosition_EnterGrinderSound, 0x6)// UnitClass
+DEFINE_HOOK(0x5198C3, FootClass_UpdatePosition_EnterGrinderSound, 0x6)// InfantryClass
+{
+	GET(BuildingClass*, pGrinder, EBX);
+	const int enterSound = pGrinder->Type->EnterGrinderSound;
+
+	if (enterSound >= 0)
+	{
+		R->ECX(enterSound);
+		return R->Origin() + 0x6;
+	}
+
+	return 0;
+}
+
 DEFINE_HOOK(0x51A304, InfantryClass_UpdatePosition_EnterBioReactorSound, 0x6)
 {
 	enum { SkipGameCode = 0x51A30A };
 
-	GET(BuildingClass*, pThis, EDI);
-	const int enterSound = pThis->Type->EnterBioReactorSound;
+	GET(BuildingClass*, pReactor, EDI);
+	const int enterSound = pReactor->Type->EnterBioReactorSound;
 
 	if (enterSound >= 0)
 	{
@@ -1784,8 +1799,8 @@ DEFINE_HOOK(0x44DBCF, BuildingClass_Mission_Unload_LeaveBioReactorSound, 0x6)
 {
 	enum { SkipGameCode = 0x44DBD5 };
 
-	GET(BuildingClass*, pThis, EBP);
-	const int leaveSound = pThis->Type->LeaveBioReactorSound;
+	GET(BuildingClass*, pReactor, EBP);
+	const int leaveSound = pReactor->Type->LeaveBioReactorSound;
 
 	if (leaveSound >= 0)
 	{
