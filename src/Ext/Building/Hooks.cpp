Phobos-developers/Phobos

commit bac236462d7d50525a67db0ac554492b59ae709b

Fix Ares3.0p1 BuildingAnim issue (#1588)

src/Ext/Building/Hooks.cpp
@@ -422,6 +422,15 @@ DEFINE_HOOK(0x445D87, BuildingClass_Limbo_DestroyableObstacle, 0x6)
 	if (pTypeExt->IsDestroyableObstacle)
 		RecalculateCells<true>(pThis);
 
+	for (auto& bAnim : pThis->Anims)
+	{
+		if (bAnim && VTable::Get(bAnim) == 0x7E3354)
+		{
+			bAnim->UnInit();
+			bAnim = nullptr;
+		}
+	}
+
 	return 0;
 }
 

src/Misc/Hooks.Ares.cpp
@@ -7,24 +7,6 @@
 
 #include <Ext/Sidebar/Body.h>
 
-// In vanilla YR, game destroys building animations directly by calling constructor.
-// Ares changed this to call UnInit() which has a consequence of doing pointer invalidation on the AnimClass pointer.
-// This notably causes an issue with Grinder that restores ActiveAnim if the building is sold/destroyed while SpecialAnim is playing even if the building is gone or in limbo.
-// Now it does not do this if the building is in limbo, which covers all cases from being destroyed, sold, to erased by Temporal weapons.
-// There is another potential case for this with ProductionAnim & IdleAnim which is also patched here just in case.
-DEFINE_HOOK_AGAIN(0x44E997, BuildingClass_Detach_RestoreAnims, 0x6)
-DEFINE_HOOK(0x44E9FA, BuildingClass_Detach_RestoreAnims, 0x6)
-{
-	enum { SkipAnimOne = 0x44E9A4, SkipAnimTwo = 0x44EA07 };
-
-	GET(BuildingClass*, pThis, ESI);
-
-	if (pThis->InLimbo)
-		return R->Origin() == 0x44E997 ? SkipAnimOne : SkipAnimTwo;
-
-	return 0;
-}
-
 // Remember that we still don't fix Ares "issues" a priori. Extensions as well.
 // Patches presented here are exceptions rather that the rule. They must be short, concise and correct.
 // DO NOT POLLUTE ISSUEs and PRs.

commit c00e2601e7a1c3c0cbbd0553b75dba4d2fabcf9d

[Minor] Fix building anims being removed after Chronoshift (#1649)
the issue is introduced by https://github.com/Phobos-developers/Phobos/pull/1588. This might fix it but need testing

src/Ext/Building/Hooks.cpp
@@ -422,6 +422,10 @@ DEFINE_HOOK(0x445D87, BuildingClass_Limbo_DestroyableObstacle, 0x6)
 	if (pTypeExt->IsDestroyableObstacle)
 		RecalculateCells<true>(pThis);
 
+	// only remove animation when the building is destroyed or sold
+	if (pThis->Health > 0 && pThis->IsAlive && pThis->GetCurrentMission() != Mission::Selling)
+		return 0;
+
 	for (auto& bAnim : pThis->Anims)
 	{
 		if (bAnim && VTable::Get(bAnim) == 0x7E3354)