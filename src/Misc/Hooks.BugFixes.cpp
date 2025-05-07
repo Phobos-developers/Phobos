Phobos-developers/Phobos

commit f501559924b6130788d4a9c893fdab0d607bd29d

[Customized] Fast access vehicle (#1475)
- Now you can let passengers quickly enter or leave the transport
vehicles (useless for buildings) without queuing. Defaults to
`[General]` -> `NoQueueUpToEnter` or `NoQueueUpToUnload`.

In `rulesmd.ini`:
```ini
[General]
NoQueueUpToEnter=false    ; boolean
NoQueueUpToUnload=false   ; boolean

[SOMEUNIT]                ; UnitType
NoQueueUpToEnter=         ; boolean
NoQueueUpToUnload=        ; boolean
```

---------

Co-authored-by: Noble Fish <89088785+DeathFishAtEase@users.noreply.github.com>

src/Misc/Hooks.BugFixes.cpp
@@ -1163,6 +1163,9 @@ DEFINE_HOOK(0x4C75DA, EventClass_RespondToEvent_Stop, 0x6)
 	// Clearing the current target should still be necessary for all technos
 	pTechno->SetTarget(nullptr);
 
+	// Stop any enter action
+	pTechno->QueueUpToEnter = nullptr;
+
 	if (commonAircraft)
 	{
 		if (pAircraft->Type->AirportBound)
@@ -1218,8 +1221,27 @@ DEFINE_HOOK(0x4C75DA, EventClass_RespondToEvent_Stop, 0x6)
 
 #pragma region UntetherFix
 
-// Radio: do not untether techno who have other tether link
-DEFINE_HOOK(0x6F4BB3, TechnoClass_ReceiveCommand_NotifyUnlink, 0x7)
+// Change enter to move when unlink
+DEFINE_HOOK(0x6F4C50, TechnoClass_ReceiveCommand_NotifyUnlink, 0x6)
+{
+	GET(TechnoClass* const, pThis, ESI);
+	GET_STACK(TechnoClass* const, pCall, STACK_OFFSET(0x18, 0x4));
+	// If the link connection is cancelled and foot A is entering techno B, it may cause A and B to overlap
+	if (!pCall->InLimbo // Has not already entered
+		&& (pCall->AbstractFlags & AbstractFlags::Foot) // Is foot
+		&& pCall->CurrentMission == Mission::Enter // Is entering
+		&& static_cast<FootClass*>(pCall)->Destination == pThis) // Is entering techno B
+	{
+		pCall->SetDestination(pThis->GetCell(), false); // Set the destination at its feet
+		pCall->QueueMission(Mission::Move, false); // Replace entering with moving
+		pCall->NextMission(); // Immediately respond to the Mission::Move
+	}
+
+	return 0;
+}
+
+// Do not untether techno who have other tether link
+DEFINE_HOOK(0x6F4BB3, TechnoClass_ReceiveCommand_RequestUntether, 0x7)
 {
 	// Place the hook after processing to prevent functions from calling each other and getting stuck in a dead loop.
 	GET(TechnoClass* const, pThis, ESI);

src/Ext/Rules/Body.cpp
@@ -139,6 +139,8 @@ void RulesExt::ExtData::LoadBeforeTypeData(RulesClass* pThis, CCINIClass* pINI)
 	this->HeightShadowScaling_MinScale.Read(exINI, GameStrings::AudioVisual, "HeightShadowScaling.MinScale");
 
 	this->ExtendedAircraftMissions.Read(exINI, GameStrings::General, "ExtendedAircraftMissions");
+	this->NoQueueUpToEnter.Read(exINI, GameStrings::General, "NoQueueUpToEnter");
+	this->NoQueueUpToUnload.Read(exINI, GameStrings::General, "NoQueueUpToUnload");
 
 	this->BuildingProductionQueue.Read(exINI, GameStrings::General, "BuildingProductionQueue");
 
@@ -364,6 +366,8 @@ void RulesExt::ExtData::Serialize(T& Stm)
 		.Process(this->HeightShadowScaling)
 		.Process(this->HeightShadowScaling_MinScale)
 		.Process(this->ExtendedAircraftMissions)
+		.Process(this->NoQueueUpToEnter)
+		.Process(this->NoQueueUpToUnload)
 		.Process(this->BuildingProductionQueue)
 		.Process(this->AllowParallelAIQueues)
 		.Process(this->ForbidParallelAIQueues_Aircraft)

src/Ext/Rules/Body.h
@@ -93,6 +93,8 @@ public:
 		double AirShadowBaseScale_log;
 
 		Valueable<bool> ExtendedAircraftMissions;
+		Valueable<bool> NoQueueUpToEnter;
+		Valueable<bool> NoQueueUpToUnload;
 
 		Valueable<bool> BuildingProductionQueue;
 
@@ -256,6 +258,8 @@ public:
 			, AirShadowBaseScale_log { 0.693376137 }
 
 			, ExtendedAircraftMissions { false }
+			, NoQueueUpToEnter { false }
+			, NoQueueUpToUnload { false }
 
 			, BuildingProductionQueue { false }
 

src/Ext/Techno/Hooks.Transport.cpp
@@ -1,4 +1,5 @@
 #include "Body.h"
+#include <HoverLocomotionClass.h>
 
 #include <Ext/Scenario/Body.h>
 
@@ -220,3 +221,248 @@ DEFINE_HOOK(0x710552, TechnoClass_SetOpenTransportCargoTarget_ShareTarget, 0x6)
 
 	return 0;
 }
+
+#pragma region NoQueueUpToEnterAndUnload
+
+// Rewrite from 0x73758A, replace send RadioCommand::QueryCanEnter
+bool __fastcall CanEnterNow(UnitClass* pTransport, FootClass* pPassenger)
+{
+	if (!pTransport->Owner->IsAlliedWith(pPassenger) || pTransport->IsBeingWarpedOut())
+		return false;
+
+	if (pPassenger->IsMindControlled() || pPassenger->ParasiteEatingMe)
+		return false;
+
+	const auto pManager = pPassenger->CaptureManager;
+
+	if (pManager && pManager->IsControllingSomething())
+		return false;
+
+	const auto pTransportType = pTransport->Type;
+	const auto bySize = TechnoTypeExt::ExtMap.Find(pTransportType)->Passengers_BySize;
+	const auto passengerSize = bySize ? Game::F2I(pPassenger->GetTechnoType()->Size) : 1;
+
+	if (passengerSize > Game::F2I(pTransportType->SizeLimit))
+		return false;
+
+	const auto maxSize = pTransportType->Passengers;
+	const auto predictSize = (bySize ? pTransport->Passengers.GetTotalSize() : pTransport->Passengers.NumPassengers) + passengerSize;
+	const auto pLink = pTransport->GetNthLink();
+	const auto needCalculate = pLink && pLink != pPassenger;
+
+	if (needCalculate)
+	{
+		const auto delta = pLink->GetCoords() - pTransport->GetCoords();
+
+		// When the most important passenger is close, need to prevent overlap
+		if (abs(delta.X) <= 384 && abs(delta.Y) <= 384)
+			return (predictSize <= (maxSize - (bySize ? Game::F2I(pLink->GetTechnoType()->Size) : 1)));
+	}
+
+	return predictSize < maxSize;
+}
+
+// Rewrite from 0x51A21B
+void __fastcall InfantryEnterNow(UnitClass* pTransport, InfantryClass* pPassenger)
+{
+	if (const auto pTag = pTransport->AttachedTag)
+		pTag->RaiseEvent(TriggerEvent::EnteredBy, pPassenger, CellStruct::Empty);
+
+	pPassenger->ArchiveTarget = nullptr;
+	pPassenger->OnBridge = false;
+	pPassenger->MissionAccumulateTime = 0;
+	pPassenger->GattlingValue = 0;
+	pPassenger->CurrentGattlingStage = 0;
+
+	/* Have checked in CanEnterNow
+	if (const auto pMind = pPassenger->MindControlledBy)
+	{
+		if (const auto pManager = pMind->CaptureManager)
+			pManager->FreeUnit(pPassenger);
+	}
+	*/
+
+	pPassenger->Limbo();
+
+	if (pTransport->Type->OpenTopped)
+		pTransport->EnteredOpenTopped(pPassenger);
+
+	pPassenger->Transporter = pTransport;
+	pTransport->AddPassenger(pPassenger);
+	pPassenger->Undiscover();
+
+	// Added, to prevent passengers from wanting to get on after getting off
+	pPassenger->QueueUpToEnter = nullptr;
+
+	// Added, to stop the passengers and let OpenTopped work normally
+	pPassenger->SetSpeedPercentage(0.0);
+
+	// Added, to stop hover unit's meaningless behavior
+	if (const auto pHover = locomotion_cast<HoverLocomotionClass*>(pPassenger->Locomotor))
+		pHover->MaxSpeed = 0;
+}
+
+// Rewrite from 0x73A6D1
+void __fastcall UnitEnterNow(UnitClass* pTransport, UnitClass* pPassenger)
+{
+	// I don't know why units have no trigger
+
+	pPassenger->ArchiveTarget = nullptr;
+	pPassenger->OnBridge = false;
+	pPassenger->MissionAccumulateTime = 0;
+	pPassenger->GattlingValue = 0;
+	pPassenger->CurrentGattlingStage = 0;
+
+	/* Have checked in CanEnterNow
+	if (const auto pMind = pPassenger->MindControlledBy)
+	{
+		if (const auto pManager = pMind->CaptureManager)
+			pManager->FreeUnit(pPassenger);
+	}
+	*/
+
+	pPassenger->Limbo();
+	pTransport->AddPassenger(pPassenger);
+
+	if (pTransport->Type->OpenTopped)
+		pTransport->EnteredOpenTopped(pPassenger);
+
+	pPassenger->Transporter = pTransport;
+
+	if (pPassenger->Type->OpenTopped)
+		pPassenger->SetTargetForPassengers(nullptr);
+
+	pPassenger->Undiscover();
+
+	// Added, to prevent passengers from wanting to get on after getting off
+	pPassenger->QueueUpToEnter = nullptr;
+
+	// Added, to stop the passengers and let OpenTopped work normally
+	pPassenger->SetSpeedPercentage(0.0);
+
+	// Added, to stop hover unit's meaningless behavior
+	if (const auto pHover = locomotion_cast<HoverLocomotionClass*>(pPassenger->Locomotor))
+		pHover->MaxSpeed = 0;
+}
+
+DEFINE_HOOK(0x51A0D4, InfantryClass_UpdatePosition_NoQueueUpToEnter, 0x6)
+{
+	enum { EnteredThenReturn = 0x51A47E };
+
+	GET(InfantryClass* const, pThis, ESI);
+
+	if (const auto pDest = abstract_cast<UnitClass*>(pThis->CurrentMission == Mission::Enter ? pThis->Destination : pThis->QueueUpToEnter))
+	{
+		if (pDest->Type->Passengers > 0 && TechnoTypeExt::ExtMap.Find(pDest->Type)->NoQueueUpToEnter.Get(RulesExt::Global()->NoQueueUpToEnter))
+		{
+			const auto delta = pThis->GetCoords() - pDest->GetCoords();
+
+			if (abs(delta.X) <= 384 && abs(delta.Y) <= 384)
+			{
+				if (CanEnterNow(pDest, pThis))
+				{
+					InfantryEnterNow(pDest, pThis);
+					return EnteredThenReturn;
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
+DEFINE_HOOK(0x73A5EA, UnitClass_UpdatePosition_NoQueueUpToEnter, 0x5)
+{
+	enum { EnteredThenReturn = 0x73A78C };
+
+	GET(UnitClass* const, pThis, EBP);
+
+	if (const auto pDest = abstract_cast<UnitClass*>(pThis->CurrentMission == Mission::Enter ? pThis->Destination : pThis->QueueUpToEnter))
+	{
+		if (pDest->Type->Passengers > 0 && TechnoTypeExt::ExtMap.Find(pDest->Type)->NoQueueUpToEnter.Get(RulesExt::Global()->NoQueueUpToEnter))
+		{
+			const auto delta = pThis->GetCoords() - pDest->GetCoords();
+
+			if (abs(delta.X) <= 384 && abs(delta.Y) <= 384)
+			{
+				if (CanEnterNow(pDest, pThis))
+				{
+					UnitEnterNow(pDest, pThis);
+					return EnteredThenReturn;
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
+DEFINE_HOOK(0x70D957, FootClass_QueueEnter_ForceMoving, 0x6)
+{
+	GET(FootClass* const, pThis, ESI);
+
+	const auto pDest = abstract_cast<UnitClass*>(pThis->QueueUpToEnter);
+
+	if (pDest && TechnoTypeExt::ExtMap.Find(pDest->Type)->NoQueueUpToEnter.Get(RulesExt::Global()->NoQueueUpToEnter))
+	{
+		const auto absType = pThis->WhatAmI();
+		// When the distance is very close, the passengers may not move, which can cause UpdatePosition to not be called
+		// So special handling is needed here, avoid not being able to trigger quick boarding without moving
+		if (absType == AbstractType::Infantry)
+		{
+			if (pThis->GetMapCoords().DistanceFromSquared(pDest->GetMapCoords()) <= 2 && CanEnterNow(pDest, pThis))
+				InfantryEnterNow(pDest, static_cast<InfantryClass*>(pThis));
+		}
+		else if (absType == AbstractType::Unit)
+		{
+			if (pThis->GetMapCoords().DistanceFromSquared(pDest->GetMapCoords()) <= 2 && CanEnterNow(pDest, pThis))
+				UnitEnterNow(pDest, static_cast<UnitClass*>(pThis));
+		}
+	}
+
+	return 0;
+}
+
+DEFINE_HOOK(0x73DC9C, UnitClass_Mission_Unload_NoQueueUpToUnloadBreak, 0xA)
+{
+	enum { SkipGameCode = 0x73E289 };
+
+	GET(UnitClass* const, pThis, ESI);
+	GET(FootClass* const, pPassenger, EDI);
+
+	// Restore vanilla function
+	pPassenger->Undiscover();
+
+	// Play the sound when interrupted for some reason
+	if (TechnoTypeExt::ExtMap.Find(pThis->Type)->NoQueueUpToUnload.Get(RulesExt::Global()->NoQueueUpToUnload))
+		VoxClass::PlayAtPos(pThis->Type->LeaveTransportSound, &pThis->Location);
+
+	return SkipGameCode;
+}
+
+DEFINE_HOOK(0x73DC1E, UnitClass_Mission_Unload_NoQueueUpToUnloadLoop, 0xA)
+{
+	enum { UnloadLoop = 0x73D8CB, UnloadReturn = 0x73E289 };
+
+	GET(UnitClass* const, pThis, ESI);
+
+	if (TechnoTypeExt::ExtMap.Find(pThis->Type)->NoQueueUpToUnload.Get(RulesExt::Global()->NoQueueUpToUnload))
+	{
+		if (pThis->Passengers.NumPassengers <= pThis->NonPassengerCount)
+		{
+			// If unloading is required within one frame, the sound will only be played when the last passenger leaves
+			VoxClass::PlayAtPos(pThis->Type->LeaveTransportSound, &pThis->Location);
+			pThis->MissionStatus = 4;
+			return UnloadReturn;
+		}
+
+		R->EBX(0); // Reset
+		return UnloadLoop;
+	}
+
+	// PlayAtPos has already handled the situation where Sound is less than 0 internally, so unnecessary checks will be skipped
+	VoxClass::PlayAtPos(pThis->Type->LeaveTransportSound, &pThis->Location);
+	return UnloadReturn;
+}
+
+#pragma endregion

src/Ext/TechnoType/Body.cpp
@@ -463,6 +463,9 @@ void TechnoTypeExt::ExtData::LoadFromINIFile(CCINIClass* const pINI)
 	this->BuildLimitGroup_ExtraLimit_MaxCount.Read(exINI, pSection, "BuildLimitGroup.ExtraLimit.MaxCount");
 	this->BuildLimitGroup_ExtraLimit_MaxNum.Read(exINI, pSection, "BuildLimitGroup.ExtraLimit.MaxNum");
 
+	this->NoQueueUpToEnter.Read(exINI, pSection, "NoQueueUpToEnter");
+	this->NoQueueUpToUnload.Read(exINI, pSection, "NoQueueUpToUnload");
+
 	this->RateDown_Delay.Read(exINI, pSection, "RateDown.Delay");
 	this->RateDown_Reset.Read(exINI, pSection, "RateDown.Reset");
 	this->RateDown_Cover_Value.Read(exINI, pSection, "RateDown.Cover.Value");
@@ -514,6 +517,9 @@ void TechnoTypeExt::ExtData::LoadFromINIFile(CCINIClass* const pINI)
 	// Ares 0.C
 	this->NoAmmoWeapon.Read(exINI, pSection, "NoAmmoWeapon");
 	this->NoAmmoAmount.Read(exINI, pSection, "NoAmmoAmount");
+
+	// Ares 2.0
+	this->Passengers_BySize.Read(exINI, pSection, "Passengers.BySize");
 
 	char tempBuffer[32];
 
@@ -877,6 +883,10 @@ void TechnoTypeExt::ExtData::Serialize(T& Stm)
 		.Process(this->BuildLimitGroup_ExtraLimit_MaxCount)
 		.Process(this->BuildLimitGroup_ExtraLimit_MaxNum)
 
+		.Process(this->NoQueueUpToEnter)
+		.Process(this->NoQueueUpToUnload)
+		.Process(this->Passengers_BySize)
+
 		.Process(this->RateDown_Delay)
 		.Process(this->RateDown_Reset)
 		.Process(this->RateDown_Cover_Value)
@@ -897,7 +907,7 @@ void TechnoTypeExt::ExtData::Serialize(T& Stm)
 		.Process(this->KeepTargetOnMove_ExtraDistance)
 
 		.Process(this->Power)
-		
+
     	.Process(this->Image_ConditionYellow)
 		.Process(this->Image_ConditionRed)
 		.Process(this->WaterImage_ConditionYellow)
@@ -905,7 +915,7 @@ void TechnoTypeExt::ExtData::Serialize(T& Stm)
 
 		.Process(this->InitialSpawnsNumber)
 		.Process(this->Spawns_Queue)
-		
+
 		.Process(this->Spawner_RecycleRange)
 		.Process(this->Spawner_RecycleAnim)
 		.Process(this->Spawner_RecycleCoord)

src/Ext/TechnoType/Body.h
@@ -236,6 +236,10 @@ public:
 		ValueableVector<int> BuildLimitGroup_ExtraLimit_MaxCount;
 		Valueable<int> BuildLimitGroup_ExtraLimit_MaxNum;
 
+		Nullable<bool> NoQueueUpToEnter;
+		Nullable<bool> NoQueueUpToUnload;
+		Valueable<bool> Passengers_BySize;
+
 		Valueable<int> RateDown_Delay;
 		Valueable<bool> RateDown_Reset;
 		Valueable<int> RateDown_Cover_Value;
@@ -273,7 +277,7 @@ public:
 		Nullable<bool> Sinkable;
 		Valueable<bool> Sinkable_SquidGrab;
 		Valueable<int> SinkSpeed;
-		
+
 		struct LaserTrailDataEntry
 		{
 			ValueableIdx<LaserTrailTypeClass> idxType;
@@ -503,6 +507,10 @@ public:
 			, BuildLimitGroup_ExtraLimit_MaxCount {}
 			, BuildLimitGroup_ExtraLimit_MaxNum { 0 }
 
+			, NoQueueUpToEnter {}
+			, NoQueueUpToUnload {}
+			, Passengers_BySize { true }
+
 			, RateDown_Delay { 0 }
 			, RateDown_Reset { false }
 			, RateDown_Cover_Value { 0 }
@@ -531,12 +539,12 @@ public:
 
 			, InitialSpawnsNumber { }
 			, Spawns_Queue { }
-			
+
 			, Spawner_RecycleRange { Leptons(-1) }
 			, Spawner_RecycleAnim { }
 			, Spawner_RecycleCoord { {0,0,0} }
 			, Spawner_RecycleOnTurret { false }
-			
+
 			, Sinkable { }
 			, Sinkable_SquidGrab { true }
 			, SinkSpeed { 5 }

commit 03e42a101fd34bf6b2b57fe68da599edf110c253

Fix f501559 caused the aircraft to sometimes hover over the airport due to continuous command reception

src/Misc/Hooks.BugFixes.cpp
@@ -1252,7 +1252,9 @@ DEFINE_HOOK(0x6F4C50, TechnoClass_ReceiveCommand_NotifyUnlink, 0x6)
 	if (!pCall->InLimbo // Has not already entered
 		&& (pCall->AbstractFlags & AbstractFlags::Foot) // Is foot
 		&& pCall->CurrentMission == Mission::Enter // Is entering
-		&& static_cast<FootClass*>(pCall)->Destination == pThis) // Is entering techno B
+		&& static_cast<FootClass*>(pCall)->Destination == pThis // Is entering techno B
+		&& pCall->WhatAmI() != AbstractType::Aircraft // Not aircraft
+		&& pThis->GetTechnoType()->Passengers > 0) // Have passenger seats
 	{
 		pCall->SetDestination(pThis->GetCell(), false); // Set the destination at its feet
 		pCall->QueueMission(Mission::Move, false); // Replace entering with moving