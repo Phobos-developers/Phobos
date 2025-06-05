a354eb1fde20328fbc0edbd797eef3c778bea739
Netsu_Negi <71598172+NetsuNegi@users.noreply.github.com>
2025-06-05 21:52:27 +0800

    Remove excess spaces

--- src/Ext/Rules/Body.cpp
+++ src/Ext/Rules/Body.cpp
@@ -271,7 +271,7 @@ void RulesExt::ExtData::LoadBeforeTypeData(RulesClass* pThis, CCINIClass* pINI)
 	this->DamagedSpeed.Read(exINI, GameStrings::General, "DamagedSpeed");
 
 	this->HarvesterScanAfterUnload.Read(exINI, GameStrings::General, "HarvesterScanAfterUnload");
-	
+
 	// Section AITargetTypes
 	int itemsCount = pINI->GetKeyCount("AITargetTypes");
 	for (int i = 0; i < itemsCount; ++i)
