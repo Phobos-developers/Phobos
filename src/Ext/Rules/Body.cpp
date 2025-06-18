5ccb6361097e6ddab1c2fb6ae8dd9c2e8ef79885
CrimRecya <335958461@qq.com>
2025-06-19 01:47:12 +0800

    More fix

--- src/Ext/Rules/Body.cpp
+++ src/Ext/Rules/Body.cpp
@@ -77,11 +77,6 @@ void RulesExt::ExtData::LoadFromINIFile(CCINIClass* pINI)
 
 void RulesExt::ExtData::LoadBeforeTypeData(RulesClass* pThis, CCINIClass* pINI)
 {
-	RulesExt::ExtData* pData = RulesExt::Global();
-
-	if (!pData)
-		return;
-
 	INI_EX exINI(pINI);
 
 	this->Storage_TiberiumIndex.Read(exINI, GameStrings::General, "Storage.TiberiumIndex");
@@ -327,12 +322,6 @@ void RulesExt::ExtData::LoadBeforeTypeData(RulesClass* pThis, CCINIClass* pINI)
 	}
 }
 
-// this runs between the before and after type data loading methods for rules ini
-void RulesExt::ExtData::InitializeAfterTypeData(RulesClass* const pThis)
-{
-
-}
-
 // this should load everything that TypeData is not dependant on
 // i.e. InfantryElectrocuted= can go here since nothing refers to it
 // but [GenericPrerequisites] have to go earlier because they're used in parsing TypeData
@@ -340,10 +329,22 @@ void RulesExt::ExtData::LoadAfterTypeData(RulesClass* pThis, CCINIClass* pINI)
 {
 	INI_EX exINI(pINI);
 
+}
+
+// this runs between the before and after type data loading methods for rules ini
+void RulesExt::ExtData::InitializeAfterTypeData(RulesClass* const pThis)
+{
+
+}
+
+void RulesExt::ExtData::InitializeAfterAllLoaded()
+{
+	const auto pRules = RulesClass::Instance;
+
 	// tint color
-	this->TintColorIronCurtain = GeneralUtils::GetColorFromColorAdd(RulesClass::Instance->IronCurtainColor);
-	this->TintColorForceShield = GeneralUtils::GetColorFromColorAdd(RulesClass::Instance->ForceShieldColor);
-	this->TintColorBerserk = GeneralUtils::GetColorFromColorAdd(RulesClass::Instance->BerserkColor);
+	this->TintColorIronCurtain = GeneralUtils::GetColorFromColorAdd(pRules->IronCurtainColor);
+	this->TintColorForceShield = GeneralUtils::GetColorFromColorAdd(pRules->ForceShieldColor);
+	this->TintColorBerserk = GeneralUtils::GetColorFromColorAdd(pRules->BerserkColor);
 }
 
 // =============================
@@ -662,6 +663,12 @@ DEFINE_HOOK(0x679CAF, RulesData_LoadAfterTypeData, 0x5)
 
 	RulesExt::LoadAfterTypeData(pItem, pINI);
 
+	return 0;
+}
+
+DEFINE_HOOK(0x668F6A, RulesData_InitializeAfterAllLoaded, 0x5)
+{
+	RulesExt::Global()->InitializeAfterAllLoaded();
 	return 0;
 }
 
