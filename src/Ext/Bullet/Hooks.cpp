9c430a6e322ffd9ae58a948cbca4af55297e9e78
Starkku <starkku.ppmod@gmail.com>
2025-07-06 22:04:41 +0300

    Fix a fuckup in f2dd14f93a4c39b7aebdfe3ddde9716538287259

--- src/Ext/Bullet/Hooks.cpp
+++ src/Ext/Bullet/Hooks.cpp
@@ -534,7 +534,7 @@ DEFINE_HOOK(0x5F5A62, ObjectClass_SpawnParachuted_BombParachute, 0x5)
 	if (pAnimType)
 	{
 		pAnim = GameCreate<AnimClass>(pAnimType, *coords);
-		pThis->Owner ? pThis->Owner->Owner : BulletExt::ExtMap.Find(pThis)->FirerHouse;
+		pAnim->Owner = pThis->Owner ? pThis->Owner->Owner : BulletExt::ExtMap.Find(pThis)->FirerHouse;
 		int schemeIndex = pAnim->Owner ? pAnim->Owner->ColorSchemeIndex : RulesExt::Global()->AnimRemapDefaultColorScheme;
 		pAnim->LightConvert = ColorScheme::Array[schemeIndex]->LightConvert;
 		pThis->Parachute = pAnim;
