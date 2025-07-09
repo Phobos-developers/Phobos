Phobos-developers/Phobos

commit b207f0686afcf9c66cb88f831a53b1452bfc3ede

Restore previous AirstrikeTargets default value and add migration notice for target eligibility checks as they only affect mod logic

src/Ext/Techno/Hooks.Airstrike.cpp
@@ -23,13 +23,10 @@ DEFINE_HOOK(0x6F348F, TechnoClass_WhatWeaponShouldIUse_Airstrike, 0x7)
 		return Primary;
 
 	const auto pTargetType = pTargetTechno->GetTechnoType();
+	const auto pTargetTypeExt = TechnoTypeExt::ExtMap.Find(pTargetType);
 
 	if (pTargetTechno->AbstractFlags & AbstractFlags::Foot)
-	{
-		const auto pTargetTypeExt = TechnoTypeExt::ExtMap.Find(pTargetType);
-
 		return pTargetTypeExt->AllowAirstrike.Get(true) ? Secondary : Primary;
-	}
 
 	const auto pTargetTypeExt = TechnoTypeExt::ExtMap.Find(pTargetType);
 

src/Ext/WarheadType/Body.h
@@ -365,7 +365,7 @@ public:
 
 			, ElectricAssaultLevel { 1 }
 
-			, AirstrikeTargets { AffectedTarget::All }
+			, AirstrikeTargets { AffectedTarget::Building }
 
 			, AffectsBelowPercent { 1.0 }
 			, AffectsAbovePercent { 0.0 }

commit 1675d7aa28d40991a327e7e4998d7f08e1018996

Fix build error introduced in b207f0686afcf9c66cb88f831a53b1452bfc3ede

src/Ext/Techno/Hooks.Airstrike.cpp
@@ -28,8 +28,6 @@ DEFINE_HOOK(0x6F348F, TechnoClass_WhatWeaponShouldIUse_Airstrike, 0x7)
 	if (pTargetTechno->AbstractFlags & AbstractFlags::Foot)
 		return pTargetTypeExt->AllowAirstrike.Get(true) ? Secondary : Primary;
 
-	const auto pTargetTypeExt = TechnoTypeExt::ExtMap.Find(pTargetType);
-
 	return pTargetTypeExt->AllowAirstrike.Get(static_cast<BuildingTypeClass*>(pTargetType)->CanC4) && (!pTargetType->ResourceDestination || !pTargetType->ResourceGatherer) ? Secondary : Primary;
 }
 