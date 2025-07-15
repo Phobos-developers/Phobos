ef5ae49d9918ae3f3dbac2b17dcd2da1aeab08cd
航味麻酱 <93972760+TaranDahl@users.noreply.github.com>
2025-07-16 00:02:27 +0800

    Add KeepRange.MaxRearmTimerAllowed to WeaponType
    Introduces the KeepRange.MaxRearmTimerAllowed property to WeaponType, allowing control over whether KeepRange logic is applied based on the firer's remaining rearm time.

--- src/Ext/WeaponType/Body.cpp
+++ src/Ext/WeaponType/Body.cpp
@@ -133,6 +133,7 @@ void WeaponTypeExt::ExtData::LoadFromINIFile(CCINIClass* const pINI)
 	this->KeepRange.Read(exINI, pSection, "KeepRange");
 	this->KeepRange_AllowAI.Read(exINI, pSection, "KeepRange.AllowAI");
 	this->KeepRange_AllowPlayer.Read(exINI, pSection, "KeepRange.AllowPlayer");
+	this->KeepRange_MaxRearmTimerAllowed.Read(exINI, pSection, "KeepRange.MaxRearmTimerAllowed");
 	this->KickOutPassengers.Read(exINI, pSection, "KickOutPassengers");
 	this->Beam_Color.Read(exINI, pSection, "Beam.Color");
 	this->Beam_Duration.Read(exINI, pSection, "Beam.Duration");
@@ -213,6 +214,7 @@ void WeaponTypeExt::ExtData::Serialize(T& Stm)
 		.Process(this->KeepRange)
 		.Process(this->KeepRange_AllowAI)
 		.Process(this->KeepRange_AllowPlayer)
+		.Process(this->KeepRange_MaxRearmTimerAllowed)
 		.Process(this->KickOutPassengers)
 		.Process(this->Beam_Color)
 		.Process(this->Beam_Duration)
@@ -390,6 +392,9 @@ int WeaponTypeExt::GetTechnoKeepRange(WeaponTypeClass* pThis, TechnoClass* pFire
 	{
 		return 0;
 	}
+
+	if (pFirer->RearmTimer.GetTimeLeft() < pExt->KeepRange_MaxRearmTimerAllowed)
+		return 0;
 
 	if (!pFirer->RearmTimer.InProgress())
 	{
