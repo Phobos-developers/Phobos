0799bedd3c377193ee2e6af27b94658c6e543a52
ayylmaoRotE <pedro.h.soares@live.com>
2025-07-26 20:58:00 +1000

    Fix unit stuck in factory bug

--- src/Ext/Building/Body.cpp
+++ src/Ext/Building/Body.cpp
@@ -379,6 +379,10 @@ void BuildingExt::KickOutStuckUnits(BuildingClass* pThis)
 				if (pThis->Owner != pUnit->Owner || pUnit->IsTether)
 					continue;
 
+				// Skip attachments - they shouldn't be treated as stuck units
+				if (TechnoExt::IsAttached(pUnit))
+					continue;
+
 				const auto height = pUnit->GetHeight();
 
 				if (height < 0 || height > Unsorted::CellHeight)
