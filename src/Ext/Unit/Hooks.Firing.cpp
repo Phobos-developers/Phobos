0e8f48dac17e2c13e54b3c8897bdc099cc6fe732
Fly-Star <100747645+a851903106@users.noreply.github.com>
2025-10-15 17:35:44 +0800

    Update Hooks.Firing.cpp

--- src/Ext/Unit/Hooks.Firing.cpp
+++ src/Ext/Unit/Hooks.Firing.cpp
@@ -16,9 +16,10 @@ DEFINE_HOOK(0x736F61, UnitClass_UpdateFiring_FireUp, 0x6)
 		return 0;
 
 	const auto pExt = TechnoExt::ExtMap.Find(pThis);
+	const auto pTypeExt = pExt->TypeExtData;
 
 	// SHP vehicles have no secondary action frames, so it does not need SecondaryFire.
-	const int fireUp = pExt->TypeExtData->FireUp;
+	const int fireUp = pTypeExt->FireUp;
 
 	if (fireUp >= 0 && !pType->OpportunityFire && pThis->Locomotor->Is_Really_Moving_Now())
 	{
@@ -31,10 +32,26 @@ DEFINE_HOOK(0x736F61, UnitClass_UpdateFiring_FireUp, 0x6)
 	const int firingFrames = pType->FiringFrames;
 	const int frames = 2 * firingFrames - 1;
 
-	if (frames >= 0 && pThis->CurrentFiringFrame == -1)
-		pThis->CurrentFiringFrame = frames;
-
 	auto const pWeapon = pThis->GetWeapon(weaponIndex)->WeaponType;
+
+	if (frames >= 0)
+	{
+		bool keepFrame = false;
+
+		if (!pTypeExt->IsSecondary(weaponIndex))
+		{
+			int syncFrame = -1;
+			const int value = pThis->CurrentBurstIndex % pWeapon->Burst;
+
+			if (value < 2)
+				syncFrame = pType->FiringSyncFrame0 + value;
+
+			keepFrame = pThis->CurrentFiringFrame != -1 && syncFrame != -1;
+		}
+
+		if (pThis->CurrentFiringFrame == -1 || (fireUp < 0 && !keepFrame))
+			pThis->CurrentFiringFrame = frames;
+	}
 
 	if (fireUp >= 0)
 	{
