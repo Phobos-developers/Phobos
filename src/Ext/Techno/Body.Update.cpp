a4f2eee54409b711a095ad538a8397645623e5a1
Coronia <2217891145@qq.com>
2025-10-25 23:39:55 +0800

    handle AutoFire for type converstion

--- src/Ext/Techno/Body.Update.cpp
+++ src/Ext/Techno/Body.Update.cpp
@@ -895,6 +895,10 @@ void TechnoExt::ExtData::UpdateTypeData(TechnoTypeClass* pCurrentType)
 			pParasiteImUsing = nullptr;
 		}
 	}
+
+	// handle AutoFire
+	if (pOldTypeExt->AutoFire && !pNewTypeExt->AutoFire)
+		pThis->SetTarget(nullptr);
 }
 
 void TechnoExt::ExtData::UpdateTypeData_Foot()
