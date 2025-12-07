90853e09895b591a26ae55047cc1bbde9e3c2e61
九千天华 <1065703286@qq.com>
2025-12-07 21:34:08 +0800

    Flash fix

--- src/Misc/Hooks.AI.cpp
+++ src/Misc/Hooks.AI.cpp
@@ -5,7 +5,24 @@
 DEFINE_HOOK(0x55B6B3, LogicClass_AI_After, 0x5)
 {
 	for (auto const& attachment : AttachmentClass::Array)
-		attachment->AI();
+	{
+		auto pType = attachment->GetType();
+		if (pType && pType->InheritStateEffects && !pType->PassSelection && attachment->Child)
+		{
+			int childFlashRemaining = attachment->Child->Flashing.DurationRemaining;
+
+			attachment->AI();
+
+			if (childFlashRemaining > 0)
+			{
+				attachment->Child->Flash(childFlashRemaining);
+			}
+		}
+		else
+		{
+			attachment->AI();
+		}
+	}
 
 	return 0;
 }
