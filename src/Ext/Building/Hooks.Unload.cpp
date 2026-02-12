296d0b5e1bfab8d1794f52a5299c0f2f498a9a22
Fly-Star <100747645+a851903106@users.noreply.github.com>
2026-02-12 23:55:15 +0800

    Support UndeployDelay

--- src/Ext/Building/Hooks.Unload.cpp
+++ src/Ext/Building/Hooks.Unload.cpp
@@ -64,7 +64,10 @@ DEFINE_HOOK(0x44E29D, BuildingClass_Mission_Unload_DeployFire, 0x6)
 				}
 			}
 
-			R->EBX(ScenarioClass::Instance->Random.RandomRanged(0, 2) + 14);
+			const int undeployDelay = pType->UndeployDelay;
+			const int result = undeployDelay < 0 ? (ScenarioClass::Instance->Random.RandomRanged(0, 2) + 14) : std::max(undeployDelay, 1);
+
+			R->EBX(result);
 			return SkipGameCode;
 		}
 
