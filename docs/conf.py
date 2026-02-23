edf60a23925ddfe5da74a645a447962449b51d21
Noble_Fish <1065703286@qq.com>
2026-02-23 09:17:07 +0800

    line

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 locale_dirs = ['locale/']
 gettext_compact = False
-gettext_location = False
 
 # -- Options for HTML output -------------------------------------------------
 
