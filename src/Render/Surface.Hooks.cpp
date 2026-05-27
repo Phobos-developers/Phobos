c15668ec39829c74f5fdadefc88d8c94bc7f5b42
secsome <302702960@qq.com>
2026-05-28 03:32:06 +0800

    Fix ProgressScreenClass crash

--- src/Render/Surface.Hooks.cpp
+++ src/Render/Surface.Hooks.cpp
@@ -6,6 +6,12 @@ DEFINE_FUNCTION_JUMP(LJMP, 0x4BA770, DXSurface::CreatePrimary);
 
 DEFINE_JUMP(LJMP, 0x77747A, 0x777575); // Skip Restore_Check.
 
+static bool __fastcall DSurface_IsSurfaceLost(DSurface*, void*) // Always return false to prevent surface lost state
+{
+	return false;
+}
+DEFINE_FUNCTION_JUMP(LJMP, 0x4BAFE0, DSurface_IsSurfaceLost);
+
 static DXSurface* __fastcall DXSurfaceCtor(DXSurface* pSurface, void*, int width, int height, bool systemMem, bool enable3D) {
 	return new(pSurface) DXSurface(width, height);
 }
