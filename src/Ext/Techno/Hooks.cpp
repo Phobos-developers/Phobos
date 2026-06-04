db48e0849eaccbb1e34a136458d4da7e3b8623b2
Noble_Fish <1065703286@qq.com>
2026-06-04 13:49:58 +0800

    move to `InfantryClass`

--- src/Ext/Techno/Hooks.cpp
+++ src/Ext/Techno/Hooks.cpp
@@ -49,7 +49,6 @@ DEFINE_HOOK(0x4DA54E, FootClass_AI, 0x6)
 	pExt->UpdateWarpInDelay();
 	pExt->UpdateTiberiumEater();
 	pExt->AmmoAutoConvertActions();
-	pExt->DeployConvertAction();
 
 	return 0;
 }
