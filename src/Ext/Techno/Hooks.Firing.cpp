Phobos-developers/Phobos

commit e6b219c1e235bb14ddf9cc66c325ee9f2d563163

Allow UnitTypes to use `AlternateFLH` in opentopped transport (#812)

src/Ext/Techno/Hooks.Firing.cpp
@@ -409,3 +409,10 @@ DEFINE_HOOK(0x6FF4CC, TechnoClass_FireAt_ToggleLaserWeaponIndex, 0x6)
 
 	return 0;
 }
+
+
+// Feature: Allow Units using AlternateFLHs - by Trsdy
+// I don't want to rewrite something new, so I use the Infantry one directly
+// afaik it has no check for infantry-specific stuff here so far
+// and neither Ares nor Phobos has touched it, even that crawling flh one was in TechnoClass
+DEFINE_JUMP(VTABLE, 0x7F5D20, 0x523250);// Redirect UnitClass::GetFLH to InfantryClass::GetFLH (used to be TechnoClass::GetFLH)

src/Ext/Techno/Body.h
@@ -5,7 +5,7 @@
 #include <Helpers/Macro.h>
 #include <Utilities/Container.h>
 #include <Utilities/TemplateDef.h>
-
+#include <Utilities/Macro.h>
 #include <New/Entity/ShieldClass.h>
 #include <New/Entity/LaserTrailClass.h>
 

src/Ext/Techno/Hooks.cpp
@@ -2,7 +2,7 @@
 #include <ScenarioClass.h>
 #include <GameStrings.h>
 #include "Body.h"
-#include <Utilities/Macro.h>
+
 #include <Ext/TechnoType/Body.h>
 #include <Ext/WarheadType/Body.h>
 #include <Ext/WeaponType/Body.h>

commit cc5483d44ecc3a6fc7cac2d8b823344a4ae7d284

Fix `SecondSpawnOffset` broken by #812 and a73a3a
By analogy, `[DRED]` will not be symmetrical to -L when used as a passenger, so this is also not used when used as a passenger.

src/Ext/Techno/Hooks.Firing.cpp
@@ -962,7 +962,7 @@ CoordStruct* GetFLHTemp::UnitClassFake::_GetFLH(CoordStruct* outBuffer, int weap
 		}
 
 		auto TechnoClass_GetFLH = reinterpret_cast<CoordStruct*(__thiscall*)(TechnoClass*, CoordStruct*, int, CoordStruct)>(0x6F3AD0);
-		TechnoClass_GetFLH(pThis, outBuffer, weaponIdx, CoordStruct::Empty);
+		TechnoClass_GetFLH(pThis, outBuffer, weaponIdx, offset);
 	}
 	while (false);
 
@@ -979,6 +979,7 @@ DEFINE_HOOK(0x6F3AEB, TechnoClass_GetFLH, 0x6)
 	GET(TechnoTypeClass*, pType, EAX);
 	GET(const int, weaponIndex, ESI);
 	GET_STACK(CoordStruct*, pCoords, STACK_OFFSET(0xD8, 0x4));
+	REF_STACK(CoordStruct, offset, STACK_OFFSET(0xD8, 0xC));
 
 	bool allowOnTurret = true;
 	CoordStruct flh = CoordStruct::Empty;
@@ -1012,6 +1013,7 @@ DEFINE_HOOK(0x6F3AEB, TechnoClass_GetFLH, 0x6)
 			allowOnTurret = false;
 	}
 
+	flh += offset;
 	*pCoords = TechnoExt::GetFLHAbsoluteCoords(pThis, flh, allowOnTurret);
 	R->EAX(pCoords);
 