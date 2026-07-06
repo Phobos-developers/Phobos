2ee1e3a0fceee96aec893683490a4bddc41deacd
Noble_Fish <1065703286@qq.com>
2026-07-06 21:00:28 +0800

    update

--- src/Ext/House/Hooks.cpp
+++ src/Ext/House/Hooks.cpp
@@ -226,7 +226,7 @@ DEFINE_HOOK(0x7015C9, TechnoClass_Captured_UpdateTracking, 0x6)
 			if (humanToComputer && computerToHuman)
 			{
 				TechnoExt::KillSelf(pThis, pTypeExt->AutoDeath_Behavior, pTypeExt->AutoDeath_VanishAnimation, isInLimbo);
-				return 0x701881;
+				return 0;
 			}
 			else if (humanToComputer || computerToHuman)
 			{
@@ -237,7 +237,7 @@ DEFINE_HOOK(0x7015C9, TechnoClass_Captured_UpdateTracking, 0x6)
 					if ((I_am_human && humanToComputer) || (!I_am_human && computerToHuman))
 					{
 						TechnoExt::KillSelf(pThis, pTypeExt->AutoDeath_Behavior, pTypeExt->AutoDeath_VanishAnimation, isInLimbo);
-						return 0x701881;
+						return 0;
 					}
 				}
 			}
