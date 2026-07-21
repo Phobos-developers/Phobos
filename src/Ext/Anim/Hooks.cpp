CrimRecya/Phobos-Mix

commit cb6d4b107bd75a9f33d6c1cc9ef0e748a3752dec

Animatable template & animation transparency customization settings (#1304)
Split from #1300 

-------------

### Customizable animation transparency settings

- `Translucency.Cloaked` can be used to override `Translucency` on
animations attached to currently cloaked TechnoTypes.
- Both `Translucency` and `Translucency.Cloaked` can use the new
keyframe system to animate along with the animation. Read more about the
keyframe system below.
- If interpolation is enabled, the keyframe values are clamped to valid
transparency values (0,25,50 and 75), e.g a value of 1.5 would become 0
and 56.525 would become 50 and so on.

In `artmd.ini`:
```ini
[SOMEANIM]             ; AnimationType
Translucency=0         ; integer - only accepted values are 75, 50, 25 and 0.
Translucency.Cloaked=  ; integer - only accepted values are 75, 50, 25 and 0.
```

----------------

### Keyframe animations (Animatable template)

- Some features use keyframe-based animation system to define animations
in INI. Defined in INI it looks something like following.

```ini
[SOMESECTION]
BASEKEY.KeyframeN.Value=        ; Key-dependant value type
BASEKEY.KeyframeN.Percentage=   ; floating point value, percents or absolute
BASEKEY.KeyframeN.Absolute=     ; integer, zero-based frame index
BASEKEY.Interpolation=none      ; Interpolation mode (none|linear)
BASEKEY.ResetData=false         ; boolean
```

- `BASEKEY` is whatever base key name the feature in question may use.
`N` is zero-based keyframe index. If no keyframes are defined, a single
value from `BASEKEY` is attempted to be parsed instead.
- `Value` is a key/feature-dependant value type associated with that
keyframe.
- `Percentage` is the percentage through the animation's frames where
the keyframe becomes active. It is also possible to instead use
zero-based frame index via `Absolute` which takes precedence over
percentage, albeit it is internally converted to a percentage value.
- `Interpolation` controls interpolation of values between keyframes.

src/Ext/Anim/Hooks.cpp
@@ -479,6 +479,82 @@ DEFINE_HOOK(0x423061, AnimClass_DrawIt_Visibility, 0x6)
 	return 0;
 }
 
+// Reverse-engineered from YR with exception of new additions.
+DEFINE_HOOK(0x42308D, AnimClass_DrawIt_Transparency, 0x6)
+{
+	enum { SkipGameCode = 0x4230FE, ReturnFromFunction = 0x4238A3 };
+
+	GET(AnimClass*, pThis, ESI);
+	GET(BlitterFlags, flags, EBX);
+
+	auto const pType = pThis->Type;
+	int translucencyLevel = pThis->TranslucencyLevel; // Used by building animations when building needs to be drawn partially transparent. >= 15 means animation skips drawing.
+
+	if (translucencyLevel >= 15)
+		return ReturnFromFunction;
+
+	int currentFrame = pThis->Animation.Value;
+	int frames = pType->End;
+	auto const pTypeExt = AnimTypeExt::ExtMap.Find(pType);
+
+	if (!pType->Translucent)
+	{
+		TranslucencyLevel level;
+		bool hasValue = false;
+
+		if (pTypeExt->Translucency_Cloaked.HasValues())
+		{
+			// New addition: Different Translucency animation for attached animations on cloaked objects. Also keyframeable.
+			if (auto const pTechno = abstract_cast<TechnoClass*>(pThis->OwnerObject))
+			{
+				if (pTechno->CloakState == CloakState::Cloaked || pTechno->CloakState == CloakState::Cloaking)
+				{
+					level = pTypeExt->Translucency_Cloaked.Get(static_cast<double>(currentFrame) / frames);
+					hasValue = true;
+				}
+			}
+		}
+
+		if (!hasValue && pTypeExt->Translucency.HasValues())
+		{
+			// New addition: Keyframeable Translucency, replaces game Translucency setting.
+			level = pTypeExt->Translucency.Get(static_cast<double>(currentFrame) / frames);
+		}
+
+		if (level == BlitterFlags::None)
+		{
+			// Translucency <= 0, map translucencyLevel to transparency blitter flags
+			if (translucencyLevel)
+			{
+				if (translucencyLevel > 10)
+					flags |= BlitterFlags::TransLucent50;
+				else if (translucencyLevel > 5)
+					flags |= BlitterFlags::TransLucent50;
+				else
+					flags |= BlitterFlags::TransLucent25;
+			}
+		}
+		else
+		{
+			// Translucency > 0, transparency level directly maps to blitter flags.
+			flags |= level;
+		}
+	}
+	else
+	{
+		// Translucent=yes vanilla game logic.
+		if (currentFrame > frames * 0.6)
+			flags |= BlitterFlags::TransLucent75;
+		else if (currentFrame > frames * 0.4)
+			flags |= BlitterFlags::TransLucent50;
+		else if (currentFrame > frames * 0.2)
+			flags |= BlitterFlags::TransLucent25;
+	}
+
+	R->EBX(flags);
+	return SkipGameCode;
+}
+
 #pragma region AltPalette
 
 // Fix AltPalette anims not using owner color scheme.

Phobos.vcxproj
@@ -391,6 +391,7 @@
     <ClInclude Include="src\Utilities\Anchor.h" />
     <ClInclude Include="src\Utilities\AresFunctions.h" />
     <ClInclude Include="src\Utilities\AresHelper.h" />
+    <ClInclude Include="src\Utilities\Interpolation.h" />
     <ClInclude Include="src\Utilities\Constructs.h" />
     <ClInclude Include="src\Utilities\Container.h" />
     <ClInclude Include="src\Utilities\Debug.h" />

src/Ext/AnimType/Body.cpp
@@ -102,6 +102,8 @@ void AnimTypeExt::ExtData::LoadFromINIFile(CCINIClass* pINI)
 	this->VisibleTo_ConsiderInvokerAsOwner.Read(exINI, pID, "VisibleTo.ConsiderInvokerAsOwner");
 	this->RestrictVisibilityIfCloaked.Read(exINI, pID, "RestrictVisibilityIfCloaked");
 	this->DetachOnCloak.Read(exINI, pID, "DetachOnCloak");
+	this->Translucency.Read(exINI, pID, "Translucency", this->OwnerObject()->End);
+	this->Translucency_Cloaked.Read(exINI, pID, "Translucency.Cloaked", this->OwnerObject()->End, true);
 	this->ConstrainFireAnimsToCellSpots.Read(exINI, pID, "ConstrainFireAnimsToCellSpots");
 	this->FireAnimDisallowedLandTypes.Read<false, true>(exINI, pID, "FireAnimDisallowedLandTypes");
 	this->AttachFireAnimsToParent.Read(exINI, pID, "AttachFireAnimsToParent");
@@ -162,6 +164,8 @@ void AnimTypeExt::ExtData::Serialize(T& Stm)
 		.Process(this->VisibleTo_ConsiderInvokerAsOwner)
 		.Process(this->RestrictVisibilityIfCloaked)
 		.Process(this->DetachOnCloak)
+		.Process(this->Translucency)
+		.Process(this->Translucency_Cloaked)
 		.Process(this->ConstrainFireAnimsToCellSpots)
 		.Process(this->FireAnimDisallowedLandTypes)
 		.Process(this->AttachFireAnimsToParent)

src/Ext/AnimType/Body.h
@@ -50,6 +50,8 @@ public:
 		Valueable<bool> VisibleTo_ConsiderInvokerAsOwner;
 		Valueable<bool> RestrictVisibilityIfCloaked;
 		Valueable<bool> DetachOnCloak;
+		Animatable<TranslucencyLevel> Translucency;
+		Animatable<TranslucencyLevel> Translucency_Cloaked;
 		Valueable<bool> ConstrainFireAnimsToCellSpots;
 		Nullable<LandTypeFlags> FireAnimDisallowedLandTypes;
 		Nullable<bool> AttachFireAnimsToParent;
@@ -90,6 +92,8 @@ public:
 			, VisibleTo_ConsiderInvokerAsOwner { false }
 			, RestrictVisibilityIfCloaked { false }
 			, DetachOnCloak { true }
+			, Translucency { TranslucencyLevel {} }
+			, Translucency_Cloaked { TranslucencyLevel {} }
 			, ConstrainFireAnimsToCellSpots { true }
 			, FireAnimDisallowedLandTypes {}
 			, AttachFireAnimsToParent {}

src/Utilities/Constructs.h
@@ -461,8 +461,20 @@ class TranslucencyLevel
 public:
 	constexpr TranslucencyLevel() noexcept = default;
 
-	TranslucencyLevel(int nInt)
+	TranslucencyLevel(int nInt, bool clamp = false)
 	{
+		if (clamp)
+		{
+			if (nInt >= 75)
+				nInt = 75;
+			else if (nInt >= 50)
+				nInt = 50;
+			else if (nInt >= 25)
+				nInt = 25;
+			else
+				nInt = 0;
+		}
+
 		*this = nInt;
 	}
 
@@ -488,14 +500,36 @@ public:
 		return *this;
 	}
 
-	operator BlitterFlags()
+	operator BlitterFlags() const
 	{
 		return this->value;
 	}
 
-	BlitterFlags GetBlitterFlags()
+	BlitterFlags GetBlitterFlags() const
 	{
-		return *this;
+		return this->value;
+	}
+
+	int GetIntValue() const
+	{
+		int value = 0;
+
+		switch (this->value)
+		{
+		case BlitterFlags::TransLucent75:
+			value = 75;
+			break;
+		case BlitterFlags::TransLucent50:
+			value = 50;
+			break;
+		case BlitterFlags::TransLucent25:
+			value = 25;
+			break;
+		default:
+			break;
+		}
+
+		return value;
 	}
 
 	bool Read(INI_EX& parser, const char* pSection, const char* pKey);

src/Utilities/Enum.h
@@ -275,7 +275,6 @@ enum class VerticalPosition : BYTE
 	Center = 1,
 	Bottom = 2
 };
-
 //hexagon
 enum class BuildingSelectBracketPosition :BYTE
 {
@@ -405,4 +404,10 @@ public:
 		}
 		return false;
 	}
+};
+
+enum class InterpolationMode : BYTE
+{
+	None = 0,
+	Linear = 1
 };

src/Utilities/Interpolation.h
@@ -1,0 +1,65 @@
+#pragma once
+
+#include <YRPPCore.h>
+#include "Enum.h"
+
+namespace detail
+{
+	template <typename T>
+	inline T interpolate(T& first, T& second, double percentage, InterpolationMode mode)
+	{
+		static_assert((sizeof(T) == 0), "interpolate<T> should not be instantiated, create and use a specialization.");
+		return first;
+	}
+
+	template <>
+	inline double interpolate<double>(double& first, double& second, double percentage, InterpolationMode mode)
+	{
+		double result = first;
+
+		switch (mode)
+		{
+		case InterpolationMode::Linear:
+			result = first + ((second - first) * percentage);
+			break;
+		default:
+			break;
+		}
+
+		return result;
+	}
+
+	template <>
+	inline int interpolate<int>(int& first, int& second, double percentage, InterpolationMode mode)
+	{
+		double firstValue = first;
+		double secondValue = second;
+		return (int)interpolate(firstValue, secondValue, percentage, mode);
+	}
+
+	template <>
+	inline BYTE interpolate<BYTE>(BYTE& first, BYTE& second, double percentage, InterpolationMode mode)
+	{
+		double firstValue = first;
+		double secondValue = second;
+		return (BYTE)interpolate(firstValue, secondValue, percentage, mode);
+	}
+
+	template <>
+	inline ColorStruct interpolate<ColorStruct>(ColorStruct& first, ColorStruct& second, double percentage, InterpolationMode mode)
+	{
+		BYTE r = interpolate(first.R, second.R, percentage, mode);
+		BYTE g = interpolate(first.G, second.G, percentage, mode);
+		BYTE b = interpolate(first.B, second.B, percentage, mode);
+		return ColorStruct { r, g, b };
+	}
+
+	template <>
+	inline TranslucencyLevel interpolate<TranslucencyLevel>(TranslucencyLevel& first, TranslucencyLevel& second, double percentage, InterpolationMode mode)
+	{
+		double firstValue = first.GetIntValue();
+		double secondValue = second.GetIntValue();
+		int value = (int)interpolate(firstValue, secondValue, percentage, mode);
+		return TranslucencyLevel(value, true);
+	}
+}

src/Utilities/Savegame.h
@@ -8,6 +8,23 @@
 namespace Savegame
 {
 	template <typename T>
+	concept ImplementsUpperCaseSaveLoad = requires (PhobosStreamWriter& stmWriter, PhobosStreamReader& stmReader, T& value, bool registerForChange)
+	{
+		value.Save(stmWriter);
+		value.Load(stmReader, registerForChange);
+	};
+
+	template <typename T>
+	concept ImplementsLowerCaseSaveLoad = requires (PhobosStreamWriter& stmWriter, PhobosStreamReader& stmReader, T& value, bool registerForChange)
+	{
+		value.save(stmWriter);
+		value.load(stmReader, registerForChange);
+	};
+
+	template <typename T>
+	concept ImplementsSaveLoad = ImplementsUpperCaseSaveLoad<T> || ImplementsLowerCaseSaveLoad<T>;
+
+	template <typename T>
 	bool ReadPhobosStream(PhobosStreamReader& Stm, T& Value, bool RegisterForChange = true);
 
 	template <typename T>

Phobos-developers/Phobos

commit 0ee50766b2154910f9f6a8cead92e031aa5d9c9a

Attempt to fix a regression introduced in cb6d4b107bd75a9f33d6c1cc9ef0e748a3752dec causing issues with drawing building animations for cloaked buildings
- Also optimize code by only hooking within relevant code path

src/Ext/Anim/Hooks.cpp
@@ -392,7 +392,7 @@ DEFINE_HOOK(0x423654, AnimClass_DrawIt_Tiled_Interval, 0x5)
 DEFINE_HOOK(0x423660, AnimClass_DrawIt_Tiled_Center, 0x5)
 {
 	GET(AnimClass*, pThis, ESI);
-	GET(const int, height, EAX); 
+	GET(const int, height, EAX);
 	R->EDX(VTable::Get(pThis)); // Restore overriden instruction
 
 	const auto pTypeExt = AnimTypeExt::Fetch(pThis->Type);
@@ -513,76 +513,61 @@ DEFINE_HOOK(0x423061, AnimClass_DrawIt_Visibility, 0x6)
 	return 0;
 }
 
-// Reverse-engineered from YR with exception of new additions.
-DEFINE_HOOK(0x42308D, AnimClass_DrawIt_Transparency, 0x6)
+// Reverse-engineered from YR for Translucent=no code path only with exception of new additions.
+DEFINE_HOOK(0x423183, AnimClass_DrawIt_Translucency, 0x6)
 {
 	enum { SkipGameCode = 0x4230FE, ReturnFromFunction = 0x4238A3 };
 
 	GET(AnimClass*, pThis, ESI);
 	GET(BlitterFlags, flags, EBX);
 
-	auto const pType = pThis->Type;
+	const auto pType = pThis->Type;
+	const auto pTypeExt = AnimTypeExt::Fetch(pType);
 	const int translucencyLevel = pThis->TranslucencyLevel; // Used by building animations when building needs to be drawn partially transparent. >= 15 means animation skips drawing.
-
-	if (translucencyLevel >= 15)
-		return ReturnFromFunction;
-
 	const int currentFrame = pThis->Animation.Value;
 	const int frames = pType->End;
-	auto const pTypeExt = AnimTypeExt::Fetch(pType);
+	TranslucencyLevel level;
+	bool hasValue = false;
 
-	if (!pType->Translucent)
+	if (pTypeExt->Translucency_Cloaked.HasValues())
 	{
-		TranslucencyLevel level;
-		bool hasValue = false;
-
-		if (pTypeExt->Translucency_Cloaked.HasValues())
+		// New addition: Different Translucency animation for attached animations on cloaked objects. Also keyframeable.
+		if (const auto pTechno = abstract_cast<TechnoClass*>(pThis->OwnerObject))
 		{
-			// New addition: Different Translucency animation for attached animations on cloaked objects. Also keyframeable.
-			if (auto const pTechno = abstract_cast<TechnoClass*>(pThis->OwnerObject))
+			if (pTechno->CloakState == CloakState::Cloaked || pTechno->CloakState == CloakState::Cloaking)
 			{
-				if (pTechno->CloakState == CloakState::Cloaked || pTechno->CloakState == CloakState::Cloaking)
-				{
-					level = pTypeExt->Translucency_Cloaked.Get(static_cast<double>(currentFrame) / frames);
-					hasValue = true;
-				}
+				level = pTypeExt->Translucency_Cloaked.Get(static_cast<double>(currentFrame) / frames);
+				hasValue = true;
 			}
 		}
+	}
 
-		if (!hasValue && pTypeExt->Translucency.HasValues())
-		{
-			// New addition: Keyframeable Translucency, replaces game Translucency setting.
-			level = pTypeExt->Translucency.Get(static_cast<double>(currentFrame) / frames);
-		}
+	if (!hasValue && pTypeExt->Translucency.HasValues())
+	{
+		// New addition: Keyframeable Translucency, replaces game Translucency setting.
+		level = pTypeExt->Translucency.Get(static_cast<double>(currentFrame) / frames);
+	}
 
-		if (level == BlitterFlags::None)
+	if (level == BlitterFlags::None)
+	{
+		// Translucency <= 0, map translucencyLevel to transparency blitter flags
+		if (translucencyLevel)
 		{
-			// Translucency <= 0, map translucencyLevel to transparency blitter flags
-			if (translucencyLevel)
-			{
-				if (translucencyLevel > 10)
-					flags |= BlitterFlags::TransLucent50;
-				else if (translucencyLevel > 5)
-					flags |= BlitterFlags::TransLucent50;
-				else
-					flags |= BlitterFlags::TransLucent25;
-			}
-		}
-		else
-		{
-			// Translucency > 0, transparency level directly maps to blitter flags.
-			flags |= level;
+			if (translucencyLevel > 15)
+				return ReturnFromFunction;
+			else if (translucencyLevel > 5)
+				flags |= BlitterFlags::TransLucent50;
+			else
+				flags |= BlitterFlags::TransLucent25;
 		}
 	}
 	else
 	{
-		// Translucent=yes vanilla game logic.
-		if (currentFrame > frames * 0.6)
-			flags |= BlitterFlags::TransLucent75;
-		else if (currentFrame > frames * 0.4)
-			flags |= BlitterFlags::TransLucent50;
-		else if (currentFrame > frames * 0.2)
-			flags |= BlitterFlags::TransLucent25;
+		// Translucency > 0, Translucency directly maps to blitter flags.
+		if (translucencyLevel >= 15)
+			return ReturnFromFunction;
+
+		flags |= level;
 	}
 
 	R->EBX(flags);