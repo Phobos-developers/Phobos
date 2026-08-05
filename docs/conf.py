24e80283dcdb8f8acb3dc29a3016513bbec9dfbb
Noble_Fish <1065703286@qq.com>
2026-08-05 22:54:09 +0800

    Resolve the conflict between VSCode navigation and HTML navigation caused by includes
    Previously, we often omitted paths in README.md to ensure proper HTML navigation, but this also caused the navigation links on the GitHub project homepage and in VSCode to become incorrect. This issue is now resolved by introducing this extension.
    
    Assisted-by: DeepSeek V4 Flash

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ author = 'The Phobos Contributors'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_rtd_theme', 'myst_parser', 'sphinx.ext.mathjax', 'sphinx_design', '_ext.sanitize_system_messages']
+extensions = ['sphinx_rtd_theme', 'myst_parser', 'sphinx.ext.mathjax', 'sphinx_design', '_ext.sanitize_system_messages', '_ext.fix_included_readme_links']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
