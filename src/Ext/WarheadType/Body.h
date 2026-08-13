eb0e6bb5916c10b49ce5fa3da2156bd977930b51
Noble_Fish <1065703286@qq.com>
2026-08-14 06:02:46 +0800

    standardize the initialization syntax of `Damage_Deployed` and `Cloning_Powered`

--- src/Ext/WarheadType/Body.h
+++ src/Ext/WarheadType/Body.h
@@ -277,7 +277,7 @@ public:
 	Valueable<bool> EffectsRequireVerses;
 	Valueable<bool> Malicious;
 	Nullable<int> Flash_Duration;
-	Valueable<double> Damage_Deployed { 1.0 };
+	Valueable<double> Damage_Deployed;
 
 	double Crit_RandomBuffer;
 	double Crit_CurrentChance;
@@ -513,6 +513,7 @@ public:
 		, EffectsRequireVerses { true }
 		, Malicious { true }
 		, Flash_Duration {}
+		, Damage_Deployed { 1.0 }
 
 		, Crit_RandomBuffer { 0.0 }
 		, Crit_CurrentChance { 0.0 }
