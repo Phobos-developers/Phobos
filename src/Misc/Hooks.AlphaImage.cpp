9ccde93a3cefc4bd4015f7ec83199506eb1b69b3
Noble_Fish <1065703286@qq.com>
2026-08-17 16:03:16 +0800

    Update Hooks.AlphaImage.cpp
    Fix the issue where AlphaShape is not erased cleanly (either leaving residues or being only partially cut while the rest remains)

--- src/Misc/Hooks.AlphaImage.cpp
+++ src/Misc/Hooks.AlphaImage.cpp
@@ -87,8 +87,12 @@ static void __fastcall UpdateAlphaShape(ObjectClass* pSource)
 
 	if (inactive)
 	{
-		if (auto pAlpha = alphaExt.get_or_default(pSource))
+		if (const auto pAlpha = alphaExt.get_or_default(pSource))
+		{
+			const RectangleStruct dirty = { pAlpha->Rect.X - tacticalPos->X, pAlpha->Rect.Y - tacticalPos->Y, pAlpha->Rect.Width, pAlpha->Rect.Height };
+			TacticalClass::Instance->RegisterDirtyArea(dirty, true);
 			GameDelete(pAlpha);
+		}
 
 		return;
 	}
