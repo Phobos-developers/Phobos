6a21a0686cd3e6e57e12ab9e3c59d04e510ffad9
Coronia <2217891145@qq.com>
2026-08-19 17:08:48 +0800

    tweak

--- src/Ext/WarheadType/Detonate.cpp
+++ src/Ext/WarheadType/Detonate.cpp
@@ -210,9 +210,6 @@ void WarheadTypeExt::DetonateOnOneUnit(HouseClass* pHouse, TechnoClass* pTarget,
 	if (this->ChangeOwner)
 		this->ApplyOwnerChange(pHouse, pTarget);
 
-	if (this->Ammo != 0)
-		this->ApplyAmmoModifier(pTarget);
-
 	// These can change the target's techno types
 	if (this->Convert_Pairs.size() > 0)
 		this->ApplyConvert(pHouse, pTarget);
@@ -221,6 +218,9 @@ void WarheadTypeExt::DetonateOnOneUnit(HouseClass* pHouse, TechnoClass* pTarget,
 		this->ApplyBuildingUndeploy(pTarget);
 
 	// Other one time effects
+	if (this->Ammo != 0)
+		this->ApplyAmmoModifier(pTarget);
+
 	if (this->RemoveDisguise)
 		this->ApplyRemoveDisguise(pTarget);
 
@@ -494,7 +494,8 @@ HouseClass* WarheadTypeExt::ApplyRemoveMindControl(HouseClass* pHouse, TechnoCla
 void WarheadTypeExt::ExtData::ApplyOwnerChange(HouseClass* pHouse, TechnoClass* pTarget)
 {
 	const bool isMindControl = this->ChangeOwner_SetAsMindControl;
-	const bool isImmune = (isMindControl && pTarget->GetTechnoType()->ImmuneToPsionics) || pTarget->IsMindControlled();
+	const auto pType = pTarget->GetTechnoType();
+	const bool isImmune = (isMindControl && pType->ImmuneToPsionics) || pTarget->IsMindControlled();
 
 	if (!isImmune)
 	{
@@ -512,7 +513,7 @@ void WarheadTypeExt::ExtData::ApplyOwnerChange(HouseClass* pHouse, TechnoClass* 
 				if (isBld)
 					location.Z += static_cast<BuildingClass*>(pTarget)->Type->Height * Unsorted::LevelHeight;
 				else
-					location.Z += pTarget->GetTechnoType()->MindControlRingOffset;
+					location.Z += pType->MindControlRingOffset;
 
 				if (const auto pOwnerAnim = GameCreate<AnimClass>(pAnimType, location))
 				{
@@ -914,7 +915,7 @@ void WarheadTypeExt::ApplyPenetratesTransport(TechnoClass* pTarget, TechnoClass*
 
 void WarheadTypeExt::ExtData::ApplyAmmoModifier(TechnoClass* pTarget)
 {
-	int maxAmmo = pTarget->GetTechnoType()->Ammo;
+	const int maxAmmo = pTarget->GetTechnoType()->Ammo;
 	int newCurrentAmmo = this->Ammo + pTarget->Ammo;
 
 	newCurrentAmmo = newCurrentAmmo < 0 ? 0 : newCurrentAmmo;
