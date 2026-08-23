04657184050be4ef359c89de4b14ea43b38532ea
dh381-1 <2575903651@qq.com>
2026-08-23 18:12:37 +0800

    1

--- src/Ext/WeaponType/Body.h
+++ src/Ext/WeaponType/Body.h
@@ -119,11 +119,8 @@ public:
 
 	Nullable<bool> CylinderRangefinding;
 
-<<<<<<< Updated upstream
-=======
 	Valueable<bool> IvanBomb_Detonate;
 	
->>>>>>> Stashed changes
 	WeaponTypeExt(WeaponTypeClass* OwnerObject) : AbstractTypeExt(OwnerObject)
 		, DiskLaser_Radius { DiskLaserClass::Radius }
 		, ProjectileRange { Leptons(100000) }
