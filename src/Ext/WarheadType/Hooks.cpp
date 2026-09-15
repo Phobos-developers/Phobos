893e4816f4f3455293183d28da70771264c31f95
Fryone <epsilon.fryone@gmail.com>
2026-09-15 07:34:04 +0300

    Update src/Ext/WarheadType/Hooks.cpp
    Co-authored-by: Netsu_Negi <71598172+NetsuNegi@users.noreply.github.com>

--- src/Ext/WarheadType/Hooks.cpp
+++ src/Ext/WarheadType/Hooks.cpp
@@ -217,7 +217,7 @@ DEFINE_HOOK(0x48A5B3, SelectDamageAnimation_CritAnim, 0x6)
 
 	auto const pWHExt = WarheadTypeExt::Fetch(pThis);
 
-	const int divider = pWHExt->Crit_AnimList_DamageDivider ? pWHExt->Crit_AnimList_DamageDivider : 25;
+	const int divider = pWHExt->Crit_AnimList_DamageDivider;
 
 	if (pWHExt->Crit_Active && pWHExt->Crit_AnimList.size()
 		&& !pWHExt->Crit_AnimOnAffectedTargets.Get(RulesExt::Global()->Crit_AnimOnAffectedTargets))
