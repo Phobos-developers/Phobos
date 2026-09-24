c6a55d730bad4fa4df53bb325492dd03cdc649f7
Coronia <2217891145@qq.com>
2026-09-24 20:11:40 +0800

    try fix Upgarde anim not appearing when preplaced building is not powered

--- src/Ext/Building/Hooks.cpp
+++ src/Ext/Building/Hooks.cpp
@@ -293,11 +293,7 @@ DEFINE_HOOK(0x44FBBF, CreateBuildingFromINIFile_AfterCTOR_BeforeUnlimbo, 0x8)
 	if (auto const pExt = BuildingExt::TryFetch(pBld))
 	{
 		pExt->IsCreatedFromMapFile = true;
-
-		GET_STACK(bool, hasPower, STACK_OFFSET(0xEC, -0xDC));
-
-		if (hasPower)
-			pExt->HasPowerFromMapFile = true;
+		pExt->HasPowerFromMapFile = true;
 	}
 
 	return 0;
@@ -308,7 +304,14 @@ DEFINE_HOOK(0x44FDC5, CreateBuildingFromINIFile_AfterCTOR_AfterUnlimbo, 0xA)
 	GET(BuildingClass* const, pBld, ESI);
 
 	if (auto const pExt = BuildingExt::TryFetch(pBld))
+	{
 		pExt->HasPowerFromMapFile = false;
+
+		GET_STACK(const bool, hasPower, STACK_OFFSET(0xEC, -0xDC));
+
+		if (!hasPower)
+			pBld->GoOffline();
+	}
 
 	return 0x44FDD3;
 }
